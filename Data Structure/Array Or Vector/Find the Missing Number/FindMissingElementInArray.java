0ae88ec4ea4ced9a0b642e2666ccd16f5279e657
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-09-28 22:55:40 +0530

    Indentation - Solution by Chanpreet

--- Data Structure/Array Or Vector/Find the Missing Number/FindMissingElementInArray.java
+++ Data Structure/Array Or Vector/Find the Missing Number/FindMissingElementInArray.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
 
 public class FindMissingElementInArray {
-
 	public static void main(String[] args) {
-		int n,sum,i;
+		int n;
 		
-		Scanner sc=new Scanner(System.in);
-		n=sc.nextInt();
-		int a[]=new int[n];
-		for(i=0;i<n;i++)
-			a[i]=sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		int a[] = new int[n];
+		int i;
+		for(i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
 		sc.close(); //input ends
 		
-		sum=((n+1)*(n+2))/2; //finding sum of n+1 elements because one of them is missing
+		int sum = ( ( n + 1) * (n + 2) ) / 2; //finding sum of n elements because one of them is missing
 		
-		for(i=0;i<n;i++)
-			sum-=a[i];
-		
+		for( i = 0; i < n;i++) {
+			sum -= a[i];
+		}
 		System.out.println(sum);
 	}
 
