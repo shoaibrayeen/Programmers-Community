7e798b5624550b8058622eb27548cd68df5a1dcb
Chan <34856246+chanpreet1999@users.noreply.github.com>
2019-09-28 21:56:49 +0530

    SolutionByChanpreet.java
    Code for right rotation of array elemets using big integers

---
+++ Data Structure/Array Or Vector/Right Rotate An Array By K Elements/RightRotateArray.java
@@ -0,0 +1,35 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class RightRotateArrray {
+
+	public static void main(String[] args) {
+		int n,k,count=0,i;
+		BigInteger temp; //BigInteger doesn't have any theoretical upperBound 
+		
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		BigInteger a[]=new BigInteger[n];
+		for(i = 0; i < n ; i++)
+			a[i] = sc.nextBigInteger();
+		k = sc.nextInt();
+		sc.close();  //input ends
+		
+		while(count<k){	
+			temp = a[n-1];
+			
+			for(i = n-2; i >= 0 ;i--)
+				a[i+1] = a[i]; //shifting elements
+			
+			a[0] = temp;
+			count++;
+		}
+		
+		display(a);
+	}
+
+	private static void display(BigInteger[] a) {
+		for(BigInteger i:a)
+			System.out.print(i+" ");
+	}
+}
