100034b7ac518ff5c6fe91f89def0c1022de1954
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-09-30 14:58:48 +0530

    By using Auxiliary Space - solution By Dhruv

---
+++ Data Structure/Array Or Vector/Right Rotate An Array By K Elements/solutionByDhruv.cpp
@@ -0,0 +1,44 @@
+#include<iostream.h>
+#include<conio.h>
+
+void right_rotation( int a[], int n, int k );
+
+void main() {
+  int n, k;
+  cout << "\nEnter Size of Array\t:\t";            //Number of elements in array a[]
+  cin >> n;
+  int a[n];
+  cout << "\nEnter Array Elements\n";
+  for( int i = 0; i < n; i++ ) {
+    cin >> a[i];
+  }
+  cout << "\nEnter how many times you want to right rotate the array\t:\t";
+  cin >> k;
+  right_rotation( a , n , k);
+  getch();
+}
+
+void right_rotation(int a[], int n, int k) { 
+  int avail[n];
+  for( int i = n - 1; i > 0; i++ ) { 
+    if( i + k >= n ) { 
+       avail[ i + k - n ] = a[i];
+    }
+    else {
+        avail[ i + k ] = a[i];       //Save the element in avail[] array
+    }
+  }
+  cout << "\nRotated Array\t:\t";
+  for( int i = 0; i < n; i++ ) {
+       cout << avail[i] << " ";
+  }
+  return;
+}
+
+
+
+   
+
+
+
+  
