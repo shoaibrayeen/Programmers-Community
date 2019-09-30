f91c4d592ea691ae2d47383c7dd3859bc06ba742
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-09-30 15:03:41 +0530

    Using Dutch National Flag Algorithm

--- Data Structure/Array Or Vector/Sort An Array of 0s , 1s and 2s/SolutionByAnuj.cpp
+++ Data Structure/Array Or Vector/Sort An Array of 0s , 1s and 2s/SolutionByAnuj.cpp
@@ -3,42 +3,42 @@ using namespace std;
 
 void swap(int arr[],int i,int j){
     int temp;
-    temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
-void sort012(int arr[], int n)  {  
-    int var=0,low=0,high=n-1;
-    while(var<=high){
-        if(arr[var]==0){
-            swap(arr,var,low);
+void sort012(int *arr, int n)  {  
+    int var = 0, low = 0, high = n - 1;
+    while( var <= high ) {
+        if( arr[var] == 0 ) {
+            swap( arr, var, low );
             var++;
             low++;
-        }else if(arr[var]==1){
+        }
+	else if( arr[var] == 1 ) {
             var++;
-        }else if(arr[var]==2){
-            swap(arr,var,high);
+        }
+	else if( arr[var] == 2 ) {
+            swap( arr, var, high );
             high--;
         }
     }
-    
 }
 
 int main() {
-
 	int size_first;
-
-	cin>>size_first;
-	int *arr_first = new int[1+size_first];	
-	
-	for(int i=0; i<size_first; i++)
-		cin>>arr_first[i];
-	
-	sort012(arr_first,size_first);
-		
-	for(int i=0;i<size_first;i++){
-		cout<<arr_first[i]<<" ";	
-  }
+	cout << "\nEnter Size of Array\t:\t"
+	cin >> size_first;
+	int *arr_first = new int[size_first];	
+	cout << "\nEnter Array Elements\n";
+	for( int i = 0; i <size_first; i++ ) {
+		cin >> arr_first[i];
+	}
+	sort012( arr_first, size_first );
+	cout << "\nSorted Array\n";
+	for( int i = 0; i < size_first; i++ ) {
+		cout << arr_first[i] << " ";	
+  	}
 	return 0;
 }
