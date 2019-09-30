aeeb26ae1f0977008ce4f960d4ed2cb910288541
Dhruv <42606683+dA505819@users.noreply.github.com>
2019-10-01 01:56:27 +0530

    Update

--- Basic/Search An Element in Sorted Array/Create SolutionByDhruv.cpp
+++ Basic/Search An Element in Sorted Array/Create SolutionByDhruv.cpp
@@ -1,25 +1,32 @@
 #include<iostream.h>
 #include<conio.h>
+#include<process.h>
 void main()
 {
  int mid, n, low=0, high= n-1, item;
  cout<<"Enter size of array:";
  cin>>n;
+ int A[n];
+ for (i=0;i<n;i++)
+ { 
+   cout<<"Enter array";
+   cin>>A[i];
+  }
  cout<<"Enter Item to be searched";
  cout<<item;
- for(int i=0;i<n;i++)
+ for(i=0;i<n;i++)
  { 
    while(low<=high)
     {
        mid=(low + high)/2;
-     if(A[low]<item)
+     if(item>A[mid])
        {
-         low=low+1;
+         low=mid+1;
         }
      else
-      if(A[high]>item)
+      if(item<A[mid])
        {
-         high=high-1;
+         high=mid-1;
          }
      else exit(0);
       }
