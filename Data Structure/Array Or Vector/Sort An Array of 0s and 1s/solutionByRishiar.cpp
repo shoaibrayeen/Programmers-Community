14d2b4066f80a19a7f252d162d5801bca8239c38
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-09-30 14:52:30 +0530

    By Counting the Number of Zeros

---
+++ Data Structure/Array Or Vector/Sort An Array of 0s and 1s/solutionByRishiar.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, i;
+  cout << "\nEnter Size of Array\t:\t";
+  cin >> n;
+  int a[n];
+  cout << "\nEnter Array Elements\n";
+  for( i = 0; i < n; i++ ) {
+    cin >> a[i];
+  }
+  int Zcount = 0;
+  for( i = 0; i < n; i++ ) {
+    if( a[i] == 0 ) {
+      Zcount++;
+    }
+  }
+  i = 0;
+  while( i < n ) {
+      while( Zcount > 0 ) {
+          a[i] = 0;
+          Zcount--;
+          i++;
+      }
+      a[i] = 1;
+      i++;
+  }
+  cout << "\nModified Array\t:\t";
+  for( i = 0; i < n; i++ ) {
+    cout << a[i] << " ";
+  }
+  return 0;
+}
