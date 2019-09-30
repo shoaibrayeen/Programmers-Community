c1571a9ec9e7af4db7166071961ed9a0376b0b45
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-09-30 15:06:19 +0530

    using Reverse Algorithm

---
+++ Data Structure/Array Or Vector/Right Rotate An Array By K Elements/solutionByPreeti.py
@@ -0,0 +1,18 @@
+def rotateRight( a,  k):
+    a  = [8,9,10,7,33]
+    length = a.len()
+    reverse( a, 0, len(a) - 1 )
+    reverse( a, 0, k - 1 )
+    reverse( a, k, n - 1 )
+    
+def reverse( a,  start,  end ):
+    while(start < end):
+        swap( a, start, end )
+        start = start+1
+        end = end-1
+        
+def swap( a, pos1, pos2 ):
+    temp = a[pos1]
+    a[pos1] = a[pos2]
+    a[pos2] = temp
+    
