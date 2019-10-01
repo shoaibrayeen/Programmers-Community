6363122129547af3c111be6f4189478d916d0a49
Garima26Sharma <43791321+Garima26Sharma@users.noreply.github.com>
2019-10-01 22:57:46 +0530

    updates

--- Data Structure/Array Or Vector/Find the Missing Number/solutionByChanpreet.java
+++ Data Structure/Array Or Vector/Find the Missing Number/solutionByChanpreet.java
@@ -5,18 +5,18 @@ public class FindMissingElementInArray {
 		int n;
 		
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		int a[] = new int[n];
+		size = sc.nextInt();
+		int a[] = new int[size];
 		int i;
-		for(i = 0; i < n; i++) {
+		for(i = 0; i < size; i++) {
 			a[i] = sc.nextInt();
 		}
 		sc.close(); //input ends
 		
-		int sum = ( ( n + 1) * (n + 2) ) / 2; //finding sum of n elements because one of them is missing
+		int sum = ( ( size + 1) * (size + 2) ) / 2; //finding sum of n elements because one of them is missing
 		
-		for( i = 0; i < n;i++) {
-			sum -= a[i];
+		for( i = 0; i < size;i++) {
+			sum-=a[i];
 		}
 		System.out.println(sum);
 	}
