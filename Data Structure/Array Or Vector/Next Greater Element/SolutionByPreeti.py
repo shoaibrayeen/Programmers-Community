e1f10a800bc1793653d7123c078d3ae4c5fd5876
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-10-02 16:33:52 +0530

    using Brute Force - O(n^2) Solution

--- Data Structure/Array Or Vector/Next Greater Element/SolutionByPreeti.py
+++ Data Structure/Array Or Vector/Next Greater Element/SolutionByPreeti.py
@@ -1,13 +1,13 @@
-t = int(input())
 def NextGenerator(arr):
-    for i in range(0, len(arr), 1): 
+    for i in range(0, len(arr)): 
   
         next = -1
         for j in range(i+1, len(arr), 1): 
             if arr[i] < arr[j]: 
                 next = arr[j] 
                 break
-        print(str(arr[i]) + " -- " + str(next))       
+        print(str(arr[i]) + " -- " + str(next))     
+        
 arr = [4,5,2,25] 
 NextGenerator(arr)        
         
