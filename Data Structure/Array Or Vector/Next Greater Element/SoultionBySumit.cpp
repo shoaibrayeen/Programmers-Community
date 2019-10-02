9687b854b252e3b6b30b169006dc1e130e7cfc5c
Sumit <29905772+isumit19@users.noreply.github.com>
2019-10-02 14:56:32 +0530

    Update SoultionBySumit.cpp

--- Data Structure/Array Or Vector/Next Greater Element/SoultionBySumit.cpp
+++ Data Structure/Array Or Vector/Next Greater Element/SoultionBySumit.cpp
@@ -1,28 +1,29 @@
 #include<bits/stdc++.h>
 #define int long long int
 using namespace std;
+
 int32_t main(){
-  int n;
+	int n;
   
-  cout<<"Enter Number of Elements : ";
-  cin>>n;
+	cin>>n;
   
-  vector<int> input(n),output(n,-1);
+	vector<int> input(n),output(n,-1);
   
-  cout<<"Enter "<<n<<" Elements : ";
-  for(int i=0;i<n;i++)
-    cin>>input[i];
-    
-  int maxi = input[n-1];
-  
-  for(int i=n-2;i>=0;i--){
-    if(maxi>input[i])
-      output[i]=maxi;
-    maxi = max(maxi,output[i]);
-  }
-  
-  for(int i=0;i<n;i++)
-    cout<<output[i]<<" ";
-  cout<<endl;
-    
+	for(int i=0;i<n;i++)
+		cin>>input[i];
+		
+	stack<int> s;
+	for(int i=n-1;i>=0;i--){
+		
+		while(!s.empty()&&s.top()<=input[i])
+			s.pop();
+			
+		if(!s.empty())
+			output[i]=s.top();
+			
+		s.push(input[i]);
+	}
+	for(int i=0;i<n;i++)
+		cout<<output[i]<<" ";
+	cout<<endl;
 }
