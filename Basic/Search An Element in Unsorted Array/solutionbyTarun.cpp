5dc5270d15cc572c65c3f8c4125902f3d4e1793e
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-10-02 10:04:57 +0530

    Linear Search

---
+++ Basic/Search An Element in Unsorted Array/solutionbyTarun.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int element,size;
+	cin >> size;
+	int arr[size];
+	for( int i = 0; i < size; i++ ){
+		cin >> arr[i];
+	}
+	cin >> element;
+
+	for( int i = 0; i < size; i++ ) {
+		if( arr[i] == element ){
+			cout << "YES" << endl;
+			return 0;
+		}
+	}
+	cout << "NO" << endl;
+	return 0;
+
+	
+}
