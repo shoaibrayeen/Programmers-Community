bd9e24634ea00433401e0ea4055f1b89794eef77
shwetachan <43998255+shwetachan@users.noreply.github.com>
2019-10-02 10:51:41 +0530

    using linear search method

---
+++ Basic/Search An Element in Unsorted Array/solutionByShweta.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i, num, n, c=0;
+	cout<<"Enter the array size : ";
+	cin>>n;
+  int arr[n];
+	cout<<"Enter Array Elements : ";
+	for(i=0; i<n; i++)
+	{
+		cin>>arr[i];
+	}
+	cout<<"Enter the element to be searched : ";
+	cin>>num;
+	for(i=0; i<n; i++)
+	{
+		if(arr[i]==num)
+		{
+			c=1;
+			pos=i+1;
+			break;
+		}
+	}
+	if(c==0)
+	{
+		cout<<"NO";
+	}
+	else
+	{
+		cout<<"YES";
+	}
+	return 0;
+}
