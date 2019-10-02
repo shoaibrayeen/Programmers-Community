01d14edff83dd381a4219a032da29f1f77b118ad
Priyansh Verma <33251160+PriyanshVerma@users.noreply.github.com>
2019-10-03 00:43:57 +0530

    SolutionByPriyanshVerma.cpp
    added zig zag array function code in C++

---
+++ Data Structure/Array Or Vector/Convert Array into Zig-Zag Fashion/SolutionByPriyanshVerma.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// function to convert given array/vector of integers into zigZag format
+// performs in O(n) time complexity
+vector<int> zigZag(vector<int> &arr, int n) {
+    for (int i = 1; i < n; i += 2)
+    {
+        if (i - 1 >= 0 && arr[i - 1] > arr[i])
+            swap(arr[i - 1], arr[i]);
+
+        if (i + 1 < n && arr[i + 1] > arr[i])
+            swap(arr[i + 1], arr[i]);
+    }
+
+    return arr;
+}
+
+int main() {
+    int n; cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+
+    arr = zigZag(arr, n);
+
+    for (int i = 0 ; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
