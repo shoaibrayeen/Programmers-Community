d31a9716ef1e08e6c936c736de8afb7221ab281a
Sumit <29905772+isumit19@users.noreply.github.com>
2019-10-04 01:12:51 +0530

    Create SoultionBySumit.cpp

---
+++ Data Structure/Array Or Vector/Arrange Given Numbers to Form the Biggest Number/SoultionBySumit.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool cmp(int a, int b){
+	
+	string num1 = to_string(a) + to_string(b);
+	string num2 = to_string(b) + to_string(a);
+	
+	return num1>num2;
+}
+vector<int> MaxNumberByJoin(vector<int> input, int n){
+	
+	sort(input.begin(), input.end(),cmp);
+	return input;
+	
+}
+
+int main()
+{    
+    int n;
+    cin>>n;
+    vector<int> input(n);
+    for(int i=0;i<n;i++)
+    	cin>>input[i];
+    	
+    vector<int> output = MaxNumberByJoin(input, n);
+
+	  for(int i=0;i<n;i++)
+		  cout<<output[i];
+	  cout<<endl;
+
+    return 0;
+}
