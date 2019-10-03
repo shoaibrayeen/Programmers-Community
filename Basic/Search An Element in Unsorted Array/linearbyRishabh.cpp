b9fc922ff71c100338e0a577809f8b8a4b914c6e
Rishabh Sharma <48138906+rishiar4@users.noreply.github.com>
2019-10-04 03:27:27 +0530

    linear search by Rishabh

---
+++ Basic/Search An Element in Unsorted Array/linearbyRishabh.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+bool linearSearch(int *a,int n,int val)
+{
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]==val)
+			return true;
+	}
+	return false;
+
+}
+int main()
+{
+	int n; 
+	cin>>n;      //size of array
+	int *a=new int[n];    
+
+	for(int i=0;i<n;i++)
+      	{
+            cin>>a[i];
+	    } 
+
+	int val;
+	cin>>val;      //value to be searched
+
+	int l=linearSearch(a,n,val);
+
+	if(l)               //true
+		cout<<"Yes";
+	else 
+		cout<<"No"; 
+
+
+}
