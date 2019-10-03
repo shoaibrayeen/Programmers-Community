3eab91f21fe0752c698d69c6ba91f24983d84ba4
sanyakataria <52246974+sanyakataria@users.noreply.github.com>
2019-10-03 13:02:13 +0530

    Update SolutionBySanya.cpp

--- Basic/Reverse A String/SolutionBySanya.cpp
+++ Basic/Reverse A String/SolutionBySanya.cpp
@@ -3,19 +3,11 @@ using namespace std;
 
 int main()
 {
-    char str[1000],temp;
-    int n,i;
-    cout<<"Enter a String : ";
-    gets(str);
-    n = strlen(str)-1;
-    int j=0;
-    while(j<n){
-        temp = str[j];
-        str[j] = str[n];
-        str[n] = temp;
-        j++;
-        n--;
-    }
-    puts(str);
+    string str;
+    cin>>str;
+    reverse(str.begin(), str.end());
+
+    cout << str;
     return 0;
 }
+
