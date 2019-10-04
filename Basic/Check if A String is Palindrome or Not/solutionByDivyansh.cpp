9f35ea0d2dda59be178a9745b3a276049873393d
Divyansh747 <divyansh.1999.dr@gmail.com>
2019-10-05 01:20:15 +0530

    Create solutionByDivyansh.cpp

---
+++ Basic/Check if A String is Palindrome or Not/solutionByDivyansh.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+
+int left=0,right,flag=0;
+char str[1000];
+
+cout<<"Enter String to check palindrome."<<endl;
+cin>>str;
+
+right=strlen(str)-1; 
+  
+while(right>left) 
+{ 
+if(str[left++]!=str[right--]) 
+{ 
+flag=0;
+} 
+else{ 
+flag=1;
+}
+}
+
+if(flag==1){
+cout<<str<<" is palindrome"<<endl;
+}else{
+cout<<str<<" is not palindrome"<<endl; 
+}
+return 0;
+}
+
