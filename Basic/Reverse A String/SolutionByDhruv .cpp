3a7b77f13ae13fabe0d8541df4d1de92ca851a34
Dhruv <42606683+dA505819@users.noreply.github.com>
2019-10-05 02:10:54 +0530

    Update

--- Basic/Reverse A String/SolutionByDhruv .cpp
+++ Basic/Reverse A String/SolutionByDhruv .cpp
@@ -1,12 +1,24 @@
 #include<iostream.h>
 #include<conio.h>
-#include<string.h>
+#include<string.h>              //To use string functions
 void main()
 {
+ char f[5];
  char c="India";
+ f=reverse (c);
+ for(int i=0;i<strlen(c);i++)
+  {
+    puts(f[i]);                //To display the reversed string
+     }
+  getch();
+}
+
+char reverse(char &c)          //Function to reverse the string
+{
+ char d[10];
  for(int i=string.length(c)-1;i>=0;i--)
  {
-   puts(i);
+   gets(d[i]);                 //Take input of reversed string
   }
- getch();
+  return d;
 }
