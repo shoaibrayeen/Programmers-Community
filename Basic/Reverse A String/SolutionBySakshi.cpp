ec405af11b55592b9ab4e27f8a2d4b137d0e487b
sakshiii <33049180+sakshiii@users.noreply.github.com>
2019-10-05 00:44:47 +0530

    Create SolutionBySakshi.cpp

---
+++ Basic/Reverse A String/SolutionBySakshi.cpp
@@ -0,0 +1,23 @@
+#include<iostream.h>
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	clrscr();
+	char str[100], temp;
+	int i=0, j;
+	cout<<"Enter the String : ";
+	gets(str);
+	j=strlen(str)-1;
+	while(i<j)
+	{
+		temp=str[i];
+		str[i]=str[j];
+		str[j]=temp;
+		i++;
+		j--;
+	}
+	cout<<"Reverse of the String = "<<str;
+	getch();
+}
