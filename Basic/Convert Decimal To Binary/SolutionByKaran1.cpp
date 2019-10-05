45875de2658fc70c21286348c57acd700896f6e1
Karansingh005 <41364215+Karansingh005@users.noreply.github.com>
2019-10-05 12:00:30 +0530

    Create SolutionByKaran1.cpp

---
+++ Basic/Convert Decimal To Binary/SolutionByKaran1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void dec_to_bin(int n){
+	int binary[32];
+	
+	int i=0;
+	while(n>0){
+		binary[i]=n%2;
+		n=n/2;
+		i++;
+	}
+	for(int j=i-1;j>=0;j--){
+		cout<<binary[j];
+	}
+}
+int main() {
+	// your code goes here
+	int a;
+	cout<<"Enter a decimal number: ";
+	cin>>a;
+	dec_to_bin(a);
+	return 0;
+}
