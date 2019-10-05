65ca85bf7407aee63d9542676f961b45c98f4dca
Prince Garg <54926566+gargprince1702@users.noreply.github.com>
2019-10-04 23:01:40 -0700

    ReverseAString

---
+++ Basic/Reverse A String/solutionbygargprince1702.cpp
@@ -0,0 +1,19 @@
+// A quickly written program for reversing a string 
+// using reverse() 
+#include <bits/stdc++.h> 
+using namespace std; 
+int main() 
+{ 
+	string str = "gargprince1702"; 
+
+	// Reverse str[beign..end] 
+	reverse(str.begin(), str.end()); 
+
+	cout << str; 
+	return 0; 
+} 
+//output
+//2071ecnirpgrag
+//--------------------------------
+//Process exited after 2.994 seconds with return value 0
+//Press any key to continue . . .
