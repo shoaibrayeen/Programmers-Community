b6c7cd6190f9687ffb88ed9272ab5e9dda145742
PIYUSH KHURANA <52041719+PIYUSH-01@users.noreply.github.com>
2019-10-06 08:22:34 +0530

    Update SolutionByPiyush.cpp

--- Basic/Implement Power Function/SolutionByPiyush.cpp
+++ Basic/Implement Power Function/SolutionByPiyush.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int raise_to_power(int number, int power)
+float raise_to_power(int number, int power) //function to make find the final value after raising the number to the given power.
 {
-    int result = 1;
+    int result;
     result = number;
-    for(int i = 2; i <= power; i++)
+    
+    if(power > 0)// CASE 1: Power is greater than 0.
     {
-        result = result * number;
+        for(int i = 2; i <= power; i++)
+        {
+            result = result * number;
+        }
+        return result;
     }
-    return result;
+    else if(power < 0)// CASE 2: power is less than 0.
+    {
+        float result_negative_power= 1.0;//A float varibale to store the fraction values for negative powers
+        for(int i = 2; i<= -(power); i++)
+        {
+            result = result * number;
+        }
+        result_negative_power = 1.0 / result; //calculating the result_negative_power by dividing the result by 1 in order to change it to the result for a negative power.
+        return result_negative_power;
+    }
+    else// CASE 3: Power isequal to zero
+    {
+        return 1; //returning 1 because any number raised to the power 0 is 1.
+    }
 }
 
 int main()
@@ -20,7 +38,6 @@ int main()
     cout<<"\nEnter the power to which you want to raise the number:";
     cin>>power;
     
-    cout<<raise_to_power(number, power);
-    
+    cout<<raise_to_power(number, power); // Calling the function to calculate the result and then printing the value that is returned by the function.
     return 0;
 }
