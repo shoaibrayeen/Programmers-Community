e78248c20d8beff82f09a09f63e77875101e3d0b
shwetachan <43998255+shwetachan@users.noreply.github.com>
2019-10-06 14:41:00 +0530

    did the required operation using functions

--- Basic/Search An Element in Sorted Array/solutionByShweta.cpp
+++ Basic/Search An Element in Sorted Array/solutionByShweta.cpp
@@ -1,44 +1,37 @@
 #include<iostream>
 using namespace std;
-
-int main()
-{
-
-	int n, i, s, first, last, middle;
-	cout<<"Enter total number of elements :";
-	cin>>n;
-  int arr[n];
-	cout<<"Enter "<<n<<" number in ascending order :";
-	for (i=0; i<n; i++)
-	{
-		cin>>arr[i];
-	}
-	cout<<"Enter a number to find :";
-	cin>>s;
+void binarysearch(int arr[], int n, int s){
+	int first, last, middle;
 	first = 0;
 	last = n-1;
 	middle = (first+last)/2;
 	while (first <= last)
 	{
 		if(arr[middle] < s)
-		{
 			first = middle + 1;
-
-		}
 		else if(arr[middle] == s)
 		{
 			cout<<"YES";
 			break;
 		}
 		else
-		{
 			 last = middle - 1;
-		}
 		middle = (first + last)/2;
 	}
 	if(first > last)
-	{
 		cout<<"NO";
-	}
+}
+int main()
+{
+	int n, i, s;
+	cout<<"Enter total number of elements :";
+	cin>>n;
+  	int arr[n];
+	cout<<"Enter "<<n<<" number in ascending order :";
+	for (i=0; i<n; i++)
+		cin>>arr[i];
+	cout<<"Enter a number to find :";
+	cin>>s;
+	binarysearch(arr,n,s);
 	return 0;
 }
