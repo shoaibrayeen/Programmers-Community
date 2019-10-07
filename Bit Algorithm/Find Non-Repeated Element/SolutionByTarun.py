7004a93f16db449cd54dc5425e5c8bef461c631c
Tarun Bansal <tarun.bansal6199@gmail.com>
2019-10-08 00:52:24 +0530

    Create SolutionByTarun.py

---
+++ Bit Algorithm/Find Non-Repeated Element/SolutionByTarun.py
@@ -0,0 +1,19 @@
+def firstNR(arr, n): 
+  
+	for i in range(n): 
+		j = 0
+		while(j < n): 
+			if (i != j and arr[i] == arr[j]): 
+				break
+			j += 1
+		if (j == n): 
+			return arr[i] 
+	
+	return -1
+	
+# Driver code
+n=raw_input('Enter size : ')
+arr=[]
+for i in range(0,n):
+  arr.append(input('enter element'))
+print(firstNR(arr, n))
