1edaeb79b866260f50099969a31a2d8bd1d3fe46
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-09 00:42:22 +0530

    SolutionByVyomChandra.java

---
+++ Basic/Convert Binary to Decimal/SolByVyomChandra.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class SolByVyomChandra { 
+	static int binaryToDecimal(int n) 
+	{ 
+		int num = n; 
+		int dec_value = 0; 
+		int base = 1; 
+		int temp = num; 
+		while (temp > 0) { 
+			int last_digit = temp % 10; 
+			temp = temp / 10; 
+      dec_value += last_digit * base; 
+      base = base * 2; 
+		} 
+		return dec_value; 
+	} 
+	public static void main(String[] args) 
+	{ 
+    Scanner sc=new Scanner(System.in);
+		int num = sc.nextInt(); 
+		System.out.println(binaryToDecimal(num)); 
+	} 
+} 
