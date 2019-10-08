2f6e0339d6c1488986fd6589ba8c8d173a4b062e
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-10-08 09:47:38 +0530

    Iterative Naive Approach

---
+++ Basic/Implement Power Function/solutionByKaran.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int power(int n, int p) {
+	int temp = 1;
+	if(p ==0 ) {
+		return 1;
+	}
+	else if(p > 0) {
+		for(int i = 1; i <= p; i++ ){
+			temp = temp * n;
+		}
+		return temp;
+	}
+	else {
+		for(int i = 1; i <= p; i++ ){
+			temp = temp * n;
+		}
+		return 1/temp;
+	}
+}
+int main() {
+	int a, p;
+	cout << "Enter the number: "
+	cin >> a;
+	cout << "Enter the power: "
+	cin >> p;
+	cout << power( a, p);
+	return 0;
+}
