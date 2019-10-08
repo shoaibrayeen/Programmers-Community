2f46a0011938a2efd6d87efc31152bc43f370be1
Isha Kansal <42314217+19isha98@users.noreply.github.com>
2019-10-08 23:23:20 +0530

    Update SolutionByIshaKansal.cpp
    Running for all cases

--- Data Structure/Matrix/Print A Given Matrix in Spiral Form/SolutionByIshaKansal.cpp
+++ Data Structure/Matrix/Print A Given Matrix in Spiral Form/SolutionByIshaKansal.cpp
@@ -6,58 +6,82 @@ void spiralprint(vector<vector<int> >&arr)
 {
 int rs=0,cs=0,re=arr.size()-1,ce=arr[0].size()-1,count=arr.size()*arr[0].size();
 //count is total number of elements
-while(count>=0)
+while(count>0)
 {
-for(int i=rs,j=cs;j<=ce && count>=0;j++)
+for(int i=rs,j=cs;count>0 && j<=ce ;j++)
 {
     cout<<arr[i][j]<<" ";
     count--;
 }
 rs++;
-
-for(int j=ce,i=rs;i<=re && count>=0;i++)
+if(count<=0)
+{
+ break;
+}
+for(int j=ce,i=rs;count>0 && i<=re;i++)
 {
     cout<<arr[i][j]<<" ";
     count--;
 }
 ce--;
+if(count<=0)
+{
+ break;
+}
 
-for(int i=re,j=ce;j>=cs && count>=0;j--)
+for(int i=re,j=ce;count>0 && j>=cs ;j--)
 {
     cout<<arr[i][j]<<" ";
     count--;
 }
 re--;
+if(count<=0)
+{
+ break;
+}
 
-for(int j=cs,i=re;i>=rs && count>=0;i--)
+for(int j=cs,i=re;count>0 && i>=rs;i--)
 {
     cout<<arr[i][j]<<" ";
     count--;
 }
 cs++;
+if(count<=0)
+{
+ break;
+}
 
 }
+ 
+ return;
 }
 void input(vector<vector<int> >&arr)
 {
     for(int i=0;i<arr.size();i++)
     {
+        cout<<"Enter data for row "<<i+1<<":";
         for(int j=0;j<arr[0].size();j++)
         {
            cin>>arr[i][j];
         }
+        cout<<endl;
     }
 }
 int main(int args,char** argv)
 {
-    int n;
+    int n,m;
+    cout<<"Enter row size:";
     cin>>n;
+     cout<<"Enter column size:";
+    cin>>m;
     //input size
-    cout<<"Enter size:";
-    vector<vector<int> >arr(n,vector<int>(n,0));
-    cout<<"Enter array:";
+    vector<vector<int> >arr(n,vector<int>(m,0));
+    cout<<"Enter array:"<<endl;
     input(arr);
     cout<<"Output: ";
     spiralprint(arr);
+     cout<<endl;
+    cout<<"Printed in spiral manner";
+   
 
 }
