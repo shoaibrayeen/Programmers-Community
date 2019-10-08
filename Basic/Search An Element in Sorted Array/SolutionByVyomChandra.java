da407265b272c0ec7165c9da9bb2b711720e0ffb
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-09 01:19:40 +0530

    class name changed

--- Basic/Search An Element in Sorted Array/SolutionByVyomChandra.java
+++ Basic/Search An Element in Sorted Array/SolutionByVyomChandra.java
@@ -1,4 +1,4 @@
-public class BinarySearch {
+public class SolutionByVyomChandra {
         public static void main(String[] args) {
         int arr[] = new int[]{10, 20, 30, 40, 50, 60, 70, 80};
         System.out.println(binarySearch(arr, 10));
