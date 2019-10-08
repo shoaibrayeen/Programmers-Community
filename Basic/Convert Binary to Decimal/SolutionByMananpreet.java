6ca8f3116a7b31d6f014d5771eb29d2f61fed4b1
Mananpreet Kaur <manankaur00@gmail.com>
2019-10-08 14:56:44 +0530

    Create SolutionByMananpreet.java

---
+++ Basic/Convert Binary to Decimal/SolutionByMananpreet.java
@@ -0,0 +1,33 @@
+
+/*
+The following java program converts the entered binary number into a decimal number.
+*/
+
+import java.util.Scanner;
+
+class BinaryToDecimal 
+{
+	public static int binaryToDecimal(int n)
+	{
+		int num = n;
+		int base = 1 ;// 2^0=1
+		int decimal =0;
+		int temp = 0;
+
+		while(num>0)
+		{
+		    temp= num%10;
+			num = num/10;
+			decimal = decimal + temp*base;
+			base*=2;
+		}
+		return decimal;
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter a binary number: ");
+		int binary = s.nextInt();
+		System.out.println("The decimal conversion of the inputed binary number "+binary+" : "+binaryToDecimal(binary));
+	}
+}
