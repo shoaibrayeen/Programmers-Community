b3cc1fa4415b361a39a939a3442b330d413a1108
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-09 01:02:47 +0530

    SolutionByVyomChandra.java

---
+++ Basic/Reverse A String/SolutionByVyomChandra.java
@@ -0,0 +1,15 @@
+ import java.lang.*; 
+import java.io.*; 
+import java.util.*;  
+class SolutionByVyomChandra
+{ 
+	public static void main(String[] args) 
+	{ 
+    Scanner sc=new Scanner(System.in);
+		String input =sc.nextInt(); 
+		StringBuilder input1 = new StringBuilder(); 
+    input1.append(input); 
+    input1 = input1.reverse(); 
+    System.out.println(input1); 
+	} 
+} 
