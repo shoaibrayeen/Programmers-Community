5ffd46932a4fd76ecebf6e33dc0de3abd3e3d7b8
Nidhi Verma <nidhiverma1997@gmail.com>
2019-10-09 19:33:36 +0530

    Create SolutionByNidhi.java
    Dynamic approach to calculate factorial of a number

---
+++ Basic/Calculate Factorial of A Number/SolutionByNidhi.java
@@ -0,0 +1,36 @@
+/*
+    Program Description: Program to calculate factorial of a number 
+ */
+import java.util.Scanner;
+
+public class Factorial {
+
+    // factorial array is maintained to store the value of already calculated factorials
+    private static long[] factorial;
+    /*
+        Method description: method to calculate factorial of a given integer
+     */
+    private static long computeFactorial(int num) {
+        if(num == 0 || num == 1) {
+            return 1;
+        }
+        // if factorial value is known
+        if(factorial[num] != 0) {
+            return factorial[num];
+        }
+        factorial[num] = num * computeFactorial(num-1);
+        return factorial[num];
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("N = ");
+        int num = scan.nextInt();
+        if(num < 0) {
+            System.out.println("Invalid input");
+            return;
+        }
+        factorial = new long[num+1];
+        factorial[0] = factorial[1] = 1;
+        System.out.println("Output = " + computeFactorial(num));
+    }
+}
