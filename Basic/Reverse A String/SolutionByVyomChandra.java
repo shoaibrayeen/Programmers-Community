419e14ea4e02579ae1a8842640386c7068a67d83
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-09 13:34:46 +0530

    update user input

--- Basic/Reverse A String/SolutionByVyomChandra.java
+++ Basic/Reverse A String/SolutionByVyomChandra.java
@@ -5,11 +5,12 @@ class SolutionByVyomChandra
 { 
 	public static void main(String[] args) 
 	{ 
-    Scanner sc=new Scanner(System.in);
+                Scanner sc=new Scanner(System.in);
+		System.out.print("Enter String: ");
 		String input =sc.nextInt(); 
 		StringBuilder input1 = new StringBuilder(); 
-    input1.append(input); 
-    input1 = input1.reverse(); 
-    System.out.println(input1); 
+                input1.append(input); 
+                input1 = input1.reverse(); 
+                System.out.println(input1); 
 	} 
 } 
