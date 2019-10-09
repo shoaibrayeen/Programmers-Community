a5b44f3b810d07aa86e9d75bc5745d4949d7c61c
ashutoshgoswami05 <40275857+ashutoshgoswami05@users.noreply.github.com>
2019-10-09 12:36:45 +0530

    Update solByAshutosh.py
    works for negative no as well as exponents

--- Basic/Implement Power Function/solByAshutosh.py
+++ Basic/Implement Power Function/solByAshutosh.py
@@ -1,8 +1,46 @@
 def power(n,x):
-    num=n
-    for i in range(0,x-1):
-        n*=num
+    
+    
+    if n>0: #positive no:
+        num=n
+        if x>0:
+            for i in range(0,x-1):
+                n*=num
+    
+        else:
+            x=-x
+            for i in range(0,x-1):
+                n*=num
+            n=1/n
+            
+    else:  #negative no:
+        num=-n
+        n=num
         
+        
+        if x>0:
+            if x%2==0:
+                for i in range(0,x-1):
+                    n*=num
+            elif x%2!=0:
+                for i in range(0,x-1):
+                    n*=num
+                n=-n
+                
+        else:
+           
+            x=-x
+            
+            if x%2==0:
+                for i in range(0,x-1):
+                    n*=num
+                n=1/n
+               
+            elif x%2!=0:
+                for i in range(0,x-1):
+                    n*=num
+                n=1/-n
+                
     return n
 
 a=int(input("enter your number:"))
