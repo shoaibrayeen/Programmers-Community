d8efc645f5543ee0ba4c230d644dc8fe9e863b99
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2019-10-10 00:39:33 +0530

    SolutionBySimarpreetSingh.cpp
    This program checks whether the string is palindrome or not (case sensitive condition)
    
    Co-Authored-By: Simarpreet Singh <simarpreetsingh.019@gmail.com>

--- Basic/Search An Element in Unsorted Array/solutionByMonika.cpp
+++ Basic/Search An Element in Unsorted Array/solutionByMonika.cpp
@@ -1,35 +1,27 @@
 #include<iostream>
 using namespace std;
-bool search1(int arr[],int size1,int k){
-    for(int i=0;i<size1;i++){
-        if(arr[i]==k){
-            return true;
-        }
-    }
-return false;
-}
+
 int main(){
     int arr[100];
-    int size1,k;       //k is the no. we need to search
+    int size1,k;      //k is the no. we need to search
 
- 
     cin>>size1;
-    cout<<endl;
-   
+
     for(int i=0;i<size1;i++){
         cin>>arr[i];
-     }
-
-   
+    }
+    
     cin>>k;
 
+
+    for(int i=0;i<size1;i++){
+        if(arr[i]==k){
+            cout<<"Yes";
+            return 0;
+        }
+    }
     
-    if(search1(arr,size1,k)){
-        cout<<"Yes";
-    }
-    else{
-        cout<<"No";
-    }
+  cout<<"No";
 
 return 0;
 }
