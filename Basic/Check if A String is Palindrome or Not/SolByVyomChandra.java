51779498d06c0ded3ad2b4ea6c623b57759332b3
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-09 13:37:17 +0530

    update user input

--- Basic/Check if A String is Palindrome or Not/SolByVyomChandra.java
+++ Basic/Check if A String is Palindrome or Not/SolByVyomChandra.java
@@ -14,7 +14,8 @@ public class SolByVyomChandra {
 	public static void main(String[] args) 
 	{ 
 		Scanner sc=new Scanner(System.in);
-    String str=sc.next(); 
+		System.out.print("Enter String: ");
+                 String str=sc.next(); 
 		if (isPalindrome(str)) 
 			System.out.print("Yes"); 
 		else
