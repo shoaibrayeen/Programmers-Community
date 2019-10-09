fbc2ff55206a7d37942eb77c04e3a1fb85752564
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-09 13:33:12 +0530

    Update user input

--- Basic/Search An Element in Sorted Array/SolutionByVyomChandra.java
+++ Basic/Search An Element in Sorted Array/SolutionByVyomChandra.java
@@ -2,12 +2,16 @@ import java.util.*;
 public class SolutionByVyomChandra {
         public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter size: ");
         int size=sc.nextInt();
+        System.out.print("Enter element: ");
         int a[]=new int[size];
         for(int i=0;i<size;i++) 
                 a[i]=sc.nextInt();
         Arrays.sort(a);
-        System.out.println(binarySearch(arr, 10));
+        System.out.print("Enter key: ");
+        int n=sc.nextInt();
+        System.out.println(binarySearch(arr, n));
     }
  private static boolean binarySearch(int arr[], int dataToSearch){
         if(arr==null || arr.length<0){
