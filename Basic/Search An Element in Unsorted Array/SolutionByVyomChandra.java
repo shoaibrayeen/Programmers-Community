6bd53fa8d2fbc3876ca337ee0bfaf69d678df138
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-09 13:20:57 +0530

    Update SolutionByVyomChandra.java
    user input added

--- Basic/Search An Element in Unsorted Array/SolutionByVyomChandra.java
+++ Basic/Search An Element in Unsorted Array/SolutionByVyomChandra.java
@@ -1,40 +1,21 @@
-import java.util.Arrays; 
+import java.util.*; 
 public class SolutionByVyomChandra
 { 
 	public static void main(String[] args) 
 	{ 
-		byte byteArr[] = {10,20,15,22,35}; 
-		char charArr[] = {'g','p','q','c','i'}; 
-		int intArr[] = {10,20,15,22,35}; 
-		double doubleArr[] = {10.2,15.1,2.2,3.5}; 
-		float floatArr[] = {10.2f,15.1f,2.2f,3.5f}; 
-		short shortArr[] = {10,20,15,22,35}; 
-
-		Arrays.sort(byteArr); 
-		Arrays.sort(charArr); 
-		Arrays.sort(intArr); 
-		Arrays.sort(doubleArr); 
-		Arrays.sort(floatArr); 
-		Arrays.sort(shortArr); 
-
-		byte byteKey = 35; 
-		char charKey = 'g'; 
-		int intKey = 22; 
-		double doubleKey = 1.5; 
-		float floatKey = 35; 
-		short shortKey = 5; 
-
-		System.out.println(byteKey + " found at index = "
-						+Arrays.binarySearch(byteArr,byteKey)); 
-		System.out.println(charKey + " found at index = "
-						+Arrays.binarySearch(charArr,charKey)); 
+ 
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Size: ");
+		int size=sc.nextInt();
+		System.out.print("Enter elements: ");
+		int intArr[] = new int[size];
+		for(int i=0;i<size;i++)
+			intArr[i]=sc.nextInt();
+		Arrays.sort(intArr);
+		System.out.print("Enter key: ");
+		int intKey = sc.nextInt(); 
 		System.out.println(intKey + " found at index = "
 						+Arrays.binarySearch(intArr,intKey)); 
-		System.out.println(doubleKey + " found at index = "
-						+Arrays.binarySearch(doubleArr,doubleKey)); 
-		System.out.println(floatKey + " found at index = "
-						+Arrays.binarySearch(floatArr,floatKey)); 
-		System.out.println(shortKey + " found at index = "
-						+Arrays.binarySearch(shortArr,shortKey)); 
+		
 	} 
 } 
