04ddfcadc8f915ac6fedd099b55b9565a1035884
EmpireMachine <56215651+EmpireMachine@users.noreply.github.com>
2019-10-09 16:14:03 +0530

    Update SolutionBySumitVerma.cpp

--- Basic/Search An Element in Unsorted Array/SolutionBySumitVerma.cpp
+++ Basic/Search An Element in Unsorted Array/SolutionBySumitVerma.cpp
@@ -1,19 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool search(int a[], int n, int e){
+	int i;
+	for(i=0;i<n;i++)
+		if(a[i]==e)
+			break;
+	if(i<n)
+		return true;
+	return false;
+}
 int main()
 {
 	int n;
 	cin>>n;
+	int a[n];
 	for(int i=0;i<n;i++)
 		cin>>a[i];
 	int ele,i;
 	cin>>ele;
-	for(i=0;i<n;i++)
-		if(a[i]==ele)
-			break;
-	if(i<n)
+	
+	if(search(a, n, ele))
 		cout<<"YES\n";
 	else
 		cout<<"NO\n";
+	
 	return 0;
 }
