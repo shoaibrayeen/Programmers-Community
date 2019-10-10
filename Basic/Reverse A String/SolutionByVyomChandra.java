944618c7d2a9c9e3dbc373e7a02753d33d9e36b5
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-10 15:29:07 +0530

    Update SolutionByVyomChandra.java

--- Basic/Reverse A String/SolutionByVyomChandra.java
+++ Basic/Reverse A String/SolutionByVyomChandra.java
@@ -7,10 +7,18 @@ class SolutionByVyomChandra
 	{ 
                 Scanner sc=new Scanner(System.in);
 		System.out.print("Enter String: ");
-		String input =sc.nextInt(); 
-		StringBuilder input1 = new StringBuilder(); 
-                input1.append(input); 
-                input1 = input1.reverse(); 
-                System.out.println(input1); 
+		String input =sc.next();
+		char[] temparray = input.toCharArray(); 
+                int left, right=0; 
+                right = temparray.length-1; 
+                for (left=0; left < right ; left++ ,right--) 
+                { 
+                 // Swap values of left and right 
+                 char temp = temparray[left]; 
+                 temparray[left] = temparray[right]; 
+                 temparray[right]=temp; 
+                 } 
+                for (char c : temparray) 
+                   System.out.print(c);
 	} 
 } 
