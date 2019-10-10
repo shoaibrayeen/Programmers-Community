47bccb7c4850568028bfe07d596782122711e510
Rishabh Bansal <42565455+therishh@users.noreply.github.com>
2019-10-11 00:41:41 +0530

    Create solutionbyRISHABHBANSAL.cpp

---
+++ Basic/Search An Element in Sorted Array/solutionbyRISHABHBANSAL.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+string findelement()
+{  
+  int size;
+  cout<<"enter the size of array";
+  cin>>size;
+  int A[size];
+  
+  for(int i=0;i<size;i++)
+{
+    cout<<"enter elemnts in a sorted way!";
+	cin>>A[i];	
+}
+  int element;
+  cout<<"enter the elemnt you want to found";
+  cin>>element;
+  int count = 0; 
+  
+  for(int i=0;i<size;i++)
+ {
+    if(A[i]==element)
+	  
+	   {
+	    count = 1;
+      } 
+ }
+	  if(count == 1)
+	 {
+	 	return "element found";
+	 }
+	      else 
+	           return "elemnet not found" ;	
+ }	
+	
+int main()
+{
+		cout<<findelement();		
+}
