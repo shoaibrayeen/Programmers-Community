074c218c3457b6986514bd65e85515e15425783d
ramneekkaur <ramneekkaur.rajindernagar@gmail.com>
2019-10-11 22:57:13 +0530

    power function - Solution By Ramneek

---
+++ Basic/Implement Power Function/SolutionByRamneek.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include <cstdlib>
+
+using namespace std;
+
+float power(int n, int p)
+{
+    int i,x;
+    float pr,c=1.0;
+    x=abs(p);
+    for( i=1;i<=x;i++)
+        {
+          c=c*n;
+        }
+    if(p>=0)
+    {
+      pr=c;
+    }
+
+    if(p<0)
+    {
+       pr= 1/c;
+    }
+return pr;
+}
+
+int main()
+{
+    int num,pow;
+    cout<<"Enter number and power\n";
+    cin>>num>>pow;
+
+    cout<<"power : "<<power(num,pow);
+
+return 0;
+}
