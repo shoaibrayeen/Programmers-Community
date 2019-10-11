13160748bf611463f169da1f7e0fee0d0b50dccb
Rishabh <rishabhsethia16@gmail.com>
2019-10-11 10:51:55 +0530

    Palindrome Solution by Rishabh Sethia

---
+++ Basic/Check if A String is Palindrome or Not/SolutionByRishabhSethia.py
@@ -0,0 +1,9 @@
+a = input()#User Input the String
+temp=""#Temporary Variable
+for i in range(len(a)-1, -1, -1): #for loop to iterate string
+    temp = temp + a[i] #concatinating sliced string
+if(temp == a): #Comparing both the strings
+    print("YES")
+else:
+    print("NO")
+
