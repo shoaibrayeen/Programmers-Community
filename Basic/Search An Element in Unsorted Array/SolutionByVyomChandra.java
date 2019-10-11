c228a13154901ec8d0674b9830d2c5225ea7fee1
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-11 11:14:11 +0530

    Update SolutionByVyomChandra.java

--- Basic/Search An Element in Unsorted Array/SolutionByVyomChandra.java
+++ Basic/Search An Element in Unsorted Array/SolutionByVyomChandra.java
@@ -3,21 +3,26 @@ public class SolutionByVyomChandra
 { 
 	public static void main(String[] args) 
 	{ 
- 
 		Scanner sc=new Scanner(System.in);
 		System.out.print("Enter Size: ");
 		int size=sc.nextInt();
 		System.out.print("Enter elements: ");
 		int intArr[] = new int[size];
 		for(int i=0;i<size;i++)
+		{
 			intArr[i]=sc.nextInt();
+		}	
 		Arrays.sort(intArr);
 		System.out.print("Enter key: ");
 		int intKey = sc.nextInt(); 
-		if(Arrays.binarySearch(intArr,intKey)>=0)
+		if(Arrays.binarySearch(intArr,intKey)>=0){
 			System.out.println(intKey + " found at index = "+Arrays.binarySearch(intArr,intKey)); 
+		}	
 		else
+		{
 			System.out.println("not found");
+		}
+			
 		
 	} 
 } 
