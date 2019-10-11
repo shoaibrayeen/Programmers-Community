f6659313612c1c56397b2a374a62bf85b57868c3
Bhavnoor_Singh <49763265+Bhavnoor-Singh98@users.noreply.github.com>
2019-10-12 00:21:11 +0530

    SolutionByBhavnoor.py
    It is being used to find the power of a number.

--- Basic/Implement Power Function/SolutionByBhavnoor.py
+++ Basic/Implement Power Function/SolutionByBhavnoor.py
@@ -1,3 +1,27 @@
-x=int(input("Enter value of base:"))
-y=int(input("Enter value of exponent:"))
-print("Final value will be:",x**y)
+def power(x, y): 
+  
+    #Let Initialize result 
+    res = 1
+      
+    while (y > 0): 
+          
+        # If y is odd, multiply 
+        # x with result 
+        if ((y & 1) == 1) : 
+            res = res * x 
+  
+        # n must be even  
+        # now y = y/2 
+        y = y >> 1
+          
+        # Change x to x^2 
+        x = x * x 
+      
+    return res 
+  
+  
+
+x = int(input("Enter a No:")) #Base 
+y = int(input("Enter a No:")) #Exponent
+  
+print("Powered No:", power(x, y))
