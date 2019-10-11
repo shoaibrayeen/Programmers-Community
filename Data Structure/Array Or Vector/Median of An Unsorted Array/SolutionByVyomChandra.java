18012b09626e85f1283cf940288cdf2e6b08da68
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-11 13:26:14 +0530

    SolutionByVyomChandra.java

---
+++ Data Structure/Array Or Vector/Median of An Unsorted Array/SolutionByVyomChandra.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class SolutionByVyomChandra{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter size of array: ");
+    int size=sc.nextInt();
+    int[] a=new int[size];
+    System.out.print("Enter Elements: ");
+    for(int i=0;i<size;i++)
+        a[i]=sc.nextInt();
+    System.out.println("Median is "+Median(a));
+  }
+  public static double Median(int[] arr)
+  {
+    Arrays.sort(arr);
+    int n=arr.length;
+    if(n%2=!0)
+    return  (double)arr[n / 2];
+    else
+    return (double)(arr[(n - 1) / 2] + arr[n / 2]) / 2.0;
+  }
+}
