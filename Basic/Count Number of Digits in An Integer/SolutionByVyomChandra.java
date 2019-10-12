6b6552952ec1e7cd89c51ea97d7853d56fa2e08f
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-12 14:23:15 +0530

    SolutionByVyomChandra.java

---
+++ Basic/Count Number of Digits in An Integer/SolutionByVyomChandra.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class SolutionByVyomChandra{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter a number : ");
+    long l=sc.nextLong();
+    int count=0;
+    while(l>0){
+      count++;
+      l=l/10; 
+    }
+    System.out.print(count);
+  }
+}
