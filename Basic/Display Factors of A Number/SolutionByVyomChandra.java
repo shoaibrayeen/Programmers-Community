3f779bb93867bd34feaa8824577267358216a0c8
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2019-10-12 14:26:55 +0530

    SolutionByVyomChandra.java

---
+++ Basic/Display Factors of A Number/SolutionByVyomChandra.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class SolutionByVyomChandra{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter a number : ");
+    long l=sc.nextLong();
+    for(long i=1;i<=l;i++){
+      if(l%i==0)
+        System.out.print(i+" ");
+    }
+  }
+}
