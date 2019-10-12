02d45ee6df9adcb0ddaaaeed2faa94cd8d461034
Karansingh005 <41364215+Karansingh005@users.noreply.github.com>
2019-10-12 23:37:40 +0530

    Create SolutionByKaran.cpp
    Program to calculate the factors of a given number.

---
+++ Basic/Display Factors of A Number/SolutionByKaran.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+void display_factors(long n)
+{
+	
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<i;
+            if(i!=n)
+            {
+                cout<<" , ";
+            }
+        }
+    }
+    cout<<"\n";
+}
+int main() 
+{	
+    long a;
+    cout<<"Enter the number: \n";
+    cin>>a;
+    cout<<"The factors are: ";
+    display_factors(a);
+    return 0;
+}
