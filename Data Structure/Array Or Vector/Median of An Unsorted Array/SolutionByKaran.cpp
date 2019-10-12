e771b182316f49f7bcee304ff5017ddf0e658e3d
Karansingh005 <41364215+Karansingh005@users.noreply.github.com>
2019-10-12 12:43:09 +0530

    Update SolutionByKaran.cpp
    Correction for the case of even number of elements in the array.

--- Data Structure/Array Or Vector/Median of An Unsorted Array/SolutionByKaran.cpp
+++ Data Structure/Array Or Vector/Median of An Unsorted Array/SolutionByKaran.cpp
@@ -2,43 +2,43 @@
 using namespace std;
 void median(int a[],int size)
 {
-    float avg;
-    int g=size-1;
-    for(int i=0;i<size-1;i++)
-    {
-        for(int j=0;j<size-i-1;j++)
+        float avg;
+        int g=size-1;
+        for(int i=0;i<size-1;i++)
         {
-            if(a[j]>a[j+1])
-            {
-                int temp = a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-            }
+                for(int j=0;j<size-i-1;j++)
+                {
+                        if(a[j]>a[j+1])
+                        {
+                                int temp = a[j];
+                                a[j]=a[j+1];
+                                a[j+1]=temp;
+                        }
+                }
         }
-    }
-    if(size%2!=0)
-    {
-        cout<<a[(g+1)/2];
-    }
-    else
-    {
+        if(size%2!=0)
+        {
+                cout<<a[(g+1)/2];
+        }
+        else
+        {
 
-          avg=(a[g/2]+ a[(g+1)/2])/2;
-          cout<<avg;
+                avg=float((a[g/2]+ a[(g+2)/2])/2.0);
+                cout<<avg;
 
-    }
+        }
 }
 int main() 
 {
-    int s;
-    cout<<"Enter the size of array: \n";
-    cin>>s;
-    int b[s];
-    cout<<"Enter the array: \n";
-    for(int k=0;k<s;k++)
-    {
-        cin>>b[k];
-    }
-    median(b,s);
-    return 0;
+        int s;
+        cout<<"Enter the size of array: \n";
+        cin>>s;
+        int b[s];
+        cout<<"Enter the array: \n";
+        for(int k=0;k<s;k++)
+        {
+                cin>>b[k];
+        }
+        median(b,s);
+        return 0;
 }
