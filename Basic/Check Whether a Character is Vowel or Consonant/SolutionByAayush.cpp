f4acf849b8354fd1cdc0b64710397c6256b0e94f
AAYUSH KHURANA <31439631+Aayushk333@users.noreply.github.com>
2019-10-13 04:05:55 +0530

    Create SolutionByAayush.cpp

---
+++ Basic/Check Whether a Character is Vowel or Consonant/SolutionByAayush.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream> 
+using namespace std; 
+  
+// Function to check whether a character is 
+// vowel or not 
+
+void vowelOrConsonant(char c) 
+{ 
+    if (c == 'a' || c == 'e' || c == 'i' ||  
+        c == 'o' || c == 'u' || c == 'A' ||  
+        c == 'E' || c == 'I' || c == 'O' || c == 'U') 
+        cout << "Vowel" << endl; 
+    else
+        cout << "Consonant" << endl; 
+} 
+  
+  
+int main() 
+{ 
+    char c;
+    cout<<"Enter the character"<<endl;
+    cin>>c;
+    vowelOrConsonant(c); 
+    return 0; 
+} 
