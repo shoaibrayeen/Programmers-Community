e8078ea22a1db532a823f5e54417b7de5ea77cc5
Ankit Vashisht <40850169+ankitvashisht12@users.noreply.github.com>
2019-10-13 11:35:25 +0530

    SolutionByAnkit.py
    Added Solution to count number of digits in an integer

---
+++ Basic/Count Number of Digits in An Integer/SolutionByAnkit.py
@@ -0,0 +1,13 @@
+'''
+     Program Description: Program to count number of digits in an integer.
+'''
+
+def count_digits(n):
+    if n > 0:
+        str_n = str(n)  # converting int to string 
+        return len(str_n)
+
+print('Enter an integer =', end=' ')
+N = int(input())
+print('Output =', count_digits(N))
+
