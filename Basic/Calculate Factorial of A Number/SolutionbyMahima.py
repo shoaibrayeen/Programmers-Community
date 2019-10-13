6c3b973637537f9bd8c8dd9c4c478f8b81f0134a
MAHIMA01 <43912375+MAHIMA01@users.noreply.github.com>
2019-10-14 01:28:52 +0530

    Update SolutionbyMahima.py

--- Basic/Calculate Factorial of A Number/SolutionbyMahima.py
+++ Basic/Calculate Factorial of A Number/SolutionbyMahima.py
@@ -1,9 +1,12 @@
 # Program to calculate Factorial of a Number.
 
 N = int(input("N = "))
-f = 1
 
-for i in range(1,N+1):
-	f *= i
+def factorial(N):
+	f = 1
+	for i in range(1,N+1):
+		f *= i
 
-print(f)
+	print(f)
+	
+factorial(N)
