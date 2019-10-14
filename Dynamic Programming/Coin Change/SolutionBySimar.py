333d962dfe534835a59f78b582948f59f95b770a
Simar Bedi <31388727+simarbedi@users.noreply.github.com>
2019-10-15 03:56:04 +0530

    Create SolutionBySimar.py
    the solution to the given DP problem using python3

---
+++ Dynamic Programming/Coin Change/SolutionBySimar.py
@@ -0,0 +1,22 @@
+ def count(S, m, n): 
+    table = [[0 for x in range(m)] for x in range(n+1)] 
+
+    for i in range(m): 
+        table[0][i] = 1
+
+    for i in range(1, n+1): 
+        for j in range(m): 
+
+            x = table[i - S[j]][j] if i-S[j] >= 0 else 0
+
+            y = table[i][j-1] if j >= 1 else 0
+
+            table[i][j] = x + y 
+
+    return table[n][m-1] 
+
+n=int(input())
+s = input()
+arr = list(map(int, s.split()))
+m = len(arr) 
+print(count(arr, m, n)) 
