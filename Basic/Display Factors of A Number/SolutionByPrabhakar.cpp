5bbe5c2cf2c67a92d74dd65c5ca32b8999aebb58
wick3ds0ul <48680692+wick3ds0ul@users.noreply.github.com>
2019-10-14 21:30:34 +0530

    SolutionByPrabhakar.cpp

---
+++ Basic/Display Factors of A Number/SolutionByPrabhakar.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int factors(int a)
+{
+int i=1;
+if(a>0)
+{
+while(a!=0)
+{
+    if(a%i==0)
+    {   if(i!=a)
+        {
+            cout<<i;
+            cout<<",";
+        }
+        else
+        {
+            cout<<i;
+        }
+    }
+    i++;
+}
+}
+}
+int main()
+{
+int p;
+cin>>p;
+factors(p);
+return 0;
+}
