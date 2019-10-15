da59503d432709f38ed6e0dc9b0d856f0a92f168
wick3ds0ul <48680692+wick3ds0ul@users.noreply.github.com>
2019-10-15 22:27:22 +0530

    SolutionByPrabhakar.cpp

---
+++ Bit Algorithm/Check if Two Integers Have Opposite Signs/SolutionByPrabhakar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+void isSame(signed int x,signed int y)
+{
+if((y>0&&x<0)||(y<0&&x>0))
+    cout<<"Yes";
+else
+    cout<<"No.";
+}
+int main()
+{
+ signed int a,b;
+ cin>>a;
+ cin>>b;
+isSame(a,b);
+    return 0;
+}
