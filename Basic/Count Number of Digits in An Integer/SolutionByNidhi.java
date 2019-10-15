65b32efab2ac7543ea7de17170ed1df0d368ffaa
Nidhi Verma <nidhiverma1997@gmail.com>
2019-10-15 22:49:43 +0530

    Create SolutionByNidhi.java
    Program to count the number of digits in a number

---
+++ Basic/Count Number of Digits in An Integer/SolutionByNidhi.java
@@ -0,0 +1,22 @@
+/*
+    Program Description: Program to count no. of digits in a given number
+ */
+import java.util.Scanner;
+
+public class CountDigits {
+    private static long countDigits(String number){
+        return number.length();
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String number = scan.next();
+        try{
+            Long.parseLong(number);    
+        }catch (Exception e) {
+            System.out.println("Invalid Input");
+            return;
+        }
+        System.out.println("Output = " + countDigits(number));
+    }
+}
