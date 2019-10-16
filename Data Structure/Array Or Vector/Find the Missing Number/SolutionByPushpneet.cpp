7a021aadc2c4f444d7ba595474d091b8c9895c52
Pushpneet Singh <40698097+PushpneetSingh@users.noreply.github.com>
2019-10-16 21:12:50 +0530

    Update SolutionByPushpneet.cpp

--- Data Structure/Array Or Vector/Find the Missing Number/SolutionByPushpneet.cpp
+++ Data Structure/Array Or Vector/Find the Missing Number/SolutionByPushpneet.cpp
@@ -2,23 +2,37 @@
 
 using namespace std;
 
+int sumByFormula(int n)
+{
+    int sum=0;
+    sum=((n+1)*(n+2))/2;
+    return sum;
+}
+
+int sumByAdding(int a[],int n)
+{
+    int i,sum=0;
+    for(i=0;i<n;i++)
+        sum=sum+a[i];
+    return sum;
+}
 int main()
 {
-    int n,a[100],i,sum1,sum2=0;
+    int n,i,sum1,sum2=0;
     cout<<"enter no. of terms";
     cin>>n;
+    int a[n];
 
     //input terms of an array
     for(i=0;i<n;i++)
         cin>>a[i];
 
     //sum by formula
-    sum1=((n+1)*(n+2))/2;
+    sum1=sumByFormula(n);
 
     //sum by adding
-    for(i=0;i<n;i++)
-        sum2=sum2+a[i];
+    sum2=sumByAdding(a,n);
 
-    cout<<"missing number "<<sum1-sum2;
+    cout<<"missing number = "<<sum1-sum2;
     return 0;
 }
