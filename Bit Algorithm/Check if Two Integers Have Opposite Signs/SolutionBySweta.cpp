70e3fe3daabfcfd963f11a4752dcfe1a891113d8
swetamittal <31439784+swetamittal@users.noreply.github.com>
2019-10-17 20:35:23 +0530

    Update SolutionBySweta.cpp

--- Bit Algorithm/Check if Two Integers Have Opposite Signs/SolutionBySweta.cpp
+++ Bit Algorithm/Check if Two Integers Have Opposite Signs/SolutionBySweta.cpp
@@ -1,20 +1,24 @@
 #include<stdbool.h>
 #include<stdio.h>
 #include<iostream>
+
 using namespace std;
 
-bool oppositeSigns(int x, int y)
+bool oppositeSigns(int a, int b)
 {
-return ((x ^ y) < 0);
+    return ((a ^ b) < 0);
 }
 
 int main()
 {
-int x , y ;
-cin>>x>>y;
-if (oppositeSigns(x, y) == true)
-printf ("Yes");
-else
-printf ("No");
-return 0;
+  int x , y ;
+  cout<<"Enter the two integers to be compared"<<endl;
+  cin>>x>>y;
+  if (oppositeSigns(x, y) == true)
+      printf ("Yes");
+  else
+      printf ("No");
+  
+  return 0;
+  
 } 
