0fc6482569027b0676bc54f67d43e8a261b5bc46
sakshi gaur <44137767+gaursakshi@users.noreply.github.com>
2019-10-17 19:16:36 +0530

    zigZagarray.cpp

---
+++ Data Structure/Array Or Vector/Convert Array into Zig-Zag Fashion/solutionbysakshi.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> zigZag(vector<int>&input)
+{
+    for(int i=0;i<input.size()-1;i++)
+    {
+        if(i%2==0 && input[i]>input[i+1])
+        {
+            int temp=input[i];
+            input[i]=input[i+1];
+            input[i+1]=temp;
+        }
+        else if(i%2!=0 && input[i]<input[i+1])
+        {
+             int temp=input[i];
+            input[i]=input[i+1];
+            input[i+1]=temp;    
+        }
+        
+    }
+    return input;
+}
+int main()
+{
+     int n;
+     cin>>n;
+     vector<int>input(n);
+     
+     for(int i=0;i<n;i++)
+     {
+         cin>>input[i];
+     }
+     
+      vector<int> output = zigZag(input);
+      for(int ele:input)
+     {
+         cout<<ele<<" ";
+     }
+     cout<<endl;
+     
+	return 0;
+}
