65d9e9cad12761da2a4f14db545f5fa33ce708e3
swetamittal <31439784+swetamittal@users.noreply.github.com>
2019-10-17 19:46:33 +0530

    Update SolutionBySweta.cpp

--- Data Structure/Linked List/Reverse A Singly Linked List/SolutionBySweta.cpp
+++ Data Structure/Linked List/Reverse A Singly Linked List/SolutionBySweta.cpp
@@ -1,73 +1,80 @@
 #include <iostream>
+
 using namespace std;
 
-struct Node {
-int data;
-struct Node* next;
-Node(int data)
+struct Node 
 {
-this->data = data;
-next = NULL;
-}
+  int data;
+  struct Node* next;
+  Node(int data)
+  {
+    this->data = data;
+    next = NULL;
+  }
 };
 
-struct LinkedList {
-Node* head;
-LinkedList()
+struct LinkedList 
 {
-head = NULL;
-}
+  Node* head;
+  LinkedList()
+  {
+    head = NULL;
+  }
+};
+
 
 Node* reverse(Node* head) //input to function is head of the linked list
 {
-if (head == NULL || head->next == NULL)
-return head;
+  
+  if (head == NULL || head->next == NULL)
+    return head;
 
-/* reverse the rest list and put
-the first element at the end */
-Node* rest = reverse(head->next);
-head->next->next = head;
-
-/* tricky step -- see the diagram */
-head->next = NULL;
-
-/* fix the head pointer */
-return rest;                  //returns head of the reversed list
+  /* reverse the rest list and put
+  the first element at the end */
+  Node* rest = reverse(head->next);
+  head->next->next = head;
+  head->next = NULL;
+  return rest;                  //returns head of the reversed list
 }
 
 
 void print()
 {
-struct Node* temp = head;
-while (temp != NULL) {
-cout << temp->data << " ";
-temp = temp->next;
-}
+  struct Node* temp = head;
+  while (temp != NULL) 
+  {
+    cout << temp->data << " ";
+    temp = temp->next;
+  }
 }
 
 void push(int data)
 {
-Node* temp = new Node(data);
-temp->next = head;
-head = temp;
+  Node* temp = new Node(data);
+  temp->next = head;
+  head = temp;
 }
-};
+
 
 int main()
 {
+  int ch;
+  LinkedList ll;
+  do
+  {
+    int x;
+    cout<<"Enter Value for node";
+    cin>>x;
+    ll.push(x);
+    cout<<"Do you want to continue?(0/1)";
+    cin>>ch;
+   
+  }while(ch==1);
 
-LinkedList ll;
-ll.push(20);
-ll.push(4);
-ll.push(15);
-ll.push(85);
-
-cout << "Given linked list\n";
-ll.print();
-
-ll.head = ll.reverse(ll.head);
-
-cout << "\nReversed Linked list \n";
-ll.print();
-return 0;
-} 
+  cout << "Given linked list\n";
+  ll.print();
+  ll.head = ll.reverse(ll.head);
+  cout << "\nReversed Linked list \n";
+  ll.print();
+  return 0;
+}
