5d600397547df478a5023d61a1d18b20da6acb86
Mananpreet Kaur <manankaur00@gmail.com>
2019-10-18 01:38:24 +0530

    Update SolutionByMananpreet.java

--- Basic/Convert Decimal To Binary/SolutionByMananpreet.java
+++ Basic/Convert Decimal To Binary/SolutionByMananpreet.java
@@ -1,13 +1,15 @@
 
 /*
 The following program converts an entered decimal number into binary and returns the answer as a string*/
-
 import java.util.Scanner;
 
 class DecimalToBinary
 {
 	public static String decimalToBinary(int n)
 	{
+		if(n == 0)
+			return "0";
+
 		String binary = "";
 		while(n>0)
 		{
@@ -18,12 +20,9 @@ class DecimalToBinary
 	}
 
 	public static void main(String[] args) {
-  
 		Scanner s = new Scanner(System.in);
-    
 		System.out.print("Enter a decimal number: ");
         int n = s.nextInt();
-        
         System.out.println("The binary conversion of the inputed decimal number "+n+" is : "+ decimalToBinary(n));
 	}
 }
