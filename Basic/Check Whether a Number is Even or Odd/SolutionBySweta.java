69696a602ff7506ccfb5251df8010f15fadf4694
swetamittal <31439784+swetamittal@users.noreply.github.com>
2019-10-17 20:03:32 +0530

    Update SolutionBySweta.java

--- Basic/Check Whether a Number is Even or Odd/SolutionBySweta.java
+++ Basic/Check Whether a Number is Even or Odd/SolutionBySweta.java
@@ -1,26 +1,24 @@
 
 import java.util.Scanner;
 
-class GFG
+class odd_even
 {
-   
-public static boolean isEven(int n)
-{
-if((n & 1) == 0)
-return true;
-else
-return false;
-}
+   public static boolean isEvenNumber(int num)
+   {
+      if((num & 1) == 0)
+         return true;
+      else
+         return false;
+   }
 
+   public static void main(String[] args)
+   {
+      Scanner input = new Scanner(System.in);
+      int number = input.nextInt();
 
-public static void main(String[] args)
-{
-Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-
-if(isEven(number) == true)
-System.out.print("EVEN");
-else
-System.out.print("ODD");
-}
+      if(isEvenNumber(number) == true)
+         System.out.print("EVEN");
+      else
+         System.out.print("ODD");
+   }
 }
