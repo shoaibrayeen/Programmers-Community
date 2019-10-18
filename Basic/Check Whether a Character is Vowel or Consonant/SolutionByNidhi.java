bc6f790fd63411b4d7cd8b15a48f50fce98397a9
Nidhi Verma <nidhiverma1997@gmail.com>
2019-10-18 21:55:35 +0530

    Create SolutionByNidhi.java
    program to check whether a character is a vowel or consonant

---
+++ Basic/Check Whether a Character is Vowel or Consonant/SolutionByNidhi.java
@@ -0,0 +1,24 @@
+/*
+    Program Description: Program to check whether a character is a vowel or consonant
+ */
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class VowelOrConsonant {
+    private static void isVowelOrConsonant(char ch) {
+        Pattern pattern = Pattern.compile("[AEIOUaeiou]");
+        Matcher matcher = pattern.matcher(ch+"");
+        if(matcher.matches()){
+            System.out.println("Output = Vowel");
+        }else{
+            System.out.println("Output = Consonant");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char ch = scan.next().charAt(0);
+        isVowelOrConsonant(ch);
+    }
+}
