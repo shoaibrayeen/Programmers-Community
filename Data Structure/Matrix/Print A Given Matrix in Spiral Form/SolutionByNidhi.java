fc8876923beb890142717983fddd0e34a260a9bd
Nidhi Verma <nidhiverma1997@gmail.com>
2019-10-18 23:35:00 +0530

    Update SolutionByNidhi.java
    updated code to print correct spiral matrix

--- Data Structure/Matrix/Print A Given Matrix in Spiral Form/SolutionByNidhi.java
+++ Data Structure/Matrix/Print A Given Matrix in Spiral Form/SolutionByNidhi.java
@@ -8,20 +8,46 @@ public class SpiralMatrix {
     // method to print matrix in spiral form
     private static void printSpiralMatrix(int[][] array) {
 
-        int noOfRows = array.length;
-        int noOfCols = array[0].length;
-        for(int row = 0; row < noOfRows; row++){
-            // if current row has even index, print elments from left to right
-            if(row % 2 == 0){
-                for(int col = 0; col < noOfCols; col++) {
-                    System.out.print(array[row][col] + " ");
-                }
+        /*
+            Approach:
+                1. Print all the elements of topRow
+                2. Print the last rightmost element of all the rows between topIndex and bottomIndex
+                3. Print all the elements of lastRow from right to left
+                4. Print elements from bottom to top of the first column denoted by leftIndex
+         */
+        int topIndex  = 0;                          // top row
+        int bottomIndex = array.length;             // bottom row
+        int rightIndex = array[0].length;           // last column
+        int leftIndex = 0;                          // first column
+
+        while(bottomIndex > topIndex && leftIndex < rightIndex) {
+            // print top row
+            for (int j = leftIndex; j < rightIndex; j++) {
+                System.out.print(array[topIndex][j] + " ");
             }
-            // otherwise, print elements from right to left
-            else {
-                for (int col = noOfCols - 1; col >= 0; col--) {
-                    System.out.print(array[row][col] + " ");
+            topIndex++;
+
+            // print last column
+            for (int i = topIndex; i < bottomIndex; i++) {
+                System.out.print(array[i][rightIndex-1] + " ");
+            }
+            rightIndex--;
+
+            // print bottom row
+            if(topIndex < bottomIndex){
+                for (int j = rightIndex-1; j >= leftIndex; j--) {
+                    System.out.print(array[bottomIndex-1][j] + " ");
                 }
+                bottomIndex--;
+            }
+
+
+            // print first column
+            if(leftIndex < rightIndex) {
+                for (int i = bottomIndex - 1; i > topIndex - 1; i--) {
+                    System.out.print(array[i][leftIndex] + " ");
+                }
+                leftIndex++;
             }
         }
     }
