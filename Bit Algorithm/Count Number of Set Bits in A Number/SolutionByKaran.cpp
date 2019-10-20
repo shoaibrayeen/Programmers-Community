b97105466af3cb150ee69ae041c60591b358d4db
Karansingh005 <41364215+Karansingh005@users.noreply.github.com>
2019-10-20 15:11:24 +0530

    Create SolutionByKaran.cpp
    Program to calculate number of 1s in binary number of the enter number.

---
+++ Bit Algorithm/Count Number of Set Bits in A Number/SolutionByKaran.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+void count(int n)
+{
+    int a=0;
+    if(n<0)
+    {
+        cout<<"Enter a positive number.";
+    }
+    else
+    {
+        while(n>0)
+        {
+            if(n%2==1)
+            {
+                a++;
+            }
+            n=n/2;
+        }
+        cout<<"Number of 1s in the binary number is: "<<a<<endl;
+    }
+}
+int main()
+{
+    int b;
+    cout<<"Enter the number: ";
+    cin>>b;
+    cout<<endl;
+    count(b);
+    return 0;
+}
