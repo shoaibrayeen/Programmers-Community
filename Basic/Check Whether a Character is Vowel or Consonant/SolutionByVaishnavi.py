141bde95e41ee413487a3c276f5b4f1601c98ad2
Vaishnavi Lokhande <vaishnavilokhande1999@gmail.com>
2019-10-21 13:20:12 +0530

    SolutionByVaishnavi.py
    Checked char is vowel or consonant using membership operator

---
+++ Basic/Check Whether a Character is Vowel or Consonant/SolutionByVaishnavi.py
@@ -0,0 +1,10 @@
+#To check character is vowel or consonant
+
+vowels = ('A','E','I','O','U','a','e','i','o','u')
+char = str(input('Character='))
+
+if (char in vowels):
+    print('Vowel')
+            
+else:
+    print('Consonant')
