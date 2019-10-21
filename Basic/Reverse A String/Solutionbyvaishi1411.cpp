f723aebdcd7c4c71e33baa99e4bf14e27dd67507
Vaibhavi-1411 <56067910+Vaibhavi-1411@users.noreply.github.com>
2019-10-21 23:23:32 +0530

    Solutionbyvaishi1411

---
+++ Basic/Reverse A String/Solutionbyvaishi1411.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string.h> 
+#include<algorithm>
+using namespace std;
+string shi;
+void rev_str(string )
+{ 
+reverse(shi.begin(),shi.end());
+}
+int main()
+{ 
+cout<<"Enter the String to be Reversed: ";
+cin>>shi; 
+rev_str(shi);
+cout<<"\nReverse of string in input is: "<<shi;
+return 0;
+}
