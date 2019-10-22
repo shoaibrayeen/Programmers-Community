e940180bf0bcfb6a2636aed1fb9668a153fd7ea6
Karansingh005 <41364215+Karansingh005@users.noreply.github.com>
2019-10-22 22:50:31 +0530

    Create SolutionByKaran.cpp
    Program to find out the sum of digits of a given number.

---
+++ Basic/Sum of Digits in A Number/SolutionByKaran.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void calculate_sum(long a)
+{
+    long sum=0;
+    while(a>0)
+    {
+        sum = sum + (a%10);
+        a=a/10;
+    }
+    cout<<"Sum of digits is: "<<sum<<endl;
+}
+int main() 
+{
+    long b;
+    cout<<"Enter the number: ";
+    cin>>b;
+    cout<<endl;
+    calculate_sum(b);
+    return 0;
+}
