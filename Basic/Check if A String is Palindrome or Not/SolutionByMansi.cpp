7bcbdcbaca1d1da744a9b875fb25d144df4595c4
mansi35 <53896251+mansi35@users.noreply.github.com>
2019-10-23 23:26:18 +0530

    Create SolutionByMansi.cpp

---
+++ Basic/Check if A String is Palindrome or Not/SolutionByMansi.cpp
@@ -0,0 +1,53 @@
+#include<iostream.h>
+#include<conio.h>
+using namespace std;
+
+bool Palindrome(char str[])
+{
+  int n=0;
+    bool check=false;
+    for (int i=0;str[i]!='\0';i++)
+    {
+        n++;
+    }
+
+    for (int i=0;i< n/2;i++)
+    {
+        if (str[i]==str[n-i-1]) 
+        {
+          check=true;
+        }
+        else
+        {
+            check=false;
+            break;
+        }
+    }
+
+    if (check == true )
+    {
+      return true;
+    }
+    else 
+    {
+      return false;
+    }
+
+}
+
+void main()
+{
+  clrscr();
+  char str[10000];
+  cin.getline(str,10000);
+
+  if(Palindrome(str))
+  {
+  	cout << "true" << endl; 	
+  }
+  else
+  {
+    cout << "false" << endl;
+  }
+  getch();
+}
