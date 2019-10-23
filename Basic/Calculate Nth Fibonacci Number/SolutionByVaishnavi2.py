49f583193cd54334b0dea5241f1b12d4255657c6
Vaishnavi Lokhande <vaishnavilokhande1999@gmail.com>
2019-10-23 20:27:57 +0530

    Update SolutionByVaishnavi2.py

--- Basic/Calculate Nth Fibonacci Number/SolutionByVaishnavi2.py
+++ Basic/Calculate Nth Fibonacci Number/SolutionByVaishnavi2.py
@@ -1,7 +1,7 @@
 #To check Nth number in fibonacci series using recursion
 def fibonacci(n):
     if n<=1:
-        return n
+        return 0
     else:
         return(fibonacci(n-1) + fibonacci(n-2))
 n = int(input('N='))
