93a65e72ad65e20920e5bf290904bb6220950bb0
rithgan <rithikgandhi1989@gmail.com>
2019-10-24 02:09:54 +0530

    add solution

---
+++ Basic/Check if A Number is Prime or Not/solutionsByrithgan.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n; 
+    cout<<"Enter a number ";
+    cin>>n;
+    if(n%2==0)
+    cout<<"Number is even";
+    else
+    cout<<"Number is odd";
+    return 0;
+}
