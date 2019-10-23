8c783aece2f30babd5e52701541ebd66b89d57cd
Vaibhavi-1411 <56067910+Vaibhavi-1411@users.noreply.github.com>
2019-10-23 23:14:19 +0530

    Solutionbyvaishi1411

---
+++ Basic/Calculate Nth Fibonacci Number/Solutionbyvaishi1411.cpp
@@ -0,0 +1,35 @@
+//c++program to find nth term of fibonacci series
+ 
+ #include<iostream>
+ using namespace std;
+
+//prototype of fibo_n()  function
+
+int fibo_n(int );
+
+//Driver code
+
+int main()
+{ 
+  int N;
+  cout<<"Enter a postive number: ";
+  cin>>N;
+if(N>=0) 
+cout<<" Nth Number in Fibonacci Series: "<<fibo_n(N);
+else 
+cout<<"You are only allowed to enter positive number!";
+return 0;
+} 
+
+//defenition of fibo_n()  function 
+
+int fibo_n(int N) 
+{ int shi[N+2];
+  shi[0]=0;
+ shi[1]=1;
+ for(int i=2;i<=N;i++)
+ { 
+   shi[i]=shi[i-1]+shi[i-2];
+  }
+ return shi[N];
+}
