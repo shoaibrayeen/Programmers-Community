15c87356d5ca9ba797466b9161c9ac3702af5d26
shagun0915 <56886360+shagun0915@users.noreply.github.com>
2019-10-24 18:19:35 +0530

    Create solutionbyshagun.cpp

---
+++ Basic/Check Whether a Number is Even or Odd/solutionbyshagun.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout <<"ENTER AN INTEGER:\n";
+  cin >>a;
+  if(a%2==0)
+  {
+    cout <<"The given integer is even";
+  }
+  else
+  {
+    cout <<"The given integer is odd";
+  }
+  return 0;
+}
+    
