c8defccbd0c31eae80365ad039292c6433ec5baa
eternal-dreamer <53971925+eternal-dreamer@users.noreply.github.com>
2019-10-24 05:44:42 -0700

    Create SolutionByShrishti.cpp

---
+++ Basic/Check Whether a Number is Even or Odd/SolutionByShrishti.cpp
@@ -0,0 +1,21 @@
+#include <iostream.h>
+void oddeven(int n)
+{
+    if(n%2==0)
+    {
+        cout<<"Even";
+    }
+    else 
+    {
+        cout<<"Odd";
+    }
+}
+int main() 
+{	
+    int b;
+    cout<<"Enter a number: \n";
+    cin>>b;
+    oddeven(b);
+    return 0;
+}
+© 2019 GitHub, Inc.
