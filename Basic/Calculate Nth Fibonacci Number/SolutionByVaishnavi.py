b8d3e1dfaac66b5e0b0ad49bee78583027987a84
Vaishnavi Lokhande <vaishnavilokhande1999@gmail.com>
2019-10-24 22:19:57 +0530

    Update SolutionByVaishnavi.py

--- Basic/Calculate Nth Fibonacci Number/SolutionByVaishnavi.py
+++ Basic/Calculate Nth Fibonacci Number/SolutionByVaishnavi.py
@@ -1,13 +1,16 @@
 #To print Nth number in fibonacci series
 
 a,b = 0,1
-c = 0
 n = int(input('N= '))
-if n<=1:
-    print(0)
+if n<0:
+    print('Not accepted')
+elif n==0:
+    print(a)
+elif n==1:
+    print(b)
 else:
     for i in range(2,n):
         c = a + b
         a = b              
         b = c
-    print(n,'no in fibonacci series is= ',c)
+    print(n,'no in fibonacci series is= ',b)
