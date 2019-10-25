0174fbf9226c22bef84b4e1541ea8db000931cfc
Nidhi Verma <nidhiverma1997@gmail.com>
2019-10-25 23:04:31 +0530

    Update SolutionByNidhi.java
    updated code tp find no. of set bits in a binary number

--- Bit Algorithm/Count Number of Set Bits in A Number/SolutionByNidhi.java
+++ Bit Algorithm/Count Number of Set Bits in A Number/SolutionByNidhi.java
@@ -5,14 +5,10 @@ import java.util.Scanner;
 
 public class CountNoOfOne {
     private static int countNoOfOne(long num) {
-        int countOne = 0, exp = 0;
-        long power = 1;
-        while(power < num) {
-            long andRes = num & power;
-            if(andRes == power){
-                countOne++;
-            }
-            power = (long)Math.pow(2, ++exp);
+        int countOne = 0;
+        while(num != 0) {
+            countOne++;
+            num  &= (num-1);
         }
         return countOne;
     }
