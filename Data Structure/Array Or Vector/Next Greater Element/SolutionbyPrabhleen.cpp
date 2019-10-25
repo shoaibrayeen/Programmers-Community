af5b66df1557c1f00f40618d771794b0f72e5076
prabhleenk <42249401+prabhleenk@users.noreply.github.com>
2019-10-25 16:59:31 +0530

    Update SolutionbyPrabhleen.cpp

--- Data Structure/Array Or Vector/Next Greater Element/SolutionbyPrabhleen.cpp
+++ Data Structure/Array Or Vector/Next Greater Element/SolutionbyPrabhleen.cpp
@@ -17,10 +17,15 @@ void printNextGreaterElement(int arr[], int n)
         printf("%d -- %d\n", arr[i], next);
     }
 }
- 
+
 int main()
 {
-    int arr[]= {11, 13, 21, 3, 7};
+	int size;
+	cin >> size;
+    int arr[size];
+	for( int i = 0; i < size; i++ ){
+		cin >> arr[i];
+	}
     int n = sizeof(arr)/sizeof(arr[0]);
     printNextGreaterElement(arr,n);
     return 0;
