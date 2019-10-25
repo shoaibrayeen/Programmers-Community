d639bf5d0fb811823d53e4eb96d4b64bb6848934
Pooja <54528116+pooja-git11@users.noreply.github.com>
2019-10-25 18:34:09 +0530

    Update and rename solutionByPooja.c to solutionByPooja.cpp

---
+++ Basic/Count Number of Vowels in A String/solutionByPooja.cpp
@@ -0,0 +1,21 @@
+//finding no.  of vowel in string
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+    int i=0,count=0;
+    string s;
+
+    cout<<"Enter string: ";
+    cin>>s;
+    for(;i<s.length();i++)
+    {
+       if(s[i]=='a'||s[i]=='e'|| s[i]=='i'|| s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+               count++;
+    }
+    cout<<"\nNo. of vowel in input sting is : "<<count;
+
+
+    return 0;
+}
