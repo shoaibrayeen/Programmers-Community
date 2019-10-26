5806a724b5a69019664a3582ab3df19e604bbc36
Mananpreet Kaur <manankaur00@gmail.com>
2019-10-26 20:57:53 +0530

    Create SolutionByMananpreet.java
    The submitted program checks if the two inputted numbers have opposite signs or not.

---
+++ Bit Algorithm/Check if Two Integers Have Opposite Signs/SolutionByMananpreet.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class CheckIfOppositeSigns
+{
+	private static String check(int a, int b)
+	{
+		if((a^b) < 0)
+		return "The two inputted numbers have opposite signs";
+
+	     else
+	     	return "The two inputted numbers do not have opposite signs";
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a ");
+		int a = s.nextInt();
+        System.out.println("Enter a ");
+		int b = s.nextInt();
+		System.out.println(check(a,b));
+	}
+}
