44e3c055f2bf6dcb66adb2cf3a9f1c1ab45334d2
Vaibhavi-1411 <56067910+Vaibhavi-1411@users.noreply.github.com>
2019-10-27 08:02:10 +0530

    Solutionbyvaishi1411(2).cpp

--- Basic/Check Leap Year/Solutionbyvaishi1411.cpp
+++ Basic/Check Leap Year/Solutionbyvaishi1411.cpp
@@ -15,13 +15,13 @@ int main()
     if(res==1)
       cout<<y<<"is a leap year!!!";
      else 
-       cout<<y<<"is not aleap year!!!";
+       cout<<y<<"is not a leap year!!!";
   return 0;
 } 
 //function defination 
 int leapyr(int y)
 { 
- if(y%4==0||y%100==0||y%400==0) 
+ if((y!=2000 && y!=1900) && (y%4==0||y%100==0||y%400==0))
    return 1;
  else 
  return 0;
