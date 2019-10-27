63078b219d0a99e69e05c415b31f82843fb76f5e
Vaibhavi-1411 <56067910+Vaibhavi-1411@users.noreply.github.com>
2019-10-27 07:27:38 +0530

    Solutionbyvaishi1411(2).cpp

--- Basic/Find LCM of Two Numbers/Solutionbyvaishi1411.cpp
+++ Basic/Find LCM of Two Numbers/Solutionbyvaishi1411.cpp
@@ -1,6 +1,11 @@
+//progran to find LCM
 #include <iostream> 
-using namespace std; 
+using namespace std;  
+
+//Function prototype
 int find_LCM(int s, int v) ; 
+
+//Driver code
 int main() 
 { int s,v,res; 
 	cout<<"Enter the two Numbers for LCM operation:";
@@ -9,6 +14,8 @@ res=find_LCM(s,v);
 	cout<<"LCM OF "<<s<<" and "<<v<<" : "<<res;
 	return 0;
 } 
+
+//Definition of the function
 int find_LCM(int s, int v) 
 { 
 	int lar = max(s, v); 
