3dc3a993291e66f83bbf46b424fa5d4ddfd3ef4b
Vaibhavi-1411 <56067910+Vaibhavi-1411@users.noreply.github.com>
2019-10-28 21:10:56 +0530

    Solutionbyvaishi1411(2).cpp

--- Basic/Implement Power Function/Solutionbyvaishi1411.cpp
+++ Basic/Implement Power Function/Solutionbyvaishi1411.cpp
@@ -1,19 +1,32 @@
 //base raise to the power 
  #include<iostream> 
- using namespace std;
- 
- //driver code 
- int main()
- { 
- float s,v,res=1;
- cout<<"Enter the base value: ";
- cin>>s;
- cout<<"Enter the exponent value: ";
- cin>>v;
-  for(int i=1;i<=v;i++) 
-  { 
-   res=res*s;
-  } 
-  cout<<"Your Result!! "<<res;
-  return 0;
- }
+	 using namespace std;
+	 //driver code 
+	 int main()
+	 { 
+	 float s,v,n=1,res;
+	 cout<<"Enter the base value: ";
+	 cin>>s;
+	 cout<<"Enter the exponent value: ";
+	 cin>>v; 
+	 
+	  if(v<0) 
+	  { 
+	  for(int i=1;i<= -(v);i++)  
+	 {
+	    n=n*s;  
+	 }
+	     res=1/n;
+	  }
+	  else if(v>=0)  
+	  { 
+	   for(int i=1;i<=v;i++)  
+	 {
+	    n=n*s;  
+	 } 
+	   res=n;
+	  }
+	   
+	  cout<<"Your Result!! "<<res;
+	  return 0;
+	 }
