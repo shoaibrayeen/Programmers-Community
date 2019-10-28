e719d3cc53076da16f93fa547504d69148935376
Pooja <54528116+pooja-git11@users.noreply.github.com>
2019-10-28 14:26:05 +0530

    Create solutionByPooja.cpp

---
+++ Basic/Calculate Factorial of A Number/solutionByPooja.cpp
@@ -0,0 +1,28 @@
+/*
+     A positive number N is given to you and You need to calculate its factorial.
+*/
+#include<iostream>
+using namespace std;
+
+int factorial(int);
+
+int main()
+{
+    int num;
+
+    cout<<"Enter a number: ";
+    cin>>num;
+
+
+    cout<<"\nFactorial of "<<num<<"is "<<factorial(num);
+
+    return 0;
+}
+int factorial(int n)
+{                              //factorial using recursion
+    if(n==1)
+        return 1;
+    else
+        return n*factorial(n-1);
+
+}
