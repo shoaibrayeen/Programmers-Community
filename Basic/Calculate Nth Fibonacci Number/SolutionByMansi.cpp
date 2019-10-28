449df2b477a5706ff1eae86ae4e8d0736c93591e
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-10-28 14:05:39 +0530

    Updated code

--- Basic/Calculate Nth Fibonacci Number/SolutionByMansi.cpp
+++ Basic/Calculate Nth Fibonacci Number/SolutionByMansi.cpp
@@ -1,22 +1,22 @@
-#include<iostream.h>
-#include<conio.h>
+#include<iostream>
+using namespace std;
 
-int fib(int n)
-{
-  if(n==0||n==1)
-    return 1;
-  else
-  {
-    return fib(n-1)+fib(n-2);
-   }
-  }
+int fib(int n) {
+    if( n == 1 ) {
+        return 0;
+    }
+    else if ( n == 2) {
+        return 1;
+    }
+    else {
+    return fib(n-1) + fib(n-2);
+    }
+}
   
-void main()
-{
-  clrscr();
-  int n;
-  cout<<"which fibonacci number you want to know?";
-  cin>>n;
-  cout<<"The "<<n<<"th fibonacci number is "<<fib(n);
-  getch();
+int main() {
+    int n;
+    cout << "\nEnter N\t:\t";
+    cin >> n;
+    cout << "The " << n << "th fibonacci number\t:\t" << fib(n) << endl;
+    return 0;
 }
