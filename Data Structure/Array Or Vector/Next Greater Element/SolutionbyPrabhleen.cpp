13d16d03c87f078cc0249872c6cc00f0d0439fdb
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-10-29 14:34:28 +0530

    Updated - Brute Force Approach

--- Data Structure/Array Or Vector/Next Greater Element/SolutionbyPrabhleen.cpp
+++ Data Structure/Array Or Vector/Next Greater Element/SolutionbyPrabhleen.cpp
@@ -26,7 +26,6 @@ int main()
 	for( int i = 0; i < size; i++ ){
 		cin >> arr[i];
 	}
-    int n = sizeof(arr)/sizeof(arr[0]);
-    printNextGreaterElement(arr,n);
+    printNextGreaterElement(arr,size);
     return 0;
 }
