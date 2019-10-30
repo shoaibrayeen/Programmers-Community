c77b58816b51af6f78f3d2baf840adabb0e8aa62
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-10-30 15:05:29 +0530

    Indented!

--- Data Structure/Array Or Vector/Search An Element in Unsorted Array/Solutionbyvaishi1411.cpp
+++ Data Structure/Array Or Vector/Search An Element in Unsorted Array/Solutionbyvaishi1411.cpp
@@ -1,25 +1,27 @@
 #include<iostream>
-	using namespace std;
-	int main()
-	{ int shi[100],v,n,i=0,res=0;
-	 
-	 cout<<"Enter the size of the array: ";
-	 cin>>n; 
-	 cout<<"Enter the elemets of the array (max. to size): ";
-	 for(i=0;i<n;i++) 
-	  cin>>shi[i]; 
-	
-	 cout<<"Enter the element to be searched :";
-	 cin>>v;  
-	   for(i=0;i<n;i++)
-	   { 
-	      if(shi[i]==v) 
-	      res=1;
-	
-	   } 
-	  if(res==1) 
-	  cout<<"Found!!"; 
-	  else 
-	  cout<<"Not Found!!";
-	 return 0;
+using namespace std;
+int main(){ 
+	int v, n, i = 0, res = 0;
+	cout << "Enter the size of the array: ";
+	cin >> n; 
+	int *shi = new int[n];
+	cout << "Enter the elemets of the array (max. to size): ";
+	for( i = 0; i < n; i++ ) { 
+	  cin >> shi[i]; 
 	}
+	cout << "Enter the element to be searched :";
+	cin >> v;  
+	for( i = 0; i < n; i++ ) { 
+		if( shi[i] == v ) {
+	      		res = 1;
+		}
+	
+	} 
+	if( res == 1 ) { 
+		cout << "Found!!\n";
+	}
+	else {
+	  	cout << "Not Found!!\n";
+	}
+	 return 0;
+}
