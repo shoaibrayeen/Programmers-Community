0f6c920805a093419ac2b5c4dd164f789f494d64
Prince Garg <54926566+gargprince1702@users.noreply.github.com>
2019-10-30 08:53:41 -0700

    solutionbygargprince1702.cpp

--- Basic/Check Leap Year/solutionbygargprince1702.cpp
+++ Basic/Check Leap Year/solutionbygargprince1702.cpp
@@ -5,7 +5,11 @@ int main()
           int year;
           cout<<"ENter Year to check :- ";
           cin>>year;
-          if(year%400==0 or year%4==0)
+          if(year%400==0)
+          cout<<"Yes";
+          else if(year%100==0)
+          cout<<"No";
+          else if(year%4==0)
           cout<<"Yes";
           else
           cout<<"No";
