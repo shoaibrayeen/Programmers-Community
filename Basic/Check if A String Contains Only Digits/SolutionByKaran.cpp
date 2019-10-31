77ed3c4fd71e46c43b155183b0ddeb090bad30e5
Karansingh005 <41364215+Karansingh005@users.noreply.github.com>
2019-10-31 23:14:47 +0530

    Create SolutionByKaran.cpp
    Program to tell if the given string has ONLY digits or not.

---
+++ Basic/Check if A String Contains Only Digits/SolutionByKaran.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include "stdio.h"
+using namespace std;
+void only_digits(char* str)
+{
+      int f=0;
+      for(int i=0; i<=strlen(str)-1;i++)
+      {
+          if(str[i]<=48 || str[i]>=57)
+          {
+              f++;	
+          }
+      }
+      if(f==0)
+      {
+          cout<<"Yes";
+      }
+      else
+      {
+          cout<<"No";
+      }
+}
+int main() 
+{	
+      char* str_a = new char();
+      cout<<"Enter the string: \n";
+      std::cin>>str_a;
+      only_digits(str_a);
+      return 0;
+}
