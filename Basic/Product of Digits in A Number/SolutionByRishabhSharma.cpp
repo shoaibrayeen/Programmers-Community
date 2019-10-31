f1ed2af039d1aabbb688e706d8cd849ef0009dc5
Rishabh Sharma <48138906+rishiar4@users.noreply.github.com>
2019-10-31 22:25:19 +0530

    SolutionByRishabhSharma.cpp
    increased the digit by one if zero.

---
+++ Basic/Product of Digits in A Number/SolutionByRishabhSharma.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int mDigits(int n)
+{
+	int ans=1;
+
+	while(n>0)
+	{
+		int rem=n%10;
+		if(rem==0)
+			rem++;
+		ans*=rem;
+		n=n/10;
+	}
+
+	return ans;
+}
+
+
+
+int main()
+{
+
+	int n;
+	cout<<"Enter the Number "<<endl;
+	cin>>n;
+
+	int ans=mDigits(n);
+
+	cout<<ans<<endl;
+}
