0f8872763559d1032483e752260ab490cde463fe
Vaibhavi-1411 <56067910+Vaibhavi-1411@users.noreply.github.com>
2019-11-01 21:13:55 +0530

    Solutiobyvaishi1411(2).cpp

--- Basic/Check Leap Year/Solutionbyvaishi1411.cpp
+++ Basic/Check Leap Year/Solutionbyvaishi1411.cpp
@@ -2,16 +2,20 @@
 #include<iostream>
 using namespace std;
  //driver code
- int main()
- { 
- int shi;
- cout<<"Enter a year : ";
- cin>>shi;
-	 if(shi%400==0) 
-	 cout<<"Leap Year!!"; 
-	 else if (shi%100==0) 
-	 cout<<"Not a Leap Year!!"; 
-	 else if (shi%4==0)
-	 cout<<"Not a Leap Year!!";
- return 0;
+ int main() {  
+	 int shi;
+        
+	 cout<<"Enter a year : ";
+          cin>>shi;
+	
+	    if(shi%400==0) 
+	     cout<<"Leap Year!!"; 
+	    else if (shi%100==0) 
+	     cout<<"Not a Leap Year!!"; 
+	     else if (shi%4==0)
+	       cout<<"Not a Leap Year!!";
+	      else 
+		 cout<<"Not a Leap Year!!";
+  
+     return 0;
  }
