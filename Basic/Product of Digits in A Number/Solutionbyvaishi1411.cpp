2a41c8766469ab599bcc03bade105f7fc55b0b91
Vaibhavi-1411 <56067910+Vaibhavi-1411@users.noreply.github.com>
2019-11-01 22:59:10 +0530

    Solutionbyvaishi1411.cpp

---
+++ Basic/Product of Digits in A Number/Solutionbyvaishi1411.cpp
@@ -0,0 +1,38 @@
+#include<iostream> 
+using namespace std; 
+// Function to get product of digits //
+int product(int shi) 
+{ 
+    int res = 1,rem=0; 
+ while(shi!=0){ 
+          rem=shi%10;
+  
+           if(rem!=0){  
+           
+                 res=res*rem; 
+                  shi=shi/10;
+           } 
+           else if (rem==0){ 
+              shi=shi/10;
+            }         
+ } 
+   return res; 
+} 
+
+// Driver program 
+int main() 
+{ 
+    int shi; 
+	  cout<<"Enter a number: "; 
+	  cin>>shi; 
+	  if(shi==0) { 
+	    cout<<"Product of it's digits is: 0 "; 
+	  }
+	  
+	  else
+	  { 
+	   cout<<"Product of it's digits is: "<<product(shi); 
+	  }
+	 
+    return 0; 
+} 
