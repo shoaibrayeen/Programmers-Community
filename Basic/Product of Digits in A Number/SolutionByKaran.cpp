18aea3c665e2c6e0174465043b27c5d0c0c3ae71
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-02 19:22:34 +0530

    Updated Code

--- Basic/Product of Digits in A Number/SolutionByKaran.cpp
+++ Basic/Product of Digits in A Number/SolutionByKaran.cpp
@@ -2,23 +2,24 @@
 using namespace std;
 long product(long n)
 {
+    if ( n == 0 ) {
+        return 0;
+    }
     long p=1;
     while(n>0)
     {
-        if(n%10==0)
-        {
-            n=n/10;
+        if(n%10 != 0) {
+            p=p*(n%10);
         }
-        p=p*(n%10);
         n=n/10;
     }
     return p;
 }
-int main() 
+int main()
 {
     long a;
     cout<<"Enter the number: "<<endl;
     cin>>a;
-    cout<<"The product is: "<<product(a);
+    cout<<"The product is: "<<product(a) << endl;
     return 0;
 }
