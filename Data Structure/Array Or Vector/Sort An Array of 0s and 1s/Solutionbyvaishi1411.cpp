71cb71f3bd0f20cb2372f5e19994ee39f020aa97
Vaibhavi-1411 <56067910+Vaibhavi-1411@users.noreply.github.com>
2019-11-02 08:45:52 +0530

    Solutionbyvaishi1411.cpp

---
+++ Data Structure/Array Or Vector/Sort An Array of 0s and 1s/Solutionbyvaishi1411.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+	int n,i=0,item,res;
+		cout<<"Enter the size of the array: ";
+		cin>>n;
+	int *shi= new int [n]; 
+		cout<<"Enter the elements of an array(with 0 & 1 only): ";
+		for(i=0;i<n;i++) 
+		{ 
+		  cin>>shi[i]; 
+		   
+		}  
+		   
+	       
+	                sort(shi,shi+n); 
+			        cout<<endl;
+		            cout<<"Sorted array is :\n"; 
+		        for(i=0;i<n;i++){ 
+		             cout<<shi[i]<<"\t";
+	          	}
+		           
+
+    return 0;
+} 
+   
