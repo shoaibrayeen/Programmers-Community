319dbeeccc9fd571cbb48b5ca68de456178f7cf9
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-03 16:11:13 +0530

    Updated and Removed Unused Variable

--- Data Structure/Array Or Vector/Reverse of An Array/Solutionbyvaishi1411.cpp
+++ Data Structure/Array Or Vector/Reverse of An Array/Solutionbyvaishi1411.cpp
@@ -1,35 +1,34 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int reverse(int v[], int n);
-int main()
-{ 
-	int n,i=0,item,res;
-	cout<<"Enter the size of the array: ";
-	cin>>n;
-	int *shi= new int [n]; 
-	cout<<"Enter the elements of an array: ";
-	for(i=0;i<n;i++) 
-	{ 
-	  cin>>shi[i];
-	}  
-		 reverse(shi,n); 
-		cout<<"REverse of the given array is: ";
-		for(i=0;i<n;i++){ 
-		cout<<shi[i]<<"\t";
-		}
-   
-    return 0;
-} 
- 
- int reverse(int v[], int n) 
-{  int i=0,j=n-1,temp; 
-		   while(i<j) { 
-		     temp=v[i]; 
-		     v[i]=v[j];
-		     v[j]=temp;
-		     i++; 
-		     j--;
+
+void reverse(int v[], int n);
+
+int main() {
+   int n, i;
+   cout<<"\nEnter the size of the array: ";
+   cin>>n;
+   int *shi= new int [n];
+   cout<<"\nEnter the elements of an array: ";
+   for(i = 0; i < n; i++) {
+       cin>>shi[i];
    }
- 
+   reverse(shi,n);
+   cout<<"\nReverse Array\t:\t";
+   for(i = 0; i < n; i++){
+       cout<<shi[i]<<"\t";
+   }
+   cout << endl;
+   return 0;
+}
+
+void reverse(int v[], int n) {
+    int i = 0, j = n -1, temp;
+    while( i < j ) {
+        temp = v[i];
+        v[i] = v[j];
+        v[j] = temp;
+        i++;
+        j--;
+    }
 }
