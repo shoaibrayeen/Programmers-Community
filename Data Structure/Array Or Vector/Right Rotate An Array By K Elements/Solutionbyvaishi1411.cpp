c064ddab209dc40be613616403fb49ad7acd2a63
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-03 16:07:10 +0530

    Updated and removed errors

--- Data Structure/Array Or Vector/Right Rotate An Array By K Elements/Solutionbyvaishi1411.cpp
+++ Data Structure/Array Or Vector/Right Rotate An Array By K Elements/Solutionbyvaishi1411.cpp
@@ -1,40 +1,35 @@
 #include<iostream>
 using namespace std;
 
-int r_rotate(int shi[],int n , int v);
+void r_rotate(int shi[],int n , int v);
 
-int main(){ 
-        int n,i,v;
-        cout<<"Enter the size of the array: ";
-        cin>>n;
-        int *shi=new int [n];
-        
-        cout<<"Enter the elements of the array : ";
-        for(i=0;i<n;i++)
-        { 
-          cin>>shi[i];
-        } 
-          
-          cout<<"Enter the key element through which Rotation is to be done : ";
-            cin>>v;
-                  
-                  r_rotate(shi,n,v);
-                    cout<<"Right Rotated array through key element "<<v<<" is : ";
-                    for(i=0;i<n;i++){ 
-                    cout<<shi[i];
-                    } 
-return 0;
-} 
+int main(){
+    int n, i, v;
+    cout<<"\nEnter the size of the array: ";
+    cin>>n;
+    int *shi=new int [n];
+    cout<<"\nEnter the elements of the array : ";
+    for(i = 0; i < n; i++ ) {
+        cin>>shi[i];
+    }
+    cout<<"Enter Rotation Value\t:\t";
+    cin>>v;
+    r_rotate(shi, n, v);
+    cout<<"\nRight Rotated array by "<< v << " is\t:\t";
+    for(i = 0; i < n; i++ ){
+        cout<<shi[i] << "\t";
+    }
+    cout << endl;
+    return 0;
+}
 
-int r_rotate(int shi[],int n,int v){ 
-            for(int i=0;i<v;i++){ 
-            int j, l;
-           
-           l=shi[n-1];
-
-            for(j=n-1;j>0;j--){ 
-            shi[j]=shi[j-1] ;
-            } 
+void r_rotate(int shi[],int n,int v) {
+    for(int i = 0; i < v; i++ ) {
+        int l;
+        l = shi[n-1];
+        for(int j = n-1; j > 0; j-- ) {
+            shi[j] = shi[j-1] ;
+        }
         shi[0]=l;
-      } 
+    }
 }
