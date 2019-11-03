f5bc8223bac3014ab1f9bc3699b38862f054abc1
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-03 16:01:18 +0530

    Removed Unused Variables and Indented

--- Data Structure/Array Or Vector/Sort An Array of 0s , 1s and 2s/Solutionbyvaishi1411.cpp
+++ Data Structure/Array Or Vector/Sort An Array of 0s , 1s and 2s/Solutionbyvaishi1411.cpp
@@ -1,26 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main()
-{ 
-	int n,i=0,item,res;
-		cout<<"Enter the size of the array: ";
-		cin>>n;
-	int *shi= new int [n]; 
-		cout<<"Enter the elements of an array(with 0 & 1 & 2 only): ";
-		for(i=0;i<n;i++) 
-		{ 
-		  cin>>shi[i]; 
-		   
-		}  
-	                sort(shi,shi+n); 
-			        cout<<endl;
-		            cout<<"Sorted array is :\n"; 
-		        for(i=0;i<n;i++){ 
-		             cout<<shi[i]<<"\t";
-	          	}
-		           
-
+{
+    int n,i;
+    cout<<"\nEnter the size of the array: ";
+    cin>>n;
+    int *shi= new int [n];
+    cout<<"Enter the elements of an array(with 0 & 1 & 2 only): ";
+    for(i = 0; i < n; i++ ) {
+        cin >> shi[i];
+    }
+    sort(shi,shi+n);
+    cout<<"\nSorted array is :\n";
+    for(i =0; i < n; i++) {
+        cout<<shi[i]<<"\t";
+    }
+    cout<<endl;
     return 0;
-} 
-   
+}
