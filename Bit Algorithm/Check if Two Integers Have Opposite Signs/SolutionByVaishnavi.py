f8974e4d117147eadb04074b5071d746d7a447cc
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-03 16:16:20 +0530

    Removed Errors

--- Bit Algorithm/Check if Two Integers Have Opposite Signs/SolutionByVaishnavi.py
+++ Bit Algorithm/Check if Two Integers Have Opposite Signs/SolutionByVaishnavi.py
@@ -2,9 +2,7 @@
 program implemented to check given two numbers have opposite signs or not
 '''
 def check_signs(x,y):
-    if x==0 or y==0:
-        return 'NO'
-    if x>0 and y>0:
+    if (x >= 0 and y >= 0) or ( x < 0 and y < 0 ):
        return 'NO'
     else:
         return 'YES'
