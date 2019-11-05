0408bab5be5719f6395b2b0b1f4a05cd875b3b84
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-05 19:13:22 +0530

    Updated Correct Solution

--- Basic/Reverse A String/SolutionByPrabhakar.cpp
+++ Basic/Reverse A String/SolutionByPrabhakar.cpp
@@ -4,32 +4,21 @@
 
 using namespace std;
 
-void rev(string str,int j)
-{
-
-    for(int i=j-1;i>=0;i--)
-    {
-        cout<<str[i];
+string rev(string str) {
+    int len = int(str.length()) - 1;
+    int i = 0;
+    while( i < len ) {
+        swap(str[i],str[len]);
+        i++;
+        len--;
     }
+    return str;
 }
 
-int length(string s)
-{int n=0;
-        for(int i = 0; s[i] != '\0'; ++i)
-        {
-            n++;
-        }
-
-            return n;
-}
-
-int main()
-{
-string str;
-int i;
-
-getline(cin,str);
-i=length(str);
-rev(str,i);//call reverser passing the count and string
-return 0;
+int main() {
+    string str;
+    cout << "\nEnter String\t:\t";
+    getline(cin,str);
+    cout << "\nRevesed String\t:\t" << rev(str) << endl;
+    return 0;
 }
