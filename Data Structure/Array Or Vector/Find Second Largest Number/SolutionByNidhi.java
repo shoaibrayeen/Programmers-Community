bbc36c18a7f89660c7477a71a00e0424e177b891
Nidhi Verma <nidhiverma1997@gmail.com>
2019-11-07 14:51:15 +0530

    Update SolutionByNidhi.java
    modified program code

--- Data Structure/Array Or Vector/Find Second Largest Number/SolutionByNidhi.java
+++ Data Structure/Array Or Vector/Find Second Largest Number/SolutionByNidhi.java
@@ -8,37 +8,31 @@ public class SecondLargestElement {
         if(array.length == 1){
             return -1;
         }
-        int secondLargestElementIndex;
-        int largestElementIndex;
+        int secondLargestElement = Integer.MIN_VALUE;
+        int largestElement = Integer.MIN_VALUE;
 
-        if(array[0] > array[1]) {
-            largestElementIndex = 0;
-            secondLargestElementIndex = 1;
-        }else {
-            largestElementIndex = 1;
-            secondLargestElementIndex = 0;
-        }
-
-
-        for(int i = 2; i < array.length; i++) {
-            if(array[largestElementIndex] < array[i]) {
-                secondLargestElementIndex = largestElementIndex;
-                largestElementIndex = i;
+        for(int i = 0; i < array.length; i++){
+            if(largestElement < array[i]){
+                secondLargestElement = largestElement;
+                largestElement = array[i];
+            }
+            else if(secondLargestElement < array[i] && array[i] != largestElement){
+                secondLargestElement = array[i];
             }
         }
-        return secondLargestElementIndex;
+
+        return secondLargestElement;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter size of array: ");
+        System.out.print("Enter size and elements of array: ");
         int size = in.nextInt();
-        System.out.println("Enter array elements:");
         int[] array = new int[size];
         for(int i = 0;  i < size; i++){
             array[i] = in.nextInt();
         }
-        int secondLargestElementIndex = getSecondLargestElementIndex(array);
-        System.out.println("Second largest element: " + (secondLargestElementIndex == -1 ? "No second largest element": array[secondLargestElementIndex]));
+        int secondLargestElement = getSecondLargestElementIndex(array);
+        System.out.println("Second largest element: " + (secondLargestElement != Integer.MIN_VALUE ? secondLargestElement: "No second largest element"));
     }
 }
