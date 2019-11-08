3867d93e1245bcaa12cc34465386b768dad2eff4
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-08 15:38:13 +0530

    Updated Code

--- Basic/Check if A String is Palindrome or Not/SolutionByGursewak.cpp
+++ Basic/Check if A String is Palindrome or Not/SolutionByGursewak.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
-#include <conio.h>
 using namespace std;
 
 int main(int argc, char const *argv[])
-{	
-	string str;
-	cout<<"Enter a Palindrome string.\n";
-	cin>>str;
-	for(int i=0; i<str.length()/2; i++){
-		if(str[i]==str[str.length()-i-1]){
-		}
-		else{
-			cout<<"String is not Palindrome.";
-			exit(0);
-		}
-	}
-	cout<<"String is Palindrome.";
-	return 0;
+{
+    string str;
+    cout<<"Enter a Palindrome string.\n";
+    cin>>str;
+    for(int i=0; i<str.length()/2; i++){
+        if(str[i] != str[str.length()-i-1]){
+            cout<<"String is not Palindrome.";
+            exit(0);
+        }
+    }
+    cout<<"String is Palindrome.";
+    return 0;
 }
