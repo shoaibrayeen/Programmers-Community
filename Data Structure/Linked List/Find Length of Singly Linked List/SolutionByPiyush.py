b9d019b5c5c75557436f906382d86b9a6d237c75
Piyush <40433598+knightowl2704@users.noreply.github.com>
2019-11-08 22:50:35 +0530

    Creating solution in python
    Python solution for finding the length of LinkedList

---
+++ Data Structure/Linked List/Find Length of Singly Linked List/SolutionByPiyush.py
@@ -0,0 +1,46 @@
+class Element:
+  def __init__(self, value):
+    self.value = value
+    self.next = None
+
+class Linkedlist:
+  def __init__(self):
+    self.head = None
+
+  def lengthoflist(self):
+    if self.head:
+      current = self.head
+      counter = 0
+      while current:
+        counter = counter + 1
+        current = current.next
+      return counter
+    else:
+      return 0
+
+  def append(self, new_data):
+    new_node = Element(new_data)
+    if self.head is None:
+      self.head = new_node
+      return
+    current = self.head
+    while (current.next):
+      current = current.next
+    current.next =  new_node
+
+  def output_list(self):
+    current_node = self.head
+    while current_node is not None:
+      print(current_node.value, end = "-->")
+      current_node = current_node.next
+    print('\n')
+    return
+    
+    
+L = Linkedlist()
+L.append(6)
+L.append(7)
+L.append(8)
+L.append(20)
+print(L.lengthoflist())
+L.output_list()
