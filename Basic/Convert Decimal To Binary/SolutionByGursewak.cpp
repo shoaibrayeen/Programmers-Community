2ebd95dc4e150a387bb3302317b43a7eae9e1eca
gursewak11 <rainugursewaksingh@gmail.com>
2019-11-08 16:53:19 +0530

    Contribution by Gursewak.

---
+++ Basic/Convert Decimal To Binary/SolutionByGursewak.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+#include <conio.h>
+using namespace std; 
+void decToBinary(int n) {
+    bool bNum[32]; 
+ 	int i = 0; 
+    while (n > 0) { 
+    	bNum[i] = n % 2; 
+        n = n / 2; 
+        i++; 
+    } 
+    for (int j = i - 1; j >= 0; j--) 
+        cout << bNum[j]; 
+} 
+int main() 
+{ 
+    int num;
+    cout<<"Enter a decimal number to convert to Binary.\n";
+    cin>>num;
+    if(num<=0){
+    	cout<<"Enter valid number";
+    }
+    else{
+    	decToBinary(num);
+    } 
+    return 0; 
+} 
