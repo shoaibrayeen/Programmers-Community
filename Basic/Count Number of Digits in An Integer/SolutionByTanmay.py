a816c92277a464e548ab3aaf59dfad31029aea32
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-09 20:01:32 +0530

    Updated Code

--- Basic/Count Number of Digits in An Integer/SolutionByTanmay.py
+++ Basic/Count Number of Digits in An Integer/SolutionByTanmay.py
@@ -1,9 +1,6 @@
-#To Count The Number Of Digits In An Integer
-
-while 1:
-    num = input("Enter The Number : ")
-    print(len(num))
-    choice = input('Wanna Test Another Number : ')
-    if choice == 'no' or 'No':
-        break
+def countDigits(num):
+    print("Number of Digits in the Number\t:\t" , len(num))
     
+
+num = input("Enter The Number : ")
+countDigits(num)
