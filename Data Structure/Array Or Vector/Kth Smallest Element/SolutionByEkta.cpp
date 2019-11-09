c3ed2750a5fcd3fbbbcbaa7b23725c1362e1d93a
Ekta Gupta <50185634+eKta37@users.noreply.github.com>
2019-11-10 00:06:25 +0530

    Create SolutionByEkta.cpp

---
+++ Data Structure/Array Or Vector/Kth Smallest Element/SolutionByEkta.cpp
@@ -0,0 +1,22 @@
+/*
+  program to find the Kth smallest element in the given array
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N, K;
+    cout<<"enter size of array"<<endl;
+    cin>>N;
+    int arr[N];
+    cout<<"enter elements of array"<<endl;
+    for(int n=0; n<N; n++){
+        cin>>arr[n];
+    }
+    cout<<"enter K"<<endl;
+    cin>>K;
+    sort(arr, arr+N);
+    cout<<K<<"th smallest element in the given array is : "<<arr[K-1]<<endl;
+    return 0;
+}
