25f38f04518b7d8e6ca2b5c1bae27472beb0fea7
Piyush <40433598+knightowl2704@users.noreply.github.com>
2019-11-09 13:43:08 +0530

    Creating SolutionByPiyush.py
    Creating SolutionByPiyush.py for reversing LinkedList in Python

---
+++ Data Structure/Linked List/Reverse A Singly Linked List/SolutionByPiyush.py
@@ -0,0 +1,47 @@
+class Element:
+  def __init__(self, value):
+    self.value = value
+    self.next = None
+
+class Linkedlist:
+  def __init__(self):
+    self.head = None
+
+  def reverselist(self):
+    prev = None
+    current = self.head
+    head = self.head
+    while(current!=None):
+      next = current.next
+      current.next = prev
+      prev = current
+      current = next
+    self.head = prev
+
+  def append(self, new_data): 
+    new_node = Element(new_data) 
+    if self.head is None: 
+      self.head = new_node 
+      return
+    last = self.head 
+    while (last.next): 
+      last = last.next
+    last.next =  new_node 
+
+  def output_list(self):    
+    current_node = self.head
+    while current_node is not None:
+      print(current_node.value, end = "-->")
+      current_node = current_node.next
+    print('\n')  
+    return
+    
+    
+L = Linkedlist()
+L.append(6)
+L.append(7)
+L.append(8)
+L.append(20)
+L.output_list()
+L.reverselist()
+L.output_list()
