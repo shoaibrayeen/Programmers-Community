652d92d621f77129c6bf8796595e7404173832ba
Mananpreet Kaur <manankaur00@gmail.com>
2019-11-11 12:34:50 +0530

    Create SolutionByMananpreet.java
    The submitted program finds the product of the digits of a number.

---
+++ Basic/Product of Digits in A Number/SolutionByMananpreet.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class ProductOfDigits
+{
+	private static int product(int n)
+	{
+	  int product = 1;
+		while(n>0)
+		{
+			product = product * (n%10);
+			n = n / 10;
+		}
+		return product;
+	}
+
+	public static void main(String[] args)
+	 {
+	  Scanner s = new Scanner(System.in);
+
+	  System.out.println("Enter a number :");
+
+	  int n = s.nextInt();
+
+	  System.out.print("The product of the digits of the entered number is: "+product(n));	
+	}
+}
