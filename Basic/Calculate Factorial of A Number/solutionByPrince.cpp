cd892e7277c8b087ee72b14044052b3cb6e8a8a0
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-11 22:11:09 +0530

    Intended and Bug Free

--- Basic/Calculate Factorial of A Number/solutionByPrince.cpp
+++ Basic/Calculate Factorial of A Number/solutionByPrince.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int fact(int x)
 {
 	if(x==1 or x==0)
-	return 1;
+		return 1;
 	else
-	return x*fact(x-1);
+		return x*fact(x-1);
 
 }
 int main()
@@ -13,6 +13,7 @@ int main()
 	int x;
 	cout<<"Enter a number which you want to get Factorial :"<<" ";
 	cin>>x;
-    int factorial =	fact(x);	
-	cout<<"The Factorial of  " <<x<<" is :- "<<" "<<factorial;
+    	int factorial =	fact(x);	
+	cout<<"The Factorial of  " <<x<<" is\t:\t "<<" "<<factorial;
+	return 0;
 }
