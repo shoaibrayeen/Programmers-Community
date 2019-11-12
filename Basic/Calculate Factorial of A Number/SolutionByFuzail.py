64ac3f0b2ee934e1fa5647e2acd885f0101b73d5
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-12 20:07:06 +0530

    Removed Bug

--- Basic/Calculate Factorial of A Number/SolutionByFuzail.py
+++ Basic/Calculate Factorial of A Number/SolutionByFuzail.py
@@ -1,13 +1,12 @@
 def factorial(n):
-
     if ( n == 0 ):
-         print ("0")
+         print ("1")
          
     elif (n<0):
         print ("enter a positive value")
         
     else :    
-        fact =1 
+        fact = 1 
         for a in range (1 ,n+1):
             fact = fact * a
         return(fact)
