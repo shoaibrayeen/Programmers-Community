fc48c7797932a71b88c7808a76dfff544d8a086c
Pooja <54528116+pooja-git11@users.noreply.github.com>
2019-11-13 14:33:12 +0530

    Create solutionByPooja.cpp

---
+++ Data Structure/Array Or Vector/Sort An Array of 0s , 1s and 2s/solutionByPooja.cpp
@@ -0,0 +1,49 @@
+/*
+    An Array is given with 0s, 1s and 2s and you need to sort the array.
+    Constraints
+                1 < size Of Array <= 10^6
+                0 <= Array Elements <= 2
+*/
+
+#include<iostream>
+using namespace std;
+
+int *sort(int *arr,int size)
+{
+    int temp,j;
+    for(int i=0;i<size;i++)
+    {
+        temp=arr[i+1];
+        j=i;
+        while(temp<arr[j] && j>=0)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=temp;
+    }
+    return arr;
+}
+
+int main()
+{
+    int size;
+    cout<<"Enter size ";
+    cin>>size;
+
+    if(size>0)
+    {
+        int *arr=new int[size];
+        cout<<"\nEnter the array element(only 0 ,1 and 2) ";
+        for(int i=0;i<size;i++)
+                cin>>arr[i];
+        arr=sort(arr,size);
+
+        for(int i=0;i<size;i++)
+                cout<<arr[i]<<" ";
+
+    }
+
+
+}
+
