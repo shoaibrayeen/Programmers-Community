fb9ebb44c0769f2c067341bcb3af8b30ca376ca9
Tanmay <54329103+Tanmay0808@users.noreply.github.com>
2019-11-13 23:50:18 +0530

    Update Code

--- Data Structure/Array Or Vector/Find Second Largest Number/SolutionByTanmay.py
+++ Data Structure/Array Or Vector/Find Second Largest Number/SolutionByTanmay.py
@@ -1,13 +1,21 @@
-# To Find Second Largest Number In An Array
+def sec_largest(arr):
+    '''
+       Objective  : To Find Second Largest Number In An Array
+       
+       Input      : Array - List Value
+       
+       Return     : Second Largest Number In An Array - Numeric Value
+       
+    '''
+    if len(arr)==1:
+        return "No Second Largest Element"
+    else:
+        arr = list(set(arr))
+        arr.insert(0,arr.pop(arr.index(max(arr))))
+    return "Second Largest Element  : " + str(max(arr[1:]))
 
-n = int(input("Enter Size Of Array : "))
-print("Enter The Elements : ")
+
+n = int(input("Enter Array Size : "))
+print("Enter Array Elements : ")
 c = [int(input()) for i in range(n)]
-if len(c)==1:
-    print("No Second Largest Number")
-else:
-    c=list(set(c))
-    num = c.pop(c.index(max(c)))
-    c.insert(0,num)
-    print("The Second Largest Number Is : ", max(c[1:]))
-    
+sec_largest(c)
