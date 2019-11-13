014858c3cc27b592d8a72be595918cfdfca5f351
Tanmay <54329103+Tanmay0808@users.noreply.github.com>
2019-11-13 23:27:11 +0530

    Update Code

--- Data Structure/Array Or Vector/Product of Elements in An Array/SolutionByTanmay.py
+++ Data Structure/Array Or Vector/Product of Elements in An Array/SolutionByTanmay.py
@@ -1,9 +1,18 @@
-#To Find Product Of Elements In An Array
-
+def prod_array(arr,mul=1):
+    '''
+       Objective  : To Find The Product Of Elements In An Array
+       
+       Input      : Array - List Value
+       
+       Return     : Product Of Elements In Array - Numeric Value
+       
+    '''
+    
+    for elem in arr:
+        mul*=elem
+    return "Product Of All Elements Is : " + str(mul)
+    
 n = int(input("Enter Array Size : "))
 print("Enter Array Elements : ")
 c = [int(input()) for i in range(n)]
-mul=1
-for elem in c:
-    mul*=elem
-print("Product Of All Elements Is : ", mul)
+prod_array(c)
