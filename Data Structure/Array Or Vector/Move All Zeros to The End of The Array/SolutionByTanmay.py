0292cc1c47a7ef466c93795a26b475de97261a67
Tanmay <54329103+Tanmay0808@users.noreply.github.com>
2019-11-13 23:35:45 +0530

    Update Code

--- Data Structure/Array Or Vector/Move All Zeros to The End of The Array/SolutionByTanmay.py
+++ Data Structure/Array Or Vector/Move All Zeros to The End of The Array/SolutionByTanmay.py
@@ -1,10 +1,21 @@
-#To Move All Zeros To The End Of The Array
-
-n = int(input("Enter Size Of Array : "))
-print("Enter The Elements : ")
+def modify_array(arr):
+    '''
+       Objective  : To Move All Zeroes To The End Of The Array
+       
+       Input      : Array - List Value
+       
+       Return     : Array With All Zeroes At The End 
+       
+    '''
+    
+    for elem in arr:
+        if elem==0:
+            arr.append(arr.pop(arr.index(elem)))
+        else:
+            continue
+    return "Resultant Array  : " + str(arr)
+    
+n = int(input("Enter Array Size : "))
+print("Enter Array Elements : ")
 c = [int(input()) for i in range(n)]
-for elem in c:
-    if elem==0:
-        c.append(c.pop(c.index(elem)))
-print("Final Array : ")
-print(c)
+modify_array(c)
