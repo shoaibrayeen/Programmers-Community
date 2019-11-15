453a331d38e5db569351de38b43f49049657396d
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-15 16:37:20 +0530

    Intended

--- Bit Algorithm/Check if A Number is Power of Two/Solutionbyvaishi1411.cpp
+++ Bit Algorithm/Check if A Number is Power of Two/Solutionbyvaishi1411.cpp
@@ -1,18 +1,18 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
+
 bool powOftwo(int );
-   int main(){ 
-			int shi;
-			cout<<"Enter the number: ";
-			cin>>shi;
-					
-					powOftwo(shi) ?cout<<"YES"<<endl : cout<<"NO";
-					 
-  return 0;
-   }
-    bool powOftwo(int v){ 
-						if(v==0) 
-						   return false; 
-						  
-						   return (ceil(log2(v)) == floor(log2(v)));
-       }
+int main(){
+    int shi;
+    cout<<"Enter the number: ";
+    cin>>shi;
+    powOftwo(shi) ?cout<<"YES"<<endl : cout<<"NO";
+    return 0;
+}
+
+bool powOftwo(int v) {
+    if(v==0)
+        return false;
+    return (ceil(log2(v)) == floor(log2(v)));
+}
