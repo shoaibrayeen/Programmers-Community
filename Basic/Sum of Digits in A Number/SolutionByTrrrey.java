3b80f130cde976be1f0c437e7c4bec34bbee8ae3
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-17 21:28:22 +0530

    Added User Input

--- Basic/Sum of Digits in A Number/SolutionByTrrrey.java
+++ Basic/Sum of Digits in A Number/SolutionByTrrrey.java
@@ -1,26 +1,28 @@
-// Java program to compute the sum of digits in a number.
+import java.util.Scanner;
 
-import java.io.*; 
-  
-class Main { 
-      
-    static int sumDigits(int n) 
-    {     
-        int sum = 0; 
-          
-        while (n != 0) 
-        { 
-            sum = sum + n % 10; 
-            n = n/10; 
-        } 
-      
-    return sum; 
-    } 
-  
+ class Main { 
 
-    public static void main(String[] args) 
-    { 
-        int n = 123456;   
-        System.out.println(sumDigits(n)); 
-    } 
-} 
+     static int sumDigits(int n) 
+     {     
+         int sum = 0; 
+
+         while (n != 0) 
+         { 
+             sum = sum + n % 10; 
+             n = n/10; 
+         } 
+
+     return sum; 
+     } 
+
+
+     public static void main(String[] args) 
+     { 
+       Scanner s = new Scanner(System.in);
+
+	    System.out.print("\nEnter a number\t:\t");
+
+	    int n = s.nextInt();
+      System.out.println("\nSum of Digits\t:\t" + sumDigits(n)); 
+     } 
+ } 
