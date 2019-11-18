b12acc5edfe3c362cc223ab50f293ee706a4dc6f
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-18 22:06:00 +0530

    Updated Code

--- Data Structure/Array Or Vector/Product of Elements in An Array/SolutionByRishabhSharma.cpp
+++ Data Structure/Array Or Vector/Product of Elements in An Array/SolutionByRishabhSharma.cpp
@@ -1,40 +1,34 @@
 #include<iostream>
 using namespace std;
 
-float ProductOfArray(int *a,int size)
+long int ProductOfArray(int *a,int size)
 {
 
-	float product=1;
+   long int product = 1;
 
-	for (int i = 0; i < size; i++)
-	{
-		
-		product *=a[i];
+   for (int i = 0; i < size; i++)
+   {
 
-	}
+       product *=a[i];
 
-return product;
+   }
+
+    return product;
 
 }
 
 int main()
 {
- 
- int n;
+    int n;
+    cout<<"Enter Size"<<endl;
+    cin>>n;
+    int *a=new int(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+        
+    }
 
- cout<<"Enter Size"<<endl;
-
- cin>>n;
-
-int *a=new int(n);
-
-for (int i = 0; i < n; i++)
-{
-
-	cin>>a[i];
-
+    cout<<ProductOfArray(a,n)<<endl;
+    return 0;
 }
-
-cout<<ProductOfArray(a,n)<<endl;
-
-
