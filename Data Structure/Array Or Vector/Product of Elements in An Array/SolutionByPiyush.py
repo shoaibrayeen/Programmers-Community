19f48adfacd3175a78271d9a90fb25c7f1a1b03e
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-19 19:25:46 +0530

    Updated Code

--- Data Structure/Array Or Vector/Product of Elements in An Array/SolutionByPiyush.py
+++ Data Structure/Array Or Vector/Product of Elements in An Array/SolutionByPiyush.py
@@ -1,10 +1,11 @@
 print('Enter the elements of the array (Space Seperated) : ')
 v = [int(i) for i in input().split(" ")]
-if 0 in v:
-  print(0)
-else:
-  product = 1
-  for i in v:
-    product = product*i  
-  print(product)
+
+def product(v):
+    product = 1
+    for i in v:
+        product = product*i  
+    print(product)
   
+
+product(v)
