55509cef8e890215ad1de48506dc737f1529395f
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-12-06 16:17:12 +0530

    Removed Bug and Indented

--- Data Structure/Array Or Vector/Product of Elements in An Array/SolutionByDhruv.cpp
+++ Data Structure/Array Or Vector/Product of Elements in An Array/SolutionByDhruv.cpp
@@ -1,27 +1,29 @@
 /* Program to calculate product of elements in an array*/
-#include<iostream.h>
-#include<conio.h>
+#include<iostream>
+using namespace std;
+
 void product(int a[], int n);                         //Declaring Prototype of the function product
-void main()
-{
- int a[10], n;
- cout<<"Enter no. of elements:";
- cin>>n;
- for(int i=0;i<n;i++)
- {
-  cout<<"Enter element in array:";
-  cin>>a[i];
-  }
- product(a,n);                                       //Calling function product
- getch();
+
+int main() {
+    int n;
+    cout<<"Enter no. of elements:";
+    cin>>n;
+    int *a = new int[n];
+    cout<<"Enter element in array:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    product(a,n);
+    return 0;
 }
 
 void product(int a[], int n)                         //Defining function product
 {
-  long int product = 1;
-  for(int i=0;i<n;i++)
-  {
-   product *= a[i];
-   }
-  cout<<"Product of elements are:"<<product;
+    long int product = 1;
+    for(int i=0;i<n;i++)
+    {
+        product *= a[i];
+    }
+    cout<<"Product of elements are:"<<product;
 }
