116c22e21e6f3d69e1ef45e61a19aca31cc4ba51
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-12-08 03:37:55 +0530

    Removed Bug

--- Basic/Check Leap Year/solutionsByrithgan.cpp
+++ Basic/Check Leap Year/solutionsByrithgan.cpp
@@ -5,13 +5,18 @@ int main(){
     cout<<"Enter a year ";
     int y;
     cin>>y;
-    if(y%4==0)
-        cout<<"leap year";
-    else if(y%100==0 || y%400==0)
-        cout<<"leap year";
-    else
-    {
-        cout<<"not a leap year";
+    
+    if( y % 400 == 0 ) {
+        cout<<"\nLeap Year\n";
+    }
+    else if ( y % 100 == 0 ) {
+        cout<<"\nNot a Leap Year\n";
+    }
+    else if ( y % 4 == 0 ) {
+        cout<<"\nLeap Year\n";
+    }
+    else {
+        cout<<"\nNot a Leap Year\n";
     }
     return 0;
 }
