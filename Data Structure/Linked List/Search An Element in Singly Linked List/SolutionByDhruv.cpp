62ad61623e7a3c455a803aeeb761a4dfbe699462
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-12-10 09:10:55 +0530

    Removed Bugs

--- Data Structure/Linked List/Search An Element in Singly Linked List/SolutionByDhruv.cpp
+++ Data Structure/Linked List/Search An Element in Singly Linked List/SolutionByDhruv.cpp
@@ -1,56 +1,64 @@
 /* Program to search the specified element in a given linked list*/
-#include<iostream.h>
-#include<conio.h>
-#include<string.h>
-int check(int element, node *start);
-struct node
-{
- int data;
- node *next;
- }*temp, *t;                                  // Pointer defined for linked list
+#include<iostream>
+using namespace std;
 
-void main()
-{
- int element, searched_element;
- node *start = NULL;
- temp = new node();
- cout<<"Enter element";
- cin>>temp->data;
- if(start == NULL)
- {
-   start = temp;
-   }
-   else
-   {
-     t = start;
-    // t = t->next;
-     t->next = temp;
-     }
-   cout<<"/nEnter element to be searched";
-   cin>>element;
-   searched_element = check(element, *start);
-   if(searched_element == 1)
-   {
-     cout<<"Element found";
-     }
-     else
-     {
-      cout<<"Element not found";
-      }
- getch();
+struct node {
+    int data;
+    node *next;
+}*t;                                  // Pointer defined for linked list
+
+int check(int element,node *start) {
+    t = start;
+    while(t != NULL) {
+        if(t->data == element) {
+            return 1;
+        }
+        t = t->next;
+    }
+    return 0;
 }
 
-int check(int element,node *start)                    // Function to check the availability of the element in the list
-{
- t = start;
- while(t!=NULL)
- {
-   if(t->data == element)
-   {
-     return 1;
-     t=t->next;
-     }
 
-   return 0;
-  }
+
+
+int main() {
+    int element, searched_element;
+    node *start = NULL;
+    cout<<"Enter element\n(For Stopptin Enter -1\n";
+    while(true) {
+        cin >> element;
+        if ( element == -1 ) {
+            break;
+        }
+        node* temp = new node();
+        temp->data = element;
+        temp->next = NULL;
+        
+        if(start == NULL) {
+            start = temp;
+        }
+        else {
+            t = start;
+            while(t->next != NULL) {
+                t = t->next;
+            }
+            t->next = temp;
+        }
+    }
+    if ( start == NULL) {
+        cout << "\nList is Empty\n";
+        return 0;
+    }
+    cout <<"\nEnter element to be searched\t:\t";
+    cin >> element;
+    searched_element = check(element, start);
+    if(searched_element == 1) {
+        cout<<"Element found\n";
+    }
+    else {
+        cout<<"Element not found\n";
+    }
+    return 0;
 }
+
+
