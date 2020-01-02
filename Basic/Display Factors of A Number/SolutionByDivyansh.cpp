306cd765e65f0eb4954dbd388267b78fc360d2f6
Divyansh-747 <divyansh.1999.dr@gmail.com>
2020-01-02 19:10:19 +0530

    Create SolutionByDivyansh.cpp

---
+++ Basic/Display Factors of A Number/SolutionByDivyansh.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int Dispfact(int N)
+{
+	for(int i=1;i<=N;i++)
+	{
+		if(N%i==0)
+		{
+			cout<<i<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+int main()
+{
+	int N;
+
+	cout<<"Enter number to find it's factors: "<<endl;
+	cin>>N;
+	
+	cout<<"Factors of "<<N<<endl;
+	Dispfact(N);
+
+	return 0;
+}
