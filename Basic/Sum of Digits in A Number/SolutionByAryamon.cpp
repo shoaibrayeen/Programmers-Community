e3b209cdaeb3f0f00da5eb328e759e4d36c1fabf
aryamonchattopadhyay <59326794+aryamonchattopadhyay@users.noreply.github.com>
2020-01-02 11:58:28 +0530

    Sum of Digits of a Number - Turbo C++

---
+++ Basic/Sum of Digits in A Number/SolutionByAryamon.cpp
@@ -0,0 +1,24 @@
+//to display the sum of digits of a number
+#include<iostream.h>
+#include<conio.h>
+int sum(int n)
+{
+	int add=0;
+	for(int i=n;i!=0;i/=10)
+	{
+		 add=add+(i%10);
+	}
+	return add;
+}
+
+void main()
+{
+	int num;
+	clrscr();
+	/******************input*********************************/
+	cout<<"enter the number ";
+	cin>>num;
+	cout<<"\nSum is \n"<<sum(num);
+	getch();
+
+}
