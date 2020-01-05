b3477c8e2eab49064cf5817e55e3b0430cfc5105
Pooja <pooja016mca19@igdtuw.ac.in>
2020-01-06 01:54:10 +0530

    Create solutionbyPooja.cpp

---
+++ Bit Algorithm/Check if Two Integers Have Opposite Signs/solutionbyPooja.cpp
@@ -0,0 +1,35 @@
+/*
+    Two numbers is given and you need to Check if they Have Opposite Signs.
+    Constraints:
+                Number could be both negative and positive.
+*/
+
+#include<iostream>
+using namespace std;
+
+bool sign(int n1,int n2)
+{
+    int r=n1^n2;      //xor of oposite sign number  is always negative
+
+    if(r>0)
+        return 0;
+
+    return 1;
+
+}
+
+int main()
+{
+   int num1,num2;
+
+   cout<<"Enter 1st number ";
+   cin>>num1;
+   cout<<"Enter 2nd number ";
+   cin>>num2;
+
+   bool check=sign(num1,num2);
+   if(check==1)
+        cout<<"\nYES!They have opposite sign";
+   else
+        cout<<"\nNO";
+}
