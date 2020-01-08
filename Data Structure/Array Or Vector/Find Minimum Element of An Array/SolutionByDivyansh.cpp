81f1019c24ce2974967ce435a508fe0ff01074b8
Divyansh-747 <divyansh.1999.dr@gmail.com>
2020-01-08 16:17:49 +0530

    Create SolutionByDivyansh.cpp

---
+++ Data Structure/Array Or Vector/Find Minimum Element of An Array/SolutionByDivyansh.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int minElement(int a,int b){
+	return (a>b)?b:a;
+}
+
+int main()
+{
+	int A[100],size,min;
+
+	cout<<"Enter size of array: "<<endl;
+	cin>>size;
+
+	cout<<"Enter elements of array: "<<endl;
+	for(int i=0;i<size;i++)
+	{
+		cin>>A[i];
+	}
+
+	min=A[0];
+	
+	for(int i=0;i<size;i++)
+	{
+		min=minElement(min,A[i]);
+	}
+
+	cout<<"Minimum element in array: "<<min<<endl;
+
+	return 0;
+
+}
