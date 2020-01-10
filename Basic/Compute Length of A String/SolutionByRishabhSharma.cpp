0d689479b10ed969639df6e7725ea95dee7d4f86
Rishabh Sharma <48138906+rishiar4@users.noreply.github.com>
2020-01-11 02:49:32 +0530

    updated using user defined solution

--- Basic/Compute Length of A String/SolutionByRishabhSharma.cpp
+++ Basic/Compute Length of A String/SolutionByRishabhSharma.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
-
+	int length=0;
 	string str;
 	cin>>str;
-	int l = str.length();
-	cout<<l;
 
-
+	for(int i=0;str[i]!='\0';i++)
+		length++;
+	cout<<length;
 }
