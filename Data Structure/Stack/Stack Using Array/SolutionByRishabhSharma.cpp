2b1fd2f9d6cadb4ef45a567dd243af268d7a9e52
Rishabh Sharma <48138906+rishiar4@users.noreply.github.com>
2020-01-23 01:29:11 +0530

    changes made

--- Data Structure/Stack/Stack Using Array/SolutionByRishabhSharma.cpp
+++ Data Structure/Stack/Stack Using Array/SolutionByRishabhSharma.cpp
@@ -1,69 +1,70 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int q[1000] ={0},f=0,e=0;
+int s[10000]= {0},top=-1;
 
-void Enqueu(int n){
-	if(e==100)
-		cout<<"Overflow Condition "<<endl;
-	else
-		q[e++]=n;
-}
-int Dequeue()
+void push(int x)
 {
-	if(f==e){
-		cout<<"Underflow Condition "<<endl;
-		return 0;
-	}
+	top++;
+	if(top==10000)
+		cout<<"Overflow Condition ";
 	else
-	{
-		return q[f++];
-
-	}
+		s[top]=x;
+}
+int pop()
+{
+	if(top<0)
+		cout<<"Underflow Condition ";
+	else
+		return s[top--];
+	return 0;
 }
 void display()
 {
-	for (int i = f; i < e; i++)
+	for (int i = 0; i <=top; i++)
 	{
 		/* code */
-		cout<<q[i]<<" ";
+		cout<<s[i]<<" ";
+
 	}
 	cout<<endl;
 }
+
 int main()
 {
 	int ch;
-	cout<<"Queue using Array "<<endl;
 	do{
-		
-		cout<<"1 Insertion into Queue "<<endl;
-		cout<<"2 Deletion from Queue "<<endl;
+		cout<<"Stack using Array "<<endl;
+		cout<<"1 Add a number "<<endl;
+		cout<<"2 Delete a number "<<endl;
 		cout<<"3 Display "<<endl;
 		cout<<"4 Exit "<<endl;
 		cin>>ch;
+
 		switch(ch)
 		{
 			case 1:
-			    {
-			    	cout<<"Enter the number to add in Queue "<<endl;
-			    	int n ;
-			    	cin>>n;
-			    	Enqueu(n);
-			    }
-			    break;
+			   {
+			   	int x;
+			   	cout<<"Enter the number "<<endl;
+			   	cin>>x;
+
+			   	push(x);
+			   }
+			   break;
+
 			case 2:
 			{
-				cout<<"The Deleted number "<<Dequeue()<<endl;
+				cout<<"Deleted Nmber "<<pop()<<endl;
 
 			}
-			break;
 
 			case 3:
 			{
 				display();
+
 			}
 			break;
-
 		}
 	}while(ch!=4);
 }
