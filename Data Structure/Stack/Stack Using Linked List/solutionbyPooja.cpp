8def97838c6766b4b744b6861fd2e45062720bf3
Pooja <pooja016mca19@igdtuw.ac.in>
2020-02-06 21:24:51 +0530

    Create solutionbyPooja.cpp

---
+++ Data Structure/Stack/Stack Using Linked List/solutionbyPooja.cpp
@@ -0,0 +1,104 @@
+/*
+    Write a program to implement Stack using Linked List..
+
+    Functions to implement
+        Push (Insertion)
+        Pop (Deletion)
+
+*/
+#include<iostream>
+using namespace std;
+
+struct linkedlist
+{
+  int data;
+  linkedlist *next;
+
+};
+class stack_linkedList
+{
+    public:
+    linkedlist *top;
+    stack_linkedList()
+    {
+        top=NULL;
+    }
+    void push(int);
+    int pop();
+    void display();
+
+};
+void stack_linkedList::push(int ele)
+{
+    linkedlist *temp=new linkedlist();
+    temp->data=ele;
+    temp->next=NULL;
+
+    if(top==NULL)
+        top=temp;
+    else
+    {
+        temp->next=top;
+        top=temp;
+    }
+}
+int stack_linkedList::pop()
+{
+    linkedlist *temp;
+    int ele;
+    if(top==NULL)
+        cout<<"\nStack is empty";
+    else
+    {
+        temp=top;
+        ele=temp->data;
+        top=top->next;
+        delete(temp);
+    }
+    return ele;
+}
+void stack_linkedList::display()
+{
+    if(top==NULL)
+        cout<<"\nStack is empty";
+    else
+    {
+        linkedlist *temp;
+        temp=top;
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+}
+
+int main()
+{
+    int op,data;
+    stack_linkedList ob;
+    cout<<"\n1. Push(Insertion) ";
+    cout<<"\n2. Pop(Deletion)";
+    cout<<"\n3. Display";
+    cout<<"\n4. Exit";
+    while(1)
+    {
+        cout<<"\nEnter your choice ";
+        cin>>op;
+        if(op==1)
+        {
+            cout<<"Enter data";
+            cin>>data;
+            ob.push(data);
+        }
+        else if(op==2)
+            data=ob.pop();
+        else if(op==3)
+            ob.display();
+        else if(op==4)
+            exit(0);
+        else
+            cout<<"\nWrong choice ";
+
+    }
+}
