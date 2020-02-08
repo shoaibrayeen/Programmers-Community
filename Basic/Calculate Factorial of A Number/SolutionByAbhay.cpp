44d014eb25a075b557d339d250928f8c0c652d97
abhayagarwal5 <abhayag1998@gmail.com>
2020-02-08 11:49:30 +0530

    new solution added

---
+++ Basic/Calculate Factorial of A Number/SolutionByAbhay.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int factorial(int n){
+	if(n==1|| n==0 ){
+		return n;
+	}
+	
+	return n*factorial (n-1);
+}
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int a;
+		cin>>a;
+		cout<<factorial(a);
+	}
+	return 0;
+}
