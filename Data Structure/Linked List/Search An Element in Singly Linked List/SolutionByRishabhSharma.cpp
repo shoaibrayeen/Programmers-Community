f51d255d1da617e775477c02b8c6792aac523f21
Rishabh Sharma <48138906+rishiar4@users.noreply.github.com>
2020-02-08 22:37:03 +0530

    SolutionByRishabhSharma.cpp

---
+++ Data Structure/Linked List/Search An Element in Singly Linked List/SolutionByRishabhSharma.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+	int data;
+	Node *next;
+
+	Node(int x){
+		data = x;
+		next = NULL;
+	}
+
+};
+Node* takeInput()
+{
+	Node *head = NULL, *tail = NULL;
+	char ch;
+	do{
+		int x;
+	cout<<"Enter Value "<<endl;
+	cin>>x;
+	Node *new_node = new Node(x);
+	if(head==NULL)
+	{
+		head = new_node;
+		tail=new_node;
+	}
+	else{
+		tail->next = new_node;
+		tail = new_node;
+	}
+	cout<<"ADD more ? "<<endl;
+	cin>>ch;
+
+
+}while(ch!='n');
+
+return head;
+
+}
+
+void Search(Node *head,int k)
+{
+while(head!=NULL)
+{
+	if(head->data == k)
+	{
+		cout<<"Yes"<<endl;
+		return;
+	}
+	head = head->next;
+}
+cout<<"No"<<endl;
+return;
+}
+void Display(Node *head)
+{
+	while(head!=NULL)
+	{
+		cout<<head->data<<"  ";
+		head = head->next;
+	}
+}
+int main()
+{
+	Node *head = takeInput();
+
+	cout<<"Enter the Number to Search "<<endl;
+	int x;
+	cin>>x;
+	Search(head,x);
+	
+
+	return 0;
+}     
