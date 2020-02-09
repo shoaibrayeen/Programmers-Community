61453c597383fa1eee52020ee9c09c09029b8734
abhayagarwal5 <abhayag1998@gmail.com>
2020-02-09 20:08:01 +0530

    Solution by abhay

---
+++ Basic/Check if A String Contains Only Digits/SolutionByAbhay.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ bool isDigit(string str){
+ 	for(int i=0;i<str.length();i++){
+ 		if(isdigit(str[i])){
+ 			continue;
+ 		}
+ 		else{
+ 			return false;
+ 		}
+ 	}
+ 	return true;
+ }
+ int main(){
+ 	int t;
+ 	cin>>t;
+ 	while(t--){
+ 		string str;
+ 		cin>>str;
+ 		bool b= isDigit(str);
+ 		if(b){
+ 			cout<<"Yes";
+ 		}
+ 		else{
+ 			cout<<"No";
+ 		}
+ 	}
+ 	return 0;
+ }
