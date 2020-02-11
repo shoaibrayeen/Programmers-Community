a30e120259e60ef8aaf959eecd1567d404ecd6b6
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2020-02-11 12:39:27 +0530

    Removed Bug

--- Basic/Calculate Nth Tribonacci Number/SolutionByAbhay.cpp
+++ Basic/Calculate Nth Tribonacci Number/SolutionByAbhay.cpp
@@ -1,8 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int tribonacci(int n){
-	if(n==1 || n==0){
-		return n;
+	if(n==2 || n==1){
+		return 0;
+	}
+	if ( n == 3) {
+	    return 1;
 	}
 	return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
 }
