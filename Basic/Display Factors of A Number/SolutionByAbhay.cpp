201342e990de5d2c4f6f474ed65cbc5ef6ba208c
abhayagarwal5 <abhayag1998@gmail.com>
2020-02-12 17:59:27 +0530

    Solution by abhay

---
+++ Basic/Display Factors of A Number/SolutionByAbhay.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Factors(int n){
+	cout<<"1"<<",";
+	for(int i=2;i<=n/2;i++){
+		if(n%i==0){
+			cout<<i<<",";
+		}
+	}
+	cout<<n<<endl;;
+}
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int num;
+		cin>>num;
+		if(num>=0){
+			if(num==0){
+				cout<<"0"<<endl;
+			}
+			else{
+				Factors(num);	
+			}
+			
+		}
+		else{
+			cout<<"Enter a positive number";
+		}
+	}
+	return 0;
+}
