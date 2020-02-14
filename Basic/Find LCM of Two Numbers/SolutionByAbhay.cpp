abd30019f99bc6a8e75bc691e70ae11d206ae9b7
abhayagarwal5 <abhayag1998@gmail.com>
2020-02-14 17:23:30 +0530

    Solution By Abhay

---
+++ Basic/Find LCM of Two Numbers/SolutionByAbhay.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void LCM(int n1,int n2){
+    // maximum value between n1 and n2 is stored in max
+    int max = (n1 > n2) ? n1 : n2;
+    do
+    {
+        if (max % n1 == 0 && max % n2 == 0)
+        {
+            cout << "LCM = " << max;
+            break;
+        }
+        else
+            ++max;
+    } while (true);
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n1, n2, max;
+        cout << "Enter the numbers: ";
+        cin >> n1 >> n2;
+        LCM(n1,n2);
+    }
+    return 0;
+        
+}
