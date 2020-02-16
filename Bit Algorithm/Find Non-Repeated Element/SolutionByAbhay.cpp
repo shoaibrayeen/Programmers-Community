68356995cc0b04ea446d9220911f2544c793a14c
abhayagarwal5 <abhayag1998@gmail.com>
2020-02-16 19:52:23 +0530

    Solution by abhay

---
+++ Bit Algorithm/Find Non-Repeated Element/SolutionByAbhay.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int repeatedElements(int a[], int n){
+	int b=a[0];
+	for(int i=1;i<n;i++){
+		b^=a[i];
+	}
+	return b;
+}
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int a[n];
+
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+		}
+		cout<<repeatedElements(a,n);
+	}
+	return 0;
+}
