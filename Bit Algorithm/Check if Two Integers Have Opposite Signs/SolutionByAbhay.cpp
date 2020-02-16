892195f411a47f228dcae5d95364c6134df5c24c
abhayagarwal5 <abhayag1998@gmail.com>
2020-02-16 19:16:57 +0530

    Solution by abhay

---
+++ Bit Algorithm/Check if Two Integers Have Opposite Signs/SolutionByAbhay.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+int oppoSign(int a,int b){
+	return((a^b)>>31);
+}
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int a,b;
+		cin>>a>>b;
+		if(oppoSign(a,b)){
+			cout<<"Yes"<<endl;
+		}
+		else{
+			cout<<"No"<<endl;
+		}
+	}
+	return 0;
+}
