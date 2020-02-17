376bbd975871956f2e5586413cb02bd6e8ff9188
Rishabh Sharma <48138906+rishiar4@users.noreply.github.com>
2020-02-18 01:59:28 +0530

    SolutionByRishabhSharma.cpp

---
+++ Data Structure/Linked List/Find Middle Element of The Singly Linked List/SolutionByRishabhSharma.cpp
@@ -0,0 +1,73 @@
+/*
+    A linked list is given and you need to find middle element of the linked list.
+    If the length of the linked list is even, You can return element at Size/2 Position.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+	int data;
+	Node *next;
+	Node(int x)
+	{
+		data = x;
+		next = NULL;
+	}	
+};
+
+Node *takeInput()
+{
+	Node *head = NULL, *tail=NULL;
+	char ch;
+
+	do{
+		cout<<"Enter the Value to add "<<endl;
+		int x;
+		cin>>x;
+		Node *new_node = new Node(x);
+		if(head == NULL)
+		{
+			head = new_node;
+			tail = new_node;
+		}
+		else
+		{
+			tail->next = new_node;
+			tail = new_node;
+		}
+
+		cout<<"Do you wish to add more ? (y/n) "<<endl;
+		cin>>ch;
+
+	}while(ch!='n');
+return head;
+}
+void Display(Node *head)
+{
+	while(head!=NULL)
+	{
+		cout<<head->data<<" ";
+		head = head->next;
+	}
+}
+void MiddleElement(Node *head)
+{
+	Node *fast = head->next, *slow = head;
+	while(fast!=NULL && fast->next!=NULL)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+
+	cout<<endl;
+	cout<<"Middle Element "<< slow->data<<endl;
+}
+
+int main()
+{
+	Node *head = takeInput();
+    Display(head);
+	MiddleElement(head);
+}
