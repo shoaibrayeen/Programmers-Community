2c027478fb80ba346f83cda303d0226d114da4bb
Pooja <pooja016mca19@igdtuw.ac.in>
2020-03-01 19:40:31 +0530

    Create solutionbyPooja.cpp

---
+++ Data Structure/Array Or Vector/Insertion Sort/solutionbyPooja.cpp
@@ -0,0 +1,35 @@
+/*
+    An Array is given and You need to sort it using Insertion Sort Algorithm.
+*/
+#include<iostream>
+using namespace std;
+
+void insertion_sort(int *arr,int n)
+{
+    int i,j,temp;
+    for(i=1;i<n;i++)
+    {
+        temp=arr[i];
+        for(j=i-1;j>=0&&temp<arr[j];j--)
+            arr[j+1]=arr[j];
+        arr[j+1]=temp;
+    }
+}
+
+int main()
+{
+    int size;
+    cout<<"Enter the size ";
+    cin>>size;
+
+    int *arr=new int[size];
+    cout<<"\nEnter the element of array ";
+    for(int i=0;i<size;i++)
+        cin>>*(arr+i);
+
+    insertion_sort(arr,size);
+
+    cout<<"\nElement after sorting ";
+    for(int i=0;i<size;i++)
+        cout<<arr[i]<<" ";
+}
