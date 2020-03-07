e5202e24db3a00f761e673c8598a7aa65d6de82e
Riya Rakshit <46257735+RakshitRiya@users.noreply.github.com>
2020-03-07 19:44:01 +0530

    Create SolutionByRiya.py

---
+++ Data Structure/Queue/Queue Using Linked List/SolutionByRiya.py
@@ -0,0 +1,62 @@
+#creating a node
+class Node:
+    def __init__(self,data):
+        self.value=data
+        self.next=None
+        
+#creating LinearQueue class
+class LinearQueue:
+    def __init__(self):
+        self.front=None
+        self.rear=None
+    def enqueue(self, newelement):
+        newnode=Node(newelement)
+        if(self.rear is None):
+            self.front=newnode
+            self.rear=newnode
+            print("Element enqueued successfully!")
+            return;
+        self.rear.next=newnode
+        self.rear=newnode
+        print("Element enqueued successfully!")
+        return;
+    def dequeue(self):
+        if self.front is None and self.rear is None:
+            print("Queue is empty! ")
+            return;
+        temp=self.front
+        self.front=self.front.next
+        if(self.front==None):
+            self.rear=None
+        print("Element dequeued successfully:",temp.value)
+        del temp
+        return;
+    #method to print the Queue
+    def printQueue(self): 
+        temp = self.front 
+        if temp is None:
+            print("Queue is empty")
+            return;
+        while (temp): 
+            print (temp.value) 
+            temp = temp.next
+            
+    #method which is basically used as a switcher
+    def switch_case_implementation(self):
+        print("Enter:\n1. To Enqueue an element\n2. To Dequeue element\n3. Print the queue\n4. Exit")
+        while(True):
+            choice=int(input("Enter Your Choice:"))
+            if choice==1:
+                insertitem=int(input("Enter your element"))
+                self.enqueue(insertitem)
+            if choice==2:
+                self.dequeue()
+            if choice==3:
+                self.printQueue()
+            if choice==4:
+                print("Exiting")
+                return;
+#main
+if __name__=="__main__":
+    myQueue=LinearQueue()
+    myQueue.switch_case_implementation()
