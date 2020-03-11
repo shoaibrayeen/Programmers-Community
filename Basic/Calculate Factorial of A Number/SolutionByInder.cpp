4359104f820e8e75f1ad9aa8da72c2c6d1dbfcf9
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2020-03-11 10:30:01 +0530

    Fixed Bug and Handled all test cases

--- Basic/Calculate Factorial of A Number/SolutionByInder.cpp
+++ Basic/Calculate Factorial of A Number/SolutionByInder.cpp
@@ -2,18 +2,34 @@
 using namespace std;
 int factorial (int v)
 {
-    if (v>1)
+    if ( v < 0 ) 
+    {
+        return -1;
+    }
+    else if (v>1)
     {
         return v * factorial(v-1);
     }
     else 
     {
-    return 1;
+        return 1;
     }
 }
 int main()
 {
   int n;
+  cout << "\nEnter Number\t:\t";
   cin>>n;
-  cout<<factorial (n);
+  int retVal = factorial (n);
+  if ( retVal == -1 )
+  {
+      cout<< "\nEnter Positive Number\n";
+  }
+  else if ( retVal > 0 ) { 
+    cout<< "\nFactorial\t:\t" << retVal << endl;
+  }
+  else {
+      cout<< "\nResult Overflowed\n";
+  }
+  return 0;
 }
