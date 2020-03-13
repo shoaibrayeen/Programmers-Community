40fd916097d1c9479255425855e298c2fcc85ddd
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2020-03-13 14:31:01 +0530

    Bug Fixed

--- Data Structure/Stack/Stack Using Array/SolutionByMananpreet.java
+++ Data Structure/Stack/Stack Using Array/SolutionByMananpreet.java
@@ -7,23 +7,31 @@ class StackArray
 
 	public void push(int x)
 	{
-		if(top == 100)
+		if(top == 100) {
 			System.out.println("Stack is full");
-
-		else
+		}
+		else {
 			arr[++top] = x;
+		}
 	}
 	public void pop()
 	{
-		if(top == -1)
+		if(top == -1) {
 			System.out.println("Stack is empty");
-
-		else
+		}
+		else {
+			System.out.println("Popped Element\t:\t" + arr[top] );
 			--top;
+		}
 	}
 
 	public void print()
 	{
+		if ( top == -1 ) 
+		{
+			System.out.println("Stack is Empty.");
+			return;
+		}
 		System.out.print("Stack: ");
 		for(int i = top; i >-1; i--)
 		{
@@ -35,35 +43,38 @@ class StackArray
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		StackArray s = new StackArray();
-			
-    int op, data;
+		int op, data;
+		System.out.println("1. Push ");
+   		System.out.println("2. Pop ");
+    		System.out.println("3. Display ");
+      		System.out.println("4. Exit ");
+    		while(true)
+    		{
+         		System.out.print("\nEnter your choice\t:\t");
+        		op = sc.nextInt();
+        		if(op==1)
+        		{
+            			System.out.print("Enter data\t:\t");
+            			data = sc.nextInt();
+            			s.push(data);
+        		}
+        		else if(op==2)
+			{
+            			s.pop();
+			}
+        		else if(op==3)
+			{
+            			s.print();
+			}
+        		else if(op==4)
+			{
+				return;
+			}
+			else 
+			{
+            			System.out.println("Wrong choice ");
+			}
 
-  
-    System.out.println("1. Push ");
-    System.out.println("2. Pop ");
-    System.out.println("3. Display ");
-      System.out.println("4. Exit ");
-    
-     while(true)
-    {
-         System.out.println(" Enter your choice ");
-        op = sc.nextInt();
-        if(op==1)
-        {
-            System.out.print("Enter data :");
-            data = sc.nextInt();
-            s.push(data);
-        }
-        else if(op==2) 
-            s.pop();
-        else if(op==3)
-            s.print();
-        else if(op==4)
-          return;
-        else
-            System.out.println("Wrong choice ");
-
-    }
-   }
-
-	}
+    		}
+   	}	
+}
