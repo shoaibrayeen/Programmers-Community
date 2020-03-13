b09675d782fe4f1f3f18599ace93cdf064f56009
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2020-03-13 14:19:44 +0530

    Fixed bug

--- Data Structure/Stack/Stack Using Linked List/SolutionByMananpreet.java
+++ Data Structure/Stack/Stack Using Linked List/SolutionByMananpreet.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class StackLL
+class Main
 {
 	Node head;
 	static class Node
@@ -26,33 +26,46 @@ class StackLL
 		if(head.next == null)
 		{
 			System.out.println("Stack underflow");
+			return;
 		}
-		else if(head.next.next == null)
+		else if(head.next.next == null) 
+		{
 			head.next = null;
-		else
-		head.next = head.next.next;
+		}
+		else 
+		{
+		    System.out.println("Element Popped");
+		    head.next = head.next.next;
+		}
+		
 	}
 
 	public void top()
 	{
-    if(head.next!=null)
-		System.out.println("Top: "+head.next.data);
-  else
-    System.out.println("The stack is empty");
+        if(head.next!=null) {
+		    System.out.println("Top: "+head.next.data);
+        }
+        else {
+            System.out.println("The stack is empty");
+        }
 	}
-   
-   public void isEmpty()
-   {
-   	if(head.next == null)
-   		System.out.println("Yes");
-    else
-      System.out.println("No");
 
-   	
-   }
+    public void isEmpty()
+    {
+   	    if(head.next == null) {
+   		    System.out.println("Stack is Empty.");
+   	    }
+        else {
+            System.out.println("Stack is not Empty");
+        }
+    }
     public void print()
     {
     	Node t = head.next;
+    	if ( t == null ) {
+    	    System.out.println("Stack is Empty.");
+    	    return;
+    	}
     	System.out.print("Stack: ");
     	    	while(t!=null)
     	{
@@ -61,45 +74,54 @@ class StackLL
     	}
     	System.out.println();
     }
-   public static void main(String[] args)
+    public static void main(String[] args)
     {
-      Scanner sc = new Scanner(System.in);
-   	StackLL s = new StackLL();
-   	s.head = new Node(0);
-    int op, data;
-
-  
-    System.out.println("1. Push ");
-    System.out.println("2. Pop ");
-    System.out.println("3. Display ");
-    System.out.println("4. Top ");
-     System.out.println("5. IsEmpty ");
-      System.out.println("6. Exit ");
-    
-     while(true)
-    {
-         System.out.println(" Enter your choice ");
-        op = sc.nextInt();
-        if(op==1)
+        Scanner sc = new Scanner(System.in);
+   	    Main s = new Main();
+   	    s.head = new Node(0);
+        int op, data;
+        System.out.println("1. Push ");
+        System.out.println("2. Pop ");
+        System.out.println("3. Display ");
+        System.out.println("4. Top ");
+        System.out.println("5. IsEmpty ");
+        System.out.println("6. Exit ");
+        while(true)
         {
-            System.out.print("Enter data :");
-            data = sc.nextInt();
-            s.push(data);
-        }
-        else if(op==2) 
-            s.pop();
-        else if(op==3)
-            s.print();
-        else if(op==4)
-            s.top();
-          else if(op == 5)
-             s.isEmpty();
-        else if(op == 6)
-          return;
-        else
-            System.out.println("Wrong choice ");
+            System.out.print("\nEnter your choice\t:\t");
+            op = sc.nextInt();
+            if(op==1)
+            {
+                System.out.print("Enter data\t:\t");
+                data = sc.nextInt();
+                s.push(data);
+            }
+            else if(op==2) 
+            {
+                s.pop();
+            }
+            else if(op==3)
+            {
+                s.print();
+            }
+            else if(op==4)
+            {
+                s.top();
+            }
+            else if(op == 5) 
+            {
+                s.isEmpty();
+            }
+            else if(op == 6) 
+            {
+                return;
+            }
+            else 
+            {
+                System.out.println("Wrong choice ");
+            }
 
+        }
     }
-   }
 
 }
