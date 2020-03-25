0d11ef822ce66bdd8c99c0a736a2949b0da2f317
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2020-03-25 14:06:01 +0530

    Bug Fixed

--- Basic/Count Number of Digits in An Integer/SolutionByInder.cpp
+++ Basic/Count Number of Digits in An Integer/SolutionByInder.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 int main()
 {
- int n,flag=0;
- cin>>n;
- while(n>0)
- {
-     n = n/10;
-     flag++;
- }
- cout<<flag;
+    int n,flag=0;
+    cout << "\nEnter Number\t:\t";
+    cin >> n;
+    if ( n == 0) {
+        flag = 1;
+    }
+    while(n > 0)
+    {
+        n = n/10;
+        flag++;
+    }
+    cout << "\nNumber of Digits\t:\t" << flag << endl;
+    return 0;
 }
