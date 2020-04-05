ccd4316521b7a304248fe84aa98809240cbfcbca
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2020-04-05 19:24:10 +0530

    Updated and Indented

--- Dynamic Programming/Longest Palindromic Subsequence/SolutionBySakshi.java
+++ Dynamic Programming/Longest Palindromic Subsequence/SolutionBySakshi.java
@@ -1,44 +1,44 @@
 import java.util.*;
-public class longestSubsequences
-{
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        String str;
-        System.out.println("Enter the string for palindromic subseq");
-        str=sc.nextLine();
+ public class Main
+ {
+     public static void main(String args[])
+     {
+         Scanner sc = new Scanner(System.in);
+         String str;
+         System.out.print("Enter the string\t:\t");
+         str = sc.nextLine();
+         int result = longestPalindromicSubsequnces(str);
+         System.out.println("\nLength of LPS\t\t:\t" + result );
+     }
 
-        int result=longestPalindromicSubsequnces(str);
-        System.out.println(result);
-    }
+     public static int longestPalindromicSubsequnces(String str)
+     {
+         int n = str.length();
+         int [][]dp = new int [n][n];
 
-    public static int longestPalindromicSubsequnces(String str)
-    {
-        int [][]dp=new int [str.length()][str.length()];
+         for( int gap = 0; gap < n; gap++ )
+         {
+             for( int i = 0, j = gap; j < n; j++, i++ )
+             {
+                 if( gap == 0 )   //gap one denotes single length string
+                 {
+                     dp[i][j] = 1;
+                 }
 
-        for(int gap=0;gap<str.length();gap++)
-        {
-            for(int i=0,j=gap;j<str.length();j++,i++)
-            {
-                if(gap==0)   //gap one denotes single length string
-                {
-                    dp[i][j]=1;
-                }
+                 else if( str.charAt(i) == str.charAt(j) )
+                 {
+                     dp[i][j] = dp[i+1][j-1] + 2;
+                 }
 
-                else if(str.charAt(i)==str.charAt(j))
-                {
-                    dp[i][j]=dp[i+1][j-1]+2;
-                }
+                 else
+                 {
+                     dp[i][j] = Math.max( dp[i+1][j], dp[i][j-1] );
+                 }
+             }
+         }
 
-                else
-                {
-                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
-                }
-            }
-        }
+         return dp[0][str.length()-1];
 
-        return dp[0][str.length()-1];
+     }
 
-    }
-
-}
+ }
