72c7a1426846c235477a7c0b893d30beb9e0a223
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2020-04-19 13:25:31 +0530

    Renamed and Fixed Bugs

--- Divide and Conquer/Median of Two Sorted Array/SolutionByVaibhavSethia.java
+++ Divide and Conquer/Median of Two Sorted Array/SolutionByVaibhavSethia.java
@@ -12,14 +12,14 @@ public class Median {
 		
 		Median median = new Median();
 		//Taking input of sizes and array elements using custom methods
-		int size1 = median.takeInput();
-		int size2 = median.takeInput();
-		
-		int arr1[] = median.takeArrayInput(size1);
-		int arr2[] = median.takeArrayInput(size2);
-		
-		
-		
+		System.out.print("\nEnter Size of 1st Array\t:\t");
+ 		int size1 = median.takeInput();
+ 		System.out.print("\nEnter Elements of 1st Array\t:\t");
+ 		int arr1[] = median.takeArrayInput(size1);
+ 		System.out.print("\nEnter Size of 2nd Array\t:\t");
+ 		int size2 = median.takeInput();
+ 		System.out.print("\nEnter Elements of 2nd Array\t:\t");
+ 		int arr2[] = median.takeArrayInput(size2);
 		//Calling function
 		median.mergeMedian(arr1,arr2,size1,size2);
 	}
@@ -41,44 +41,37 @@ public class Median {
 	private void mergeMedian(int arr1[],int arr2[],int size1,int size2) {
 		int i=0,j=0,k=0;
 		int sortedData[]=new int[size1+size2];
-		
 		//Sorting the data in Ascending order
-		while(i<size1 && j<size2) {
-			
+		while(i<size1 && j<size2) {	
 			if(arr1[i]<arr2[j])
 				sortedData[k++]=arr1[i++];
 			else
 				sortedData[k++]=arr2[j++];
 		}
-		
 		//If elements in arr1 are left ,all  are add directly
 		while(i<size1) {
 			sortedData[k++]=arr1[i++];
 		}
-		
 		//Else if elements in arr2 are left ,all  are add directly
 		while(j<size2) {
 			sortedData[k++]=arr2[j++];
 		}
-		
-
 		int median=0;
-		int medianValue=0;
+		double medianValue=0;
 		int size=size1+size2;
-		
 		//If no. of sorted elements are odd using median formula for odd size and printing
 		if((size & 1)==1) {
 			median=(size-1)/2;
 			medianValue=sortedData[median];
-			System.out.print(medianValue);
+			System.out.print("\nMedian\t:\t" + medianValue);
 		}
 //		If no. of sorted elements are even using median formula for even size and printing
 		else {
 			int median1=(size-1)/2;
 			int median2=median1+1;
-		
-			double medianValueD=(sortedData[median1]+sortedData[median2])/2;
-			System.out.println(medianValueD);
+			medianValue = sortedData[median1]+ sortedData[median2];
+ 			medianValue /= 2;
+ 			System.out.println("\nMedian\t:\t" + medianValue);
 		}
 	}
 
