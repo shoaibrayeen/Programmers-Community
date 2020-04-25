6b850c92707fe18210a86ace5eb8d648aaac8406
yashtuli215 <60575658+yashtuli215@users.noreply.github.com>
2020-04-26 02:52:41 +0530

    Create SolutionByYash.cpp
    New solution added

---
+++ Basic/Calculate Factorial of A Number/SolutionByYash.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+int fact=1,i,n;
+cout<<"Enter the number: ";
+cin>>n;
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+cout<<<"Factorial of given number is :"<<fact;
+getch();
+}
+
