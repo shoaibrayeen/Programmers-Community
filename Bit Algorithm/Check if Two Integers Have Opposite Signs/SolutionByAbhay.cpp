76059de1e64902f6eff6711f954f9840dab877d5
Abhay <abhayag1998@gmail.com>
2020-04-25 19:22:21 +0530

    SolutionByAbhay

---
+++ Bit Algorithm/Check if Two Integers Have Opposite Signs/SolutionByAbhay.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isTwoDifferentSign( signed int a, signed int b){
+	return ((a^b)<0);
+}
+int main(){
+	signed int x,y;
+	cin>>x>>y;
+	if(isTwoDifferentSign(x,y)) cout<<"Yes"<<endl;
+	else count<<"No"<<endl;
+	return 0;
+}
