fb47604cfb06820fb14c8213372957c8c95c2953
TheExecuter1 <54216009+TheExecuter1@users.noreply.github.com>
2020-05-31 01:51:30 +0530

    Create SolutionByFuzail.cpp

---
+++ Data Structure/Array Or Vector/Find Minimum Element of An Array/SolutionByFuzail.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int min_ele(int arr[] , int n) 
+{
+    int max = arr[0] ;
+    for (int i=0 ; i<n ; i++)
+    {
+        if (arr [i] < max)
+        {
+            max = arr [i] ;
+        }
+    }
+    return max ;
+}
+
+int main ()
+{
+    int n ;
+    cout << "Enter size of array " ;
+    cin >> n ;
+    int arr [n] ;
+    cout << "enter the array" ;
+    for (int i=0 ; i<n ; i++) 
+    {
+        cin >> arr[i] ;
+    }
+    int result ;
+    result = min_ele(arr , n ) ;
+    cout << result ;
+    return 0 ;
+}
