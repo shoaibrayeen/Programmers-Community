6dbed61ffb8639fa3321a7c601e1d3ef55ea460e
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2020-06-04 01:03:51 +0530

    Fixed bug

--- Data Structure/Array Or Vector/Bubble Sort/SolutionByFuzail.cpp
+++ Data Structure/Array Or Vector/Bubble Sort/SolutionByFuzail.cpp
@@ -5,7 +5,7 @@ void sort(int arr[] , int n)
     int temp ;
     for (int i=0 ; i<n ; i++)
     {
-        for (int j=0 ; j<n ; j++)
+        for (int j=0 ; j<n-1-i ; j++)
         {
             if(arr[j]>arr[j+1])
             {
