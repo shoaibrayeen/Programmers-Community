e9ad6d251dce935e65b59bdc5f9cbe0668764dda
kuljeet singh <48275443+Kuljeet-123@users.noreply.github.com>
2020-06-15 01:23:07 +0530

    Update SolutionByKuljeet.cpp

--- Basic/Calculate Nth Fibonacci Number/SolutionByKuljeet.cpp
+++ Basic/Calculate Nth Fibonacci Number/SolutionByKuljeet.cpp
@@ -3,18 +3,21 @@ using namespace std;
 
 int fibonacci(int n)
 {
-    if(n <= 1)
-      return n;
-
-   return fibonacci(n-2) + fibonacci(n-1);
+    if(n <= 2)
+        return n-1;
+    else
+        return fibonacci(n-2) + fibonacci(n-1);
 }
 
 
 int main()
 {
-    int n;
+    int n = 0;
     cout << "Enter value of n:" << endl;
     cin >> n;
-    cout << "The Nth Fibonacci is " << fibonacci(n);
+    if(fibonacci(n) == -1)
+        cout << "Fibonacci Series at 0th element doesn't exist" << endl;
+    else
+        cout << "The Nth Fibonacci is " << fibonacci(n);
     return 0; 
 }
