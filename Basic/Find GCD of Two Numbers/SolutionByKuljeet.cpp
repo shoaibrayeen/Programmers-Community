247632bfb7ae4fc094f2c709d85bedf433a7cd68
Kuljeet-123 <kuljeetsingh951@gmail.com>
2020-06-17 00:38:00 +0530

    GCD of two numbers

---
+++ Basic/Find GCD of Two Numbers/SolutionByKuljeet.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int x , int y)
+{
+    if(y == 0)
+       return x;
+
+    return gcd(y , x%y);
+}
+
+int main()
+{
+    int x,y;
+    cout << "\n Enter value of two numbers:";
+    cin >> x >> y;
+    cout << "GCD of " << x << " and " << y << " is " << gcd(x,y);
+  
+  return 0;
+}
