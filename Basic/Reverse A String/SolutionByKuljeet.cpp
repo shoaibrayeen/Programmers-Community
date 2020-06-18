53589d5331df5fd94ef0926eecb75da18cada56a
Kuljeet-123 <kuljeetsingh951@gmail.com>
2020-06-18 21:05:50 +0530

    Reverse of a string

---
+++ Basic/Reverse A String/SolutionByKuljeet.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void reverseString(string& str)
+{
+    int n = str.length();
+    for(int i = 0;i < n/2;i++)
+    {
+        char temp = str[i];
+        str[i] = str[n-i-1];
+        str[n-i-1] = temp;
+    }
+}
+
+int main()
+{
+    string str;
+    cout << "Enter String" << endl;
+    cin >> str;
+    reverseString(str);
+    cout << str;
+   return 0; 
+}
