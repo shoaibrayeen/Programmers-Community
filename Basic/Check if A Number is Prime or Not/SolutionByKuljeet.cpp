6d66dec55b987c9d9ebf1f315a3d0aa8aee732cc
kuljeet singh <48275443+Kuljeet-123@users.noreply.github.com>
2020-06-23 13:28:07 +0530

    Updated Prime Solution

--- Basic/Check if A Number is Prime or Not/SolutionByKuljeet.cpp
+++ Basic/Check if A Number is Prime or Not/SolutionByKuljeet.cpp
@@ -3,22 +3,21 @@ using namespace std;
 
 bool isPrime(int n)
 {
-    if(n <= 1)
-      return false;
-
-    if(n <= 3)
-      return true;
-
-    if(n%2 == 0 || n%3 == 0)
-      return false;
-
-    for(int i = 5; i*i <= n; i = i + 6)
-    {
-        if(n%i == 0 || n%(i + 2) == 0)
-           return false;
-    }
-
-   return true;
+    int count=0;
+	if(n == 2){
+		return true;
+	}
+	for(int i = 2;i <= sqrt(n);i++){
+		if(n%i == 0){
+			count++;
+		}
+	}
+	if(count > 0){
+		return false;
+	}
+	else{
+		return true;
+	}
 }
 
 
