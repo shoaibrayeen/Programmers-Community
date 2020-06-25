3c91529c1b3a6351aa21248854a7ff508983abec
Kuljeet-123 <kuljeetsingh951@gmail.com>
2020-06-25 19:30:56 +0530

    Maximum element in an array

---
+++ Data Structure/Array Or Vector/Find Maximum Element of An Array/SolutionByKuljeet.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int maxElement(int arr[], int n)
+{
+    int max = arr[0];
+    for(int i = 0;i < n;i++)
+    {
+        if(arr[i] > max)
+           max = arr[i];
+    }
+
+  return max;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array:" << endl;
+    cin >> n;
+    int arr[n];
+    cout << "\nEnter the elements of array" << endl;
+    for(int i = 0;i < n;i++)
+       cin >> arr[i];
+    
+    cout << "\nMaximum element in an array is: " << maxElement(arr,n);
+
+  return 0;
+  
+}
