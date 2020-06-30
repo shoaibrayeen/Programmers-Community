4d1cf4f40b95951f8b2353e7bf78b6a207d35b42
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2020-07-01 00:02:10 +0530

    Modified

--- Basic/Product of Digits in A Number/SolutionByKuljeet.cpp
+++ Basic/Product of Digits in A Number/SolutionByKuljeet.cpp
@@ -6,22 +6,16 @@ int productDigits(int n)
     int product = 1;
     while(n != 0)
     {
-        if(n%10 == 0)
+        if(n%10 != 0)
         {
-            n = n/10;
+            product = product * ( n % 10 );
         }
-
-        else
-        {
-            product = product*(n%10);
-            n = n/10;
-        }
+        n = n/10;
     }
 
     return product; 
 }
-
-
+ 
 int main()
 {
     int n;
