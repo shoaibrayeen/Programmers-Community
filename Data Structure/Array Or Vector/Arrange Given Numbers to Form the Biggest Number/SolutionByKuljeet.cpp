4a88815b33d8f4745ca8d8467108c3bfaf201716
Kuljeet-123 <kuljeetsingh951@gmail.com>
2020-07-03 21:53:37 +0530

    Created Arranging given numbers

---
+++ Data Structure/Array Or Vector/Arrange Given Numbers to Form the Biggest Number/SolutionByKuljeet.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int compare(string X, string Y)
+{
+    string XY = X.append(Y);
+    string YX = Y.append(X);
+    if(XY > YX)
+       return 1;
+
+    else 
+       return 0;   
+}
+
+void display(string arr[], int n)
+{
+    for(int i = 0;i < n;i++)
+       cout << arr[i];
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of an array:" << endl;
+    cin >> n;
+    string arr[100];
+    for(int i = 0;i < n;i++)
+       cin >> arr[i];
+    
+    sort(arr,arr+n,compare);
+    display(arr,n);
+    return 0;
+
+}
