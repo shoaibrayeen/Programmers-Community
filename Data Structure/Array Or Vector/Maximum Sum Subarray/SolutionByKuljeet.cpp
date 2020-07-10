ae1b90a2578ebad6ef9c6f240d29bfd5270df443
Kuljeet-123 <kuljeetsingh951@gmail.com>
2020-07-10 20:26:29 +0530

    Maximum Sum Subarray

---
+++ Data Structure/Array Or Vector/Maximum Sum Subarray/SolutionByKuljeet.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int getMaxSumSubarray(int arr[], int n)
+{
+    int result = arr[0];
+    int maxEnding = arr[0];
+
+    for(int i = 1;i < n;i++)
+    {
+        maxEnding = max(maxEnding+arr[i],arr[i]);
+        result = max(result,maxEnding);
+    }
+  
+  return result;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of an array:" << endl;
+    cin >> n;
+    int arr[n];
+    for(int i = 0;i < n;i++)
+       cin >> arr[i];
+    
+    cout << "Maximum sum of subarray is: " << getMaxSumSubarray(arr,n);
+
+    return 0;
+}
