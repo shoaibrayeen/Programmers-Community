2d9475e3b4e2b99d6909ee0ce42cfd23f057fe44
Kuljeet-123 <kuljeetsingh951@gmail.com>
2020-07-12 19:56:15 +0530

    Subarray with given sum

---
+++ Data Structure/Array Or Vector/Find Subarray With Given sum/SolutionByKuljeet.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+bool isSubarraySum(int arr[], int n, int sum)
+{
+    int current_sum = arr[0];
+    int start = 0;
+    for(int end = 1;end < n;end++)
+    {
+        //Clean the previous window
+        while(current_sum > sum && start < end-1)
+        {
+            current_sum -= arr[start];
+            start++;
+        }
+
+        if(current_sum == sum)
+           return true;
+        
+        if(end < n)
+          current_sum += arr[end];
+    }
+
+    return (current_sum == sum);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of an array:" <<  endl;
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of an array:" << endl;
+    for(int i = 0;i < n;i++)
+       cin >> arr[i];
+    
+    int sum;
+    cout << "Enter the Sum :" << endl;
+    cin >> sum;
+    if(isSubarraySum(arr,n,sum))
+       cout << "Yes" << endl;
+    
+    else
+       cout << "No" ;
+    return 0;
+}
