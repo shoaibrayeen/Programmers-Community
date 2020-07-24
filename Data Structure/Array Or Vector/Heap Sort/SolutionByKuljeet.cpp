577b37d0c03f6fb42c715b20e25d0bc9a8003834
Kuljeet-123 <kuljeetsingh951@gmail.com>
2020-07-24 21:29:51 +0530

    Heap Sort

---
+++ Data Structure/Array Or Vector/Heap Sort/SolutionByKuljeet.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+void maxHeapify(int arr[], int n, int i)
+{
+    int largest = i, left = 2*i+1, right = 2*i+2;
+    if(left < n && arr[left] > arr[largest])
+         largest = left;
+    
+    if(right < n && arr[right] > arr[largest])
+        largest = right;
+    
+    if(largest != i)
+    {
+        swap(arr[largest],arr[i]);
+        maxHeapify(arr,n,largest);
+    }
+}
+
+void build(int arr[], int n)
+{
+    for(int i = (n-2)/2;i >= 0;i--)
+       maxHeapify(arr,n,i);
+}
+
+void heapSort(int arr[], int n)
+{
+    build(arr,n);
+    for(int i = n-1;i >= 1;i--)
+    {
+        swap(arr[0],arr[i]);
+        maxHeapify(arr,i,0);
+    }
+
+
+    for(int i = 0;i < n;i++)
+        cout << arr[i] << " ";
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0;i < n;i++)
+       cin >> arr[i];
+    heapSort(arr,n);
+    return 0;
+}
