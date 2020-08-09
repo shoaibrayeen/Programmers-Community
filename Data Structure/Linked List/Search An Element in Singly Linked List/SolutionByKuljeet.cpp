67d745abb8ce6a0e096f92f1d7f279a083e041aa
Kuljeet-123 <kuljeetsingh951@gmail.com>
2020-08-09 21:28:15 +0530

    Search in a Singly Linked List

---
+++ Data Structure/Linked List/Search An Element in Singly Linked List/SolutionByKuljeet.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *insertAtEnd(Node *head, int data)
+{
+    Node *temp = new Node(data);
+    if(head == NULL)
+       return temp;
+    
+    Node *curr = head;
+    while (curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+
+    return head;
+}
+
+Node *searchList(Node *head, int key)
+{
+    if(head == NULL)
+       return NULL;
+
+    if(head->data == key)
+        return head;
+    else
+       return searchList(head->next,key);
+}
+
+void display(Node *head)
+{
+    while(head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+
+int main()
+{
+    Node *head = NULL;
+    char choice;
+    int data;
+    int key;
+
+    cout<<"Do you want to enter element(y/n) ";
+    cin>>choice;
+    if(choice=='y')
+    {
+         do
+        {
+                cin >> data;
+                insertAtEnd(head,data);
+                cout<<"Do you want to add more element(y/n)";
+                cin>>choice;
+        }while(choice=='y');
+    }
+
+
+    cout<<"Enter an element you want to search ";
+    cin>>key;
+
+    searchList(head,key);
+    display(head);
+
+    return 0;
+
+}
