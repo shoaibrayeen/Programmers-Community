391038d55a3297a2a922a564c011e33f603f1cda
Ambika <ambika.arora1998@gmail.com>
2020-10-01 23:18:16 +0530

    Transpose a N*M matrix java solution

---
+++ Data Structure/Matrix/Transpose of A Matrix/SolutionByAmbika.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Main
+{ 
+    // This function stores transpose 
+    // of A[][] in B[][] 
+    static void transpose(int A[][], int B[][]) 
+    { 
+        int i, j; 
+        for (i = 0; i < A[0].length; i++) 
+            for (j = 0; j < A.length; j++) 
+                B[i][j] = A[j][i]; 
+    } 
+      
+    // Driver code 
+    public static void main (String[] args) 
+    { 
+        Scanner scn = new Scanner(System.in);
+        int N = scn.nextInt();
+        int M = scn.nextInt();
+
+        int[][] A = new int[N][M];
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                A[i][j]=scn.nextInt();
+            }
+        }
+      
+        int B[][] = new int[M][N], i, j; 
+      
+        transpose(A, B); 
+      
+        System.out.print("Result matrix is \n"); 
+        for (i = 0; i < M; i++) 
+        { 
+            for (j = 0; j < N; j++) 
+            System.out.print(B[i][j] + " "); 
+            System.out.print("\n"); 
+        } 
+    } 
+} 
