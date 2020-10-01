2d1b9c3caa967c8862eee3a08150620b38a75d92
Purvansh-gourh <52257597+Purvansh-gourh@users.noreply.github.com>
2020-10-01 14:38:25 +0530

    Update SolutionByPurvanshGourh.cpp

--- Data Structure/Stack/Stack using Queue/SolutionByPurvanshGourh.cpp
+++ Data Structure/Stack/Stack using Queue/SolutionByPurvanshGourh.cpp
@@ -1,24 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//solution by making pop operation costly
 class QueueStack{
 private :
     queue<int>q1;
     queue<int>q2;
 public:
     void push(int x){
+        //simply push item into queue 1
         q1.push(x);
     }
     int pop(){
+        //if both queues are empty
         if(q1.empty() && q2.empty())
             return -1;
 
+        //pop all elements except last from queue 1
+        //and push to queue 2
         while(q1.size()>1){
             q2.push(q1.front());
             q1.pop();
         }
+        //return element is last element
         int x=q1.front();
         q1.pop();
+
+        //push back elements into queue 1
         while(!q2.empty()){
             q1.push(q2.front());
             q2.pop();
