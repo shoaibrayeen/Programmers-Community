879e3cb652de153777635fc47eb44b1ee3683084
Shubham Pal <shubham.mca19.du@gmail.com>
2020-10-01 11:31:09 +0530

    Reverse a number(Java): Solution by Shubham

---
+++ Basic/Reverse A Given Number/SolutionByShubham.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SolutionByShubham {
+    
+    static Scanner scan = new Scanner(System.in);
+    
+    static int reverse(int number) {
+        int rev = 0;
+        
+        while (number > 0) {
+            rev = rev * 10 + number % 10;
+            number = number / 10;
+        }
+        
+        return rev;
+    }
+    
+    public static void main(String []args){
+        System.out.println("Enter a number to reverse: ");
+        int number = scan.nextInt();
+        
+        System.out.println("Reverse of the number is: " + reverse(number));
+    }
+}
