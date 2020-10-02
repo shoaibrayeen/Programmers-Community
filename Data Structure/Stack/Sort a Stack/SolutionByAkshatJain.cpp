9dee38eef3b6c0e5057b291027fdf1038f7ba055
Akshat Jain <akshatj355@gmail.com>
2020-10-03 00:46:28 +0530

    Create SolutionByAkshatJain.cpp

---
+++ Data Structure/Stack/Sort a Stack/SolutionByAkshatJain.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+void sortedInsert(stack<int> &s, int x)
+{
+	if(s.empty() or x>s.top())
+	{
+		s.push(x);
+		return;
+	}
+	int temp = s.top();
+	s.pop();
+	sortedInsert(s,x);
+	s.push(temp);
+}
+
+void SortedStack :: sort()
+{
+	if(!s.empty())
+	{
+		int x = s.top();
+		s.pop();
+		sort();
+		sortedInsert(s,x);
+	}
+}
+
