a48445e247fefba8fc2d157a167dfcbc70dce1e2
seema <ujjwal_sharma@unifyed.com>
2020-10-03 20:57:11 +0530

    file to determine if given number is even/odd using mode operator

---
+++ Basic/Check Whether a Number is Even or Odd/solutionBySeema.cpp
@@ -0,0 +1,36 @@
+/*
+Author : Seema Kumari Patel
+Problem : To determine if the given number is even or odd.
+Input : Number (integer)
+Output : Conclusion , if the number is even or odd.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// function to check whether a number is odd or even
+void evenOdd(int num)
+{
+    if (num % 2 == 0)
+    {
+
+        cout << "Even";
+    }
+    else
+    {
+        cout << "Odd";
+    }
+}
+
+
+int main()
+{
+
+    int n;
+    cout << "enter the a number : ";
+    cin >> n;
+    evenOdd(n);
+    cout << endl;
+
+    return 0;
+}
