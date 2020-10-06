7b510d909b50a3faf3c75ae9783466ee2e36a26c
Shrishti Goyal <shrishtigoyal2608@gmail.com>
2020-10-06 15:14:42 +0530

    Create SolutionByShrishti.cpp

---
+++ Data Structure/Array Or Vector/Trapping Rainwater Problem/SolutionByShrishti.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++>
+using namespace std;
+
+int MaxRainwater(int arr[], int n)
+{
+    int res = 0;
+    int leftMax = arr[0],rightMax = arr[n-1];
+    int i = 0 , j = n-1;
+    while(i <= j)
+    { 
+      leftMax = max(arr[i],leftMax);
+      rightMax = max(arr[j],rightMax);
+      if(leftMax <= rightMax)
+      { res += leftMax - arr[i];
+        i++;
+      }
+      else
+      { res += rightMax - arr[i];
+        j--;
+      }
+     }
+
+    
+
+  return res;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of an array:" << endl;
+    cin >> n;
+    int arr[n];
+    for(int i = 0;i < n;i++)
+       cin >> arr[i];
+    
+    cout << MaxRainwater(arr,n);
+    return 0;
+}
