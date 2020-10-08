33a05c0b8733d2201c1d29e9d48243e5b31a5f31
Shrishti Goyal <shrishtigoyal2608@gmail.com>
2020-10-08 17:33:20 +0530

    Update SolutionByShrishti.cpp

--- Bit Algorithm/Find Non-Repeated Element/SolutionByShrishti.cpp
+++ Bit Algorithm/Find Non-Repeated Element/SolutionByShrishti.cpp
@@ -1,21 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+// function to find nonrepeated element
 int nonrepeated(int arr[],int n)
 {
-  int a = 0;
-  for(int i = 0;i < n;i++)
-    a ^= arr[i];
-  return a;
+    int a = 0;
+    for(int i = 0;i < n;i++)
+      // using xor 
+      a ^= arr[i];
+     return a;
 }
 
 int main()
 {
-  int n , arr[10000];
-  cin>>n;
-  
-  for(int i = 0;i < n;i++)
-    cin>> arr[i];
- 
-  cout<< nonrepeated(arr,n) <<endl;
-  return 0;
+    int n , arr[10000];
+    cin >> n;
+    for(int i = 0;i < n;i++)
+      // input the array
+      cin>> arr[i];
+   
+    cout<< nonrepeated(arr,n) <<endl;
+    return 0;
 }
