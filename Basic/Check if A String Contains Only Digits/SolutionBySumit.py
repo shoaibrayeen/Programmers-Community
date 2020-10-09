be6f6164178f2152e27cbed5be2e82214534d668
sumit076 <sumit360saha@gmail.com>
2020-10-10 03:05:07 +0530

    Create SolutionBySumit.py

---
+++ Basic/Check if A String Contains Only Digits/SolutionBySumit.py
@@ -0,0 +1,16 @@
+ini_string1 = '1234556'
+ini_string2 = 'ab123bc'
+  
+ 
+print ("Initial Strings : ", ini_string1, ini_string2) 
+  
+# Using isdigit() 
+if ini_string1.isdigit(): 
+    print ("String1 contains all numbers") 
+else: 
+    print ("String1 doesn't contains all numbers") 
+      
+if ini_string2.isdigit(): 
+    print ("String2 conatins all numbers") 
+else: 
+    print ("String2 doesn't contains all numbers") 
