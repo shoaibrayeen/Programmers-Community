0a6a1ec18af54769aca395b1def8161c743da58b
Shrishti Goyal <shrishtigoyal2608@gmail.com>
2020-10-09 11:56:16 +0530

    Update SolutionByShrishti.cpp

--- Bit Algorithm/Check if A Number is Power of Two/SolutionByShrishti.cpp
+++ Bit Algorithm/Check if A Number is Power of Two/SolutionByShrishti.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 // function to check whether an integer is power of 2
 bool isPoweroftwo(long long n)
-{
+{   
+    // if n and n-1 then it is false
     if(n & n-1)
     return false;
     return true;
@@ -10,7 +11,7 @@ bool isPoweroftwo(long long n)
 int main()
 {
     long long n;
-    cout<<"Enter the number that you want to check"<<endl;
+    cout<<"Enter the number"<<endl;
     cin>> n;
     if(isPoweroftwo(n))
         cout<<"The given number is a power of two"<<endl;
