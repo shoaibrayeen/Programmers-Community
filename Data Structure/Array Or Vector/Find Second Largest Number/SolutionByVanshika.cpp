be46b79a9b91685097ec2a20e12ea19ff68c55e9
kvanshika10 <65731141+kvanshika10@users.noreply.github.com>
2020-10-11 00:37:21 +0530

    Update SolutionByVanshika.cpp

--- Data Structure/Array Or Vector/Find Second Largest Number/SolutionByVanshika.cpp
+++ Data Structure/Array Or Vector/Find Second Largest Number/SolutionByVanshika.cpp
@@ -9,16 +9,9 @@ int main(){
    ll a[n];
    for(i=0;i<n;i++)
      cin >> a[i];
-     sort(a,a+n,greater<int>());
-     ll seclargest=a[0];
-     for(i=1;i<n;i++){
-        if(a[i]!=a[0]){
-          seclargest=a[i];
-          break;
-          }
-             }
-        
-cout << seclargest << endl;
+     sort(a,a+n);
+     
+cout << a[n-2] << endl;
 
 return 0;
 
