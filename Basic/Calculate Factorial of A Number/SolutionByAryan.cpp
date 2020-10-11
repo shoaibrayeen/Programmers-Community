3d589163ba175f7277eefaad2786af6310937ca1
“aryansin08” <msin.aryan08@gmail.com>
2020-10-11 14:46:48 +0530

    Added Solution

---
+++ Basic/Calculate Factorial of A Number/SolutionByAryan.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fact(int n)	{
+	if(n==0 || n==1)
+		return 1;
+	else
+		return n*fact(n-1);
+}
+
+int main()	{
+	int t;
+	cin>>t;
+	while(t--)	{
+	
+		int n;	
+		cin>>n;
+		cout<<fact(n);
+	}
+	
+	return 0;
+}
