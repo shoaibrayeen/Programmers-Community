be30715ccc219a847becbdd4d830710cbc3a06b0
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2020-10-16 17:26:24 +0530

    Updated

--- Basic/Check Leap Year/solutionBySeema.cpp
+++ Basic/Check Leap Year/solutionBySeema.cpp
@@ -10,12 +10,18 @@ using namespace std;
 
 void leapYear(int year)
 {
-
-    if (year % 400 == 0 && year % 4 == 0)
+    if (year % 400 == 0) {
         cout << year << "is a leap year.";
-
-    else if (year % 100 == 0)
-        cout << year << "is not a leap year!!";
+    }
+    else if (year % 100 == 0) {
+        cout << year << "is not a leap year!";
+    }
+    else if (year % 4 == 0){
+        cout << year << "is a leap year.";
+    }
+    else {
+        cout << year << "is not a leap year!";
+    }
 }
 
 // Driver code
