4b13a30676ba0d21ed74d4d3416212e3510899be
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2020-10-16 18:59:51 +0530

    Update SolutionbyMashiyatHussain.cpp

--- Data Structure/Array Or Vector/Selection Sort/SolutionbyMashiyatHussain.cpp
+++ Data Structure/Array Or Vector/Selection Sort/SolutionbyMashiyatHussain.cpp
@@ -8,30 +8,30 @@ printf("Enter number of elements\n");
 scanf("%d",&n);
 
 printf("Enter %d integers\n",n);
-for(c=0;c<n;c++)
+ for(c=0;c<n;c++)
  scanf("%d",&array[c]);
  
 for(c=0;c<(n-1);c++)
 {
- position=c;
+position=c;
  
- for(d=c+1;d<n;d++)
- {
-  if(array[position]>array[d])
-    position=d;
-  }
-  if(position !=c)
-  {
-  swap = array[c];
-  array[c] = array[position];
-  array[position] = swap;
-   }
+for(d=c+1;d<n;d++)
+{
+if(array[position]>array[d])
+ position=d;
+}
+ if(position !=c)
+{
+swap = array[c];
+array[c] = array[position];
+array[position] = swap;
  }
+}
  
- printf("Sorted list in ascending order:\n");
+printf("Sorted list in ascending order:\n");
  
- for(c = 0; c<n; c++)
-  printf("%d\n", array[c]);
+for(c = 0; c<n; c++)
+ printf("%d\n", array[c]);
  
 getch();
 }
