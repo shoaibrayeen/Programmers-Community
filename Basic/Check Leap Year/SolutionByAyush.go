207956e3f69f066333f2f69dd915a4621dedfe38
Ayush Modi <hot9cups@yahoo.in>
2020-10-16 19:51:34 +0530

    Create SolutionByAyush.go

---
+++ Basic/Check Leap Year/SolutionByAyush.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	for {
+		fmt.Printf("Please enter a positive year N : ")
+		_, err := fmt.Scan(&x)
+		if err != nil {
+			fmt.Println("Please enter a valid integer!")
+		} else if x <= 0 {
+			fmt.Println("You need to enter a positive integer!")
+		} else {
+			break
+		}
+	}
+	fmt.Println("Input =", x)
+	fmt.Println("Output =", isLeapYear(x))
+}
+
+func isLeapYear(year int) string {
+	if year%4 == 0 {
+		if year%100 == 0 && year%400 != 0 {
+			return "No"
+		}
+		return "Yes"
+	}
+	return "No"
+}
