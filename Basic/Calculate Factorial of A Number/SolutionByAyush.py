d181bbee2c12e80066aea8f2eca7ce713288670f
ayushgupta7856 <73484615+ayushgupta7856@users.noreply.github.com>
2020-10-26 20:50:08 +0530

    Create SolutionByAyush.py

---
+++ Basic/Calculate Factorial of A Number/SolutionByAyush.py
@@ -0,0 +1,8 @@
+n=int(input())
+         #Problem- A positive number N is given to you and You need to calculate its factorial.
+	       #Approach- Iterative
+
+s=1
+for i in range(1,n+1):
+    s=s*i
+print(s)
