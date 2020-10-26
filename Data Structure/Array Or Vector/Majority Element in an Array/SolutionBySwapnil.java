6359f8245c4fe77bf11787781962de54b03b80ec
Swapnil <31646351+SwapnilMadaan@users.noreply.github.com>
2020-10-27 00:55:07 +0530

    Linear Time Solution

---
+++ Data Structure/Array Or Vector/Majority Element in an Array/SolutionBySwapnil.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Solution {
+    public static int majorityElement(int[] nums) {
+            int count = 0;
+            Integer candidate = null;
+
+            for (int num : nums) {
+                if (count == 0) {
+                    candidate = num;
+                }
+                count += (num == candidate) ? 1 : -1;
+            }
+
+            return candidate == null ? -1 : candidate;
+    }
+    
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for(int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int result = majorityElement(arr);
+        if(result == -1)
+            System.out.println("No Majority Element Found");
+        else
+            System.out.println("Majority Element Found = " +result);
+    }
+}
