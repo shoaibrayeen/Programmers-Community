4ebf919f17f4d9e353d99aec7348593ba5879c4b
Simranjeet Kaur <66678133+Simran1017@users.noreply.github.com>
2020-10-30 23:32:12 +0530

    Create solutionBySimranjeet.java

---
+++ Basic/Check Whether a Number is Even or Odd/solutionBySimranjeet.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class SolutionsBySimranjeet {
+    
+    public static int EvenOdd( int n )
+    {
+         if( n%2 == 0 )
+              return 0 ;
+         else 
+             return 1 ;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in) ;
+        int n = sc.nextInt() ;
+        
+        int no = EvenOdd(n) ;
+        
+        if( no == 0 )
+            System.out.println("EVEN") ;
+        else
+            System.out.println("ODD") ;
+    }
+}
