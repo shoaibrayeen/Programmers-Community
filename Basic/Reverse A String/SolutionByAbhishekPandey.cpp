d5fbec51949f965db542e1fd0118c09c2ab4d275
aabhipandey000 <46788614+aabhipandey000@users.noreply.github.com>
2020-10-31 02:22:19 +0530

    Update and rename SolutionByAbhishekPandey.py to SolutionByAbhishekPandey.cpp

---
+++ Basic/Reverse A String/SolutionByAbhishekPandey.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+
+char ch[200],temp;
+cin>>ch;
+
+int l=strlen(ch);
+
+//cout<<l;
+
+for(int i=0;i<=l/2;i++)
+{
+     temp=ch[i];
+
+     ch[i]=ch[l-i];
+
+     ch[l-i]=temp;
+}
+
+
+for(int i=0;i<=l;i++)
+	cout<<ch[i];
+
+}
--- Basic/Reverse A String/SolutionByAbhishekPandey.py
+++
@@ -1,6 +0,0 @@
-str = input("Enter a string")
-s1 = ""
-for c in str:
-  s1 = c + s1
-print(s1)
-
