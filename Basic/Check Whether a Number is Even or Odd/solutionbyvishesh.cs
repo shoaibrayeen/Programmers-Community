568df87eea54937c640e0c2e4dbcd27b34903aee
Vishesh khurana <33988211+mainhoovishesh@users.noreply.github.com>
2020-10-30 23:31:53 +0530

    solutionbyvishesh.cs

---
+++ Basic/Check Whether a Number is Even or Odd/solutionbyvishesh.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace evenodd
+{
+    class Program
+    {
+        public static string evenodd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "even";
+            }
+            else
+                return "odd";
+        }
+        static void Main(string[] args)
+        {
+            string val = Console.ReadLine();
+            int res = Convert.ToInt32(val);
+            Console.WriteLine(evenodd(res));
+        }
+    }
+}
