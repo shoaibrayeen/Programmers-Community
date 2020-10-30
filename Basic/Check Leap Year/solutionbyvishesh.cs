a31a3869ece7f495fc3600f4035b60348e38556d
Vishesh khurana <33988211+mainhoovishesh@users.noreply.github.com>
2020-10-30 23:24:41 +0530

    Create solutionbyvishesh.cs

---
+++ Basic/Check Leap Year/solutionbyvishesh.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+public class Class1
+{
+    static void leapyear(int year)
+    {
+        string ans;
+        if (year % 400 == 0)
+        {
+            ans = "yes";
+
+        }
+        else
+        {
+            ans = "no";
+        }
+        Console.WriteLine(ans);
+    }
+    static void Main(string[] args)
+    {
+
+        string val;
+        val = Console.ReadLine();
+        int res = Convert.ToInt32(val);
+        leapyear(res);
+    }
+}
