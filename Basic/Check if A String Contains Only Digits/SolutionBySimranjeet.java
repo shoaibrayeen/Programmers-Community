2156b92d08f97cc6ec24b7351527d6fa2338a599
Simranjeet Kaur <66678133+Simran1017@users.noreply.github.com>
2020-10-30 23:25:37 +0530

    Create SolutionBySimranjeet.java

---
+++ Basic/Check if A String Contains Only Digits/SolutionBySimranjeet.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class SolutionBySimranjeet {
+    
+    public static int containsDigits( String str )
+    {
+        int flag = 0 ;
+        for( int i=0 ; i<str.length() ; i++ )
+        {
+            char ch = str.charAt(i);
+            if( ch >= 'a' && ch <= 'z' )
+            {
+                flag = 1 ; 
+                break ;
+            }
+        }
+        
+        return flag ;
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine() ;  
+        
+       int flag = containsDigits(str) ;
+        
+        if( flag == 0 )
+            System.out.println("YES") ;
+        else
+            System.out.println("NO") ;
+    }
+}
