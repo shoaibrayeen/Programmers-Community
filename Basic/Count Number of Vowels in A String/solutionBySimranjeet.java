9a54e9e278fd7a298f46629913434dc549cce789
Simranjeet Kaur <66678133+Simran1017@users.noreply.github.com>
2020-10-30 23:30:24 +0530

    Create solutionBySimranjeet.java

---
+++ Basic/Count Number of Vowels in A String/solutionBySimranjeet.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class SolutionBySimranjeet1 {
+    
+     public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine() ;  
+        
+        int no = NoOfVowels(str) ;
+        System.out.println(no) ;
+        
+     
+    }
+
+    private static int NoOfVowels(String str) {
+        
+        int cnt = 0 ;
+        for( int i=0 ; i<str.length() ; i++ )
+        {
+            char ch = str.charAt(i) ;
+            if( ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' )
+                cnt++ ;
+        }
+        return cnt ;
+    }
+}
