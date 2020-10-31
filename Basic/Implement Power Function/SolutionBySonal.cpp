38004b26b8ef275124d70319f5ca08f8b8045ffa
sonal30 <56064552+sonal30@users.noreply.github.com>
2020-10-31 23:59:35 +0530

    Create SolutionBySonal.cpp

---
+++ Basic/Implement Power Function/SolutionBySonal.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+float power(float m,int n) {
+	if(n==0)
+		return 1.0;
+	if(n<0)
+		return ((1.0/m)*power(m,n+1));
+	else
+		return (m*power(m,n-1));
+}
+
+int main(int arg,char *args[]) {
+	cout<<"\n Enter the base: ";
+	float base;
+	cin>>base;
+	cout<<"\nEnter the no. for power: ";
+	int po;
+	cin>>po;
+	cout<<"\nOutput: "<<power(base,po);
+	return 0;
+}
