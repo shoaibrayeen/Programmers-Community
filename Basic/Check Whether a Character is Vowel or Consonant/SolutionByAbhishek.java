afb469c57f95b5e7464341d214a82acc6d60debc
aabhipandey000 <46788614+aabhipandey000@users.noreply.github.com>
2020-10-31 16:21:25 +0530

    Create SolutionByAbhishek.java

---
+++ Basic/Check Whether a Character is Vowel or Consonant/SolutionByAbhishek.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class CheckCharacter
+{
+	public static String check( String s)
+	{
+		String c = s.toLowerCase();
+
+		if(c.charAt(0) == 'a' || c.charAt(0) == 'o'|| c.charAt(0) == 'i' || c.charAt(0) == 'o' || c.charAt(0) == 'u')
+			return "Vowel";
+
+		return "Consonant";
+	}
+
+	public static void main(String[] args) 
+	{
+      Scanner scan = new Scanner(System.in);
+      System.out.print("Enter a character :");
+      String s = scan.next();
+      System.out.println(check(s));
+
+		
+	}
+}
