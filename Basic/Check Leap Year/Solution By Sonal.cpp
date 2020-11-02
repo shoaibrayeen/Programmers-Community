5b4f76829fc3430dd169fd2f004f5ff486c4f126
sonal30 <56064552+sonal30@users.noreply.github.com>
2020-11-02 17:27:45 +0530

    Create Solution By Sonal.cpp

---
+++ Basic/Check Leap Year/Solution By Sonal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n%400==0){
+		cout<<"Yes";
+	}
+	else 
+	if(n%100!=0 && n%4==0){
+		cout<<"Yes";
+	}
+	else 
+	cout<<"No";
+	// your code goes here
+	return 0;
+}
