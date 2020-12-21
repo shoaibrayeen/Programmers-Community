1e8d734bdfe47783ee6551050e3070618f3c62bc
Ananthakrishnan Nair RS <61831021+akrish4@users.noreply.github.com>
2020-12-21 13:15:34 +0530

    Create SolutionByAnanthakrishnan.cpp

---
+++ Basic/Count Number of Digits in An Integer/SolutionByAnanthakrishnan.cpp
@@ -0,0 +1,30 @@
+#include <iostream>           // Header Files 
+using namespace std;
+
+int getCount(int n)
+{
+    int count = 0;
+    if(n == 0)
+    {
+        return 1;
+    }
+
+    while(n > 0)
+    {
+        n = n/10;
+        count++;
+    }
+
+   return count;
+}
+
+
+int main()  // Driver Code 
+{
+    int n;
+    cout << "Enter Value:" << endl;
+    cin >> n;
+    cout << "Number of digits in an integer is: " << getCount(n);
+
+   return 0;
+}
