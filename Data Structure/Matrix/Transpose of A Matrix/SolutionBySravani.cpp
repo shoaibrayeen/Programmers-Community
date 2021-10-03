ae1d29bafead1d3ec156539272c3285f7c961112
Sravani-7701 <sravanishivasln@gmail.com>
2021-10-03 08:43:08 +0530

    File added

---
+++ Data Structure/Matrix/Transpose of A Matrix/SolutionBySravani.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int A[n][n];
+	for(int i=0;i<n;i++){
+	    for(int j=0;j<n;j++){
+	        cin>>A[i][j];
+	    }
+	}
+
+	for (int i = 0; i < n; i++){
+		for (int j = i+1; j < n; j++){
+			swap(A[i][j], A[j][i]);
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		printf("%d ", A[i][j]);
+		printf("\n");
+	}
+
+	return 0;
+}
