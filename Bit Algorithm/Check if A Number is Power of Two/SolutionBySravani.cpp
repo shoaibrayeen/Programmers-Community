feb4368ffbad88146d2b903498cf777690dd2da7
Sravani-7701 <sravanishivasln@gmail.com>
2021-10-03 08:34:34 +0530

    File added

---
+++ Bit Algorithm/Check if A Number is Power of Two/SolutionBySravani.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n &&(!(n&(n-1)))){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+	return 0;
+}
+
