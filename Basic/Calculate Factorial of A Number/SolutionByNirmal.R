94baeaafdbefb1af23df7d1c622aa7bf29140f21
Nirmal Nath Upadhyay <91836957+nirmalnathupadhyay@users.noreply.github.com>
2021-10-07 22:17:37 +0530

    Solution File

---
+++ Basic/Calculate Factorial of A Number/SolutionByNirmal.R
@@ -0,0 +1,19 @@
+# check is the number is negative, positive or zero
+
+factorial <- function(num) {
+    if(num < 0) {
+        print("Sorry, factorial does not exist for negative numbers")
+    } else if(num == 0) {
+        print("The factorial of 0 is 1")
+    } else {
+        factorial = 1
+        for(i in 1:num) {
+            factorial = factorial * i
+        }
+        print(paste("The factorial of", num ,"is",factorial))
+    }
+}
+
+
+num <- sample(1 : 2000, 1)
+factorial(num)
