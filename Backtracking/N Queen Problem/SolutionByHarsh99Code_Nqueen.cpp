4960ec38fb24e836a09d0dbcfd308d3cc8d70930
harsh99code <72193356+harsh99code@users.noreply.github.com>
2021-10-07 23:04:48 +0530

    Nqueen using bitwise solution

---
+++ Backtracking/N Queen Problem/SolutionByHarsh99Code_Nqueen.cpp
@@ -0,0 +1,30 @@
+/*
+  Program Description - nQueen using bitsets
+  
+*/
+
+#include<iostream>
+#include<bitset>
+using namespace std;
+ 
+bitset<30> col,d1,d2;   // column,  diagonal one and diagonal 2
+void solve(int r,int n,int &ans){
+        if(r==n){
+            ans++;
+            return ;
+        }
+        for(int c=0;c<n;c++){
+            if( !col[c] and !d1[r-c+n-1] && !d2[r+c]){
+                col[c] = d1[r-c+n-1] = d2[r+c] = 1;
+                solve(r+1,n,ans);
+                col[c] = d1[r-c+n-1] = d2[r+c] = 0;
+            }
+        }
+}
+int main(){
+    int n;
+    cin>>n;
+    int ans=0;
+    solve(0,n,ans);
+    cout<<ans;
+}
