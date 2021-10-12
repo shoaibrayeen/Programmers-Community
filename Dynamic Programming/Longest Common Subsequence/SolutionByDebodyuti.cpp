1254c69508061efb3b4b935feb9494c561a41a27
debodyuti1 <db8145@srmist.edu.in>
2021-10-12 10:59:03 +0530

    Added Solution to Longest Common Subsequence

---
+++ Dynamic Programming/Longest Common Subsequence/SolutionByDebodyuti.cpp
@@ -0,0 +1,43 @@
+//Space optimized C++ implementation
+// of LCS problem
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+
+int lcs(string &X, string &Y)
+{
+     
+    int m = X.length(), n = Y.length();
+    int L[2][n + 1];
+    bool bi;
+ 
+    for (int i = 0; i <= m; i++)
+    {
+
+        bi = i & 1;
+        for (int j = 0; j <= n; j++)
+        {
+            if (i == 0 || j == 0)
+                L[bi][j] = 0;
+ 
+            else if (X[i-1] == Y[j-1])
+                 L[bi][j] = L[1 - bi][j - 1] + 1;
+ 
+            else
+                L[bi][j] = max(L[1 - bi][j],
+                               L[bi][j - 1]);
+        }
+    }
+
+    return L[bi][n];
+}
+ 
+// Driver code
+int main()
+{
+    string X = "AGGTAB";
+    string Y = "GXTXAYB";
+    printf("Length of LCS is %d\n", lcs(X, Y));
+    return 0;
+}
