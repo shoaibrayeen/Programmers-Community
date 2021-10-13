bd601b2917880172ad61bc6fe220202a9916fb71
Karna Shah <54734810+karna20@users.noreply.github.com>
2021-10-14 01:23:02 +0530

    Update SolutionbyKarna.cpp

--- Greedy Algorithms/Kruskal's Algorithm/SolutionbyKarna.cpp
+++ Greedy Algorithms/Kruskal's Algorithm/SolutionbyKarna.cpp
@@ -1,159 +1,108 @@
-// C++ program for Kruskal's algorithm to find Minimum
-// Spanning Tree of a given connected, undirected and
-// weighted graph
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
 
-// Creating shortcut for an integer pair
-typedef pair<int, int> iPair;
-
-// Structure to represent a graph
-struct Graph
-{
-	int V, E;
-	vector< pair<int, iPair> > edges;
-
-	// Constructor
-	Graph(int V, int E)
-	{
-		this->V = V;
-		this->E = E;
-	}
-
-	// Utility function to add an edge
-	void addEdge(int u, int v, int w)
-	{
-		edges.push_back({w, {u, v}});
-	}
-
-	// Function to find MST using Kruskal's
-	// MST algorithm
-	int kruskalMST();
+/* Define an edge struct to handle edges more easily.*/
+struct edge {
+	int first, second, weight;
 };
 
-// To represent Disjoint Sets
-struct DisjointSets
-{
-	int *parent, *rnk;
-	int n;
+/* Needed to typedef to reduce keystrokes or copy/paste */
+typedef std::vector< std::vector< std::pair<int,int> > > adj_list;
 
-	// Constructor.
-	DisjointSets(int n)
-	{
-		// Allocate memory
-		this->n = n;
-		parent = new int[n+1];
-		rnk = new int[n+1];
+std::vector<edge> kruskal(const adj_list& graph);
+int get_pred(int vertex, const std::vector<int>& pred);
 
-		// Initially, all vertices are in
-		// different sets and have rank 0.
-		for (int i = 0; i <= n; i++)
-		{
-			rnk[i] = 0;
-
-			//every element is parent of itself
-			parent[i] = i;
-		}
+int main() {
+	int n,m; std::cin >> n >> m;
+	
+	adj_list graph(n);
+	int f,s,w;
+	while (m-- > 0) {
+		std::cin >> f >> s >> w;
+		if (f == s) continue; /* avoid loops */
+		graph[ f-1 ].push_back( std::make_pair( s-1 , w ) );
 	}
 
-	// Find the parent of a node 'u'
-	// Path Compression
-	int find(int u)
-	{
-		/* Make the parent of the nodes in the path
-		from u--> parent[u] point to parent[u] */
-		if (u != parent[u])
-			parent[u] = find(parent[u]);
-		return parent[u];
+	std::vector<edge> result = kruskal(graph);
+
+	std::cout << "Here is the minimal tree:\n";
+	for (auto& _edge : result) {
+		std::cout << char(_edge.first+65) << " connects to " << char(_edge.second+65) << std::endl;
 	}
-
-	// Union by rank
-	void merge(int x, int y)
-	{
-		x = find(x), y = find(y);
-
-		/* Make tree with smaller height
-		a subtree of the other tree */
-		if (rnk[x] > rnk[y])
-			parent[y] = x;
-		else // If rnk[x] <= rnk[y]
-			parent[x] = y;
-
-		if (rnk[x] == rnk[y])
-			rnk[y]++;
-	}
-};
-
-/* Functions returns weight of the MST*/
-
-int Graph::kruskalMST()
-{
-	int mst_wt = 0; // Initialize result
-
-	// Sort edges in increasing order on basis of cost
-	sort(edges.begin(), edges.end());
-
-	// Create disjoint sets
-	DisjointSets ds(V);
-
-	// Iterate through all sorted edges
-	vector< pair<int, iPair> >::iterator it;
-	for (it=edges.begin(); it!=edges.end(); it++)
-	{
-		int u = it->second.first;
-		int v = it->second.second;
-
-		int set_u = ds.find(u);
-		int set_v = ds.find(v);
-
-		// Check if the selected edge is creating
-		// a cycle or not (Cycle is created if u
-		// and v belong to same set)
-		if (set_u != set_v)
-		{
-			// Current edge will be in the MST
-			// so print it
-			cout << u << " - " << v << endl;
-
-			// Update MST weight
-			mst_wt += it->first;
-
-			// Merge two sets
-			ds.merge(set_u, set_v);
-		}
-	}
-
-	return mst_wt;
-}
-
-// Driver program to test above functions
-int main()
-{
-	/* Let us create above shown weighted
-	and unidrected graph */
-	int V = 9, E = 14;
-	Graph g(V, E);
-
-	// making above shown graph
-	g.addEdge(0, 1, 4);
-	g.addEdge(0, 7, 8);
-	g.addEdge(1, 2, 8);
-	g.addEdge(1, 7, 11);
-	g.addEdge(2, 3, 7);
-	g.addEdge(2, 8, 2);
-	g.addEdge(2, 5, 4);
-	g.addEdge(3, 4, 9);
-	g.addEdge(3, 5, 14);
-	g.addEdge(4, 5, 10);
-	g.addEdge(5, 6, 2);
-	g.addEdge(6, 7, 1);
-	g.addEdge(6, 8, 6);
-	g.addEdge(7, 8, 7);
-
-	cout << "Edges of MST are \n";
-	int mst_wt = g.kruskalMST();
-
-	cout << "\nWeight of MST is " << mst_wt;
 
 	return 0;
+}
+
+std::vector<edge> kruskal(const adj_list& graph) {
+	std::vector<edge> edges, minimum_spanning_tree;
+
+	/*
+		`pred` will represent our Disjointed sets by naming a set head.
+		In the beginning, each node is its own head in its own set.
+		We merge sets in the while loop.
+	*/
+	std::vector<int> pred(graph.size());
+
+	for (int i = 0, n = graph.size(); i < n; i++) {
+		for (auto& _edge : graph[i])
+			edges.push_back( { i, _edge.first, _edge.second } );
+		pred[i] = i;
+	}
+
+	/*
+		Let's reverse-sort our edge vector
+		so that we can just pop off the last (smallest)
+		element.
+	*/
+	auto comp = [&](edge left, edge right) { return left.weight > right.weight; };
+	std::sort(edges.begin(), edges.end(), comp);
+
+	while( !edges.empty() ) {
+
+		/* get shortest/least-heavy edge */
+		edge shortest = edges.back();
+		edges.pop_back();
+
+
+		int f_head,s_head; /* first_head, second... */
+		f_head = get_pred(shortest.first, pred);
+		s_head = get_pred(shortest.second, pred);
+
+
+		/*
+			If the nodes making up a certain edge are
+			not already in the same set...
+		*/
+		if (f_head != s_head) {
+			/* Add that edge to the Min. Span. Tree*/
+			minimum_spanning_tree.push_back(shortest);
+
+			/*
+				Merge the sets by setting
+				the head of one set to the head
+				of the other set.
+				If the head of one set is A and the other is C,
+				as long as we point C to A, all nodes part of the
+				set once headed by C will find A in linear time.
+			*/
+			if (f_head < s_head)
+				pred[s_head] = f_head;
+			else
+				pred[f_head] = s_head;
+		}
+	}
+
+	return minimum_spanning_tree;
+}
+
+int get_pred(int vertex, const std::vector<int>& pred) {
+	/*
+		We stop when a node/vertex is its own predecessor.
+		This means we have found the head of the set.
+	*/
+	while(pred[vertex] != vertex)
+		vertex = pred[vertex];
+	return vertex;
 }
