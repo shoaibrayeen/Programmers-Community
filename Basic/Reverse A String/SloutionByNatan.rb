da076a0ab124a5aa494b58e0e22063d57b144265
Natan7 <natan.ribeiro@ccc.ufcg.edu.br>
2022-10-02 11:34:15 -0300

    fix comment

--- Basic/Reverse A String/SloutionByNatan.rb
+++ Basic/Reverse A String/SloutionByNatan.rb
@@ -2,7 +2,7 @@ def reverse (word)
   return reversed_word = word.reverse
 end
 
-# Take a word from the user as input and your output is 'Yes' if is palindrome word or 'NO' if not
+# Take a word from the user as input and your output is reverse word
 print("Enter a word: ")
 word = gets.chomp
 
