76bdae971cd4a85b6382e9561e93d52d41f6976d
Bavleen Kaur <47499446+bavkaur@users.noreply.github.com>
2023-10-03 19:48:26 +0530

    Create SolutionByBavleen.CPP

---
+++ Basic/Check Whether a Number is Even or Odd/SolutionByBavleen.CPP
@@ -0,0 +1,14 @@
+#include <iostream>
+int main() {
+int a;
+std::cout<<"Enter a number ";   
+std::cin>>a;
+   if(a%2==0)
+   {
+        std::cout<<"it's an even number";
+   }
+   else
+   {
+       std::cout<<"it's an odd number";
+   }
+}
