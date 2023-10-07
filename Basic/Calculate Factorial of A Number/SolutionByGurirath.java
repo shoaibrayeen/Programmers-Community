c17cd641d2b07051a96521b03a69712b618e6c34
Gurirath <73177464+Gurirath@users.noreply.github.com>
2023-10-07 23:56:38 +0530

    SolutionByGurirath.java

---
+++ Basic/Calculate Factorial of A Number/SolutionByGurirath.java
@@ -0,0 +1,22 @@
+
+/**
+ Write a program to print the factorial of a number.
+ */
+import java.util.*;
+public class Hacktoberfest1
+{
+    public static void main(String args[])
+    {
+        Scanner kb= new Scanner(System.in);
+        int f=1,i;
+        System.out.println("Enter a number to print its factorial");
+        int n=kb.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            f=f*i;
+        }
+        System.out.println("Factorial:"+f);
+        
+        
+    }
+}
